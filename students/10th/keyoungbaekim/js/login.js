71635f6644108ebe6053deecfbe269e13fed62ae
KIM <rudqo14@gmail.com>
2020-07-03 23:25:13 +0900

    Fixed: 리뷰 반영하여 코드 수정

--- students/10th/keyoungbaekim/js/login.js
+++ students/10th/keyoungbaekim/js/login.js
@@ -1,13 +1,7 @@
-// # 로그인 페이지
-// 1. id, pw 에 각각 한 글자 이상 써야 버튼이 활성화 되도록 해주세요.
-// - 원래 연한 파란색이었다가 -> 활성화 되면 파란색으로!
-const keyupIdValue = document.getElementsByClassName('id')[0].value;
-const keyupPwValue = document.getElementsByClassName('pw')[0].value;
 
-const keyupId = document.getElementsByClassName('id')[0];
-keyupId.addEventListener('keyup', function(){
-    const keyupPwValue = document.getElementsByClassName('pw')[0].value;
-    const keyupIdValue = document.getElementsByClassName('id')[0].value;
+function idPasswordComparison() {
+    const keyupPwValue = keyupPw.value;
+    const keyupIdValue = keyupId.value;
 
     if(keyupPwValue !== "" && keyupIdValue !== ""){
         let activeBtn = document.getElementsByClassName('loginBtn')[0];
@@ -20,24 +14,15 @@ keyupId.addEventListener('keyup', function(){
         activeBtn.style = "cursor:default";
         activeBtn.style.backgroundColor = "#B2DFFC";
     }
-});
+}
 
-const keyupPw = document.getElementsByClassName('pw')[0];
+const keyupId = document.getElementsByClassName('loginInputId')[0];
+const keyupPw = document.getElementsByClassName('loginInputPw')[0];
+keyupId.addEventListener('keyup', function(){
+    idPasswordComparison();
+});
 keyupPw.addEventListener('keyup', function(){
-    const keyupIdValue = document.getElementsByClassName('id')[0].value;
-    const keyupPwValue = document.getElementsByClassName('pw')[0].value;
-    
-    if(keyupIdValue !== "" && keyupPwValue !== ""){
-        let activeBtn = document.getElementsByClassName('loginBtn')[0];
-        activeBtn.disabled = false;
-        activeBtn.style = "cursor:pointer";
-        activeBtn.style.backgroundColor = "#0095F6";
-    }else{
-        let activeBtn = document.getElementsByClassName('loginBtn')[0];
-        activeBtn.disabled = true;
-        activeBtn.style = "cursor:default";
-        activeBtn.style.backgroundColor = "#B2DFFC";
-    }
+    idPasswordComparison();
 });
 
 
