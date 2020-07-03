2fc8fd7f83281e9f12dacef40f4af86e900fa643
leeyosi <dark235612@gmail.com>
2020-07-03 10:23:30 +0900

    인스타그램 초기세팅 완료(10기 이윤식)

--- students/10th/이윤식/js/main.js
+++ students/10th/이윤식/js/main.js
@@ -1,8 +1,6 @@
 const thisIsButton = document.querySelector('.coment-button');
 const comment = document.querySelector('.coment')
 const comment_controler = document.querySelector('.commnet-plus');
-const search = document.querySelector('.search')
-const searchcontainer = document.querySelector('.search_contaner')
 
 function commentHandler(e) {
     if(event.keyCode == 13){
@@ -24,13 +22,14 @@ function commentHandler(e) {
         divtag2.appendChild(spantag1)
         divtag2.appendChild(spantag2)
         divtag.appendChild(buttontag)
+        comment.value = ""
 
         buttontag.addEventListener('click',function() {
             divtag.parentNode.removeChild(divtag)
         });
     }
 }
-    function commnetlickHandler(){
+    function commnetclickHandler(){
         let commnetDivtag = document.createElement('div')
         let commnetDivtag2 = document.createElement('div')
         let commnetSpantag1 = document.createElement('span')
@@ -52,31 +51,5 @@ function commentHandler(e) {
         spantag2.innerHTML = ""
     }
 
-    // function searchClickHandler(){
-    //     let searchDivtag = document.createElement('div')
-    //     let searchDivtag2 = document.createElement('div')
-    //     let searchDivtag3 = document.createElement('div')
-    //     let searchDivtag4 = document.createElement('div')
-    //     let searchDivtag5 = document.createElement('div')
-    //     let searchDivtag6 = document.createElement('div')
-
-    //     searchDivtag.classList = "classSearch_divtag"
-    //     searchDivtag2.classList = "classSearch_divtag2"
-    //     searchDivtag3.classList = "classSearch_divtag3"
-    //     searchDivtag4.classList = "classSearch_divtag4"
-    //     searchDivtag5.classList = "classSearch_divtag5"
-    //     searchDivtag6.classList = "classSearch_divtag6"
-
-    //     searchcontainer.appendChild(searchDivtag)
-    //     searchDivtag.appendChild(searchDivtag2)
-    //     searchDivtag.appendChild(searchDivtag3)
-    //     searchDivtag.appendChild(searchDivtag4)
-    //     searchDivtag.appendChild(searchDivtag5)
-    //     searchDivtag.appendChild(searchDivtag6)
-
-    //     console.log(searchDivtag);
-    // }
-
-// search.addEventListener('keyup', )
 comment.addEventListener('keyup', commentHandler);
 thisIsButton.addEventListener('click', commnetclickHandler);
