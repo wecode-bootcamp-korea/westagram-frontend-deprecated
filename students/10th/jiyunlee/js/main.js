6e6edcd763df9e2eff7de6c33060aba7ef5e0679
limo-sum <i01029407043@gmail.com>
2020-07-04 12:32:52 +0900

    Refactor: 댓글 기능 개선

--- students/10th/jiyunlee/js/main.js
+++ students/10th/jiyunlee/js/main.js
@@ -2,65 +2,72 @@ const inputComment = document.getElementsByClassName("inputComment");
 const commentBtn = document.getElementsByClassName("commentBtn");
 
 // ACTIVATE BUTTON
-
 function activateCommentBtn(num) {
-    (inputComment[num].value !== "")
-    ? (commentBtn[num].style.color= "rgb(38, 167, 218)",
-    commentBtn[num].disabled = false)
-    : (commentBtn[num].style.color="lightblue",
-    commentBtn[num].disabled = true)
+  inputComment[num].value !== ""
+    ? ((commentBtn[num].style.color = "rgb(38, 167, 218)"),
+      (commentBtn[num].disabled = false))
+    : ((commentBtn[num].style.color = "lightblue"),
+      (commentBtn[num].disabled = true));
 }
 
-for(let i = 0; i < inputComment.length; i++){    
-    inputComment[i].addEventListener('keyup', () => activateCommentBtn(i)) ; 
+for (let i = 0; i < inputComment.length; i++) {
+  inputComment[i].addEventListener("keyup", () => activateCommentBtn(i));
 }
 
 // ADD COMMENTS
-function showComments(comment, j){
-    event.preventDefault();
-    inputComment[j].value="";
-    const commentsFrame = document.getElementsByClassName('commentsFrame')[j];
-    let eachComment = document.createElement('li');
-    let commentId = document.createElement('strong');
-    let commentValue = document.createElement('span');
-    let deleteBtn = document.createElement('button');
-    deleteBtn.addEventListener("click", function(){
-        eachComment.parentElement.removeChild(eachComment);
-    })
-    eachComment.classList.add('eachComment');
-    commentId.classList.add('commentId');
-    commentValue.classList.add('commentValue');
-    deleteBtn.classList.add('deleteBtn');
+function showComments(comment, j) {
+  event.preventDefault();
+  inputComment[j].value = "";
+  const commentsFrame = document.getElementsByClassName("commentsFrame")[j];
+  let eachComment = document.createElement("li");
+  let commentId = document.createElement("strong");
+  let commentValue = document.createElement("span");
+  let deleteBtn = document.createElement("button");
+  deleteBtn.addEventListener("click", function () {
+    eachComment.parentElement.removeChild(eachComment);
+  });
+  eachComment.classList.add("eachComment");
+  commentId.classList.add("commentId");
+  commentValue.classList.add("commentValue");
+  deleteBtn.classList.add("deleteBtn");
 
-    commentId.innerText = "limosum91";
-    commentValue.innerText = comment + '\n';
-    deleteBtn.innerText = "X";
-    commentId.style.flexShrink = '0';
-    commentValue.style.flexGrow = '1';
-    deleteBtn.style.flexGrow = '1';
+  commentId.innerText = "limosum91";
+  commentValue.innerText = comment + "\n";
+  deleteBtn.innerText = "X";
+  commentId.style.flexShrink = "0";
+  commentValue.style.flexGrow = "1";
+  deleteBtn.style.flexGrow = "1";
 
-    eachComment.appendChild(commentId);
-    eachComment.appendChild(commentValue);
-    eachComment.appendChild(deleteBtn);
-    commentsFrame.appendChild(eachComment);
+  eachComment.appendChild(commentId);
+  eachComment.appendChild(commentValue);
+  eachComment.appendChild(deleteBtn);
+  commentsFrame.appendChild(eachComment);
 }
 
-for(let j = 0; j < inputComment.length; j++){    
-    commentBtn[j].addEventListener('click', () => showComments(inputComment[j].value, j));
+for (let k = 0; k < inputComment.length; k++) {
+  inputComment[k].addEventListener("keyup", (e) =>
+    e.keyCode === 13 ? showComments(inputComment[k].value, k) : undefined
+  );
 }
 
-const moreBtn =  document.getElementsByClassName('moreBtn');
-const paragraph= document.getElementsByClassName("paragraph");
-
-function seeMore(num){
-    moreBtn[num].style.display = "none";
-    paragraph[num].style.maxWidth= "640px";
-    paragraph[num].style.lineHeight= "auto";
-    paragraph[num].style.whiteSpace= "normal";
-    paragraph[num].style.overflow= "visible";
-    
-    
+for (let j = 0; j < inputComment.length; j++) {
+  commentBtn[j].addEventListener("click", () =>
+    showComments(inputComment[j].value, j)
+  );
 }
-for(let k = 0; k < paragraph.length; k++){    
-    moreBtn[k].addEventListener('click', () => seeMore(k));
+
+// SEE HIDDEN CONTENTS
+const moreBtn = document.getElementsByClassName("moreBtn");
+const paragraph = document.getElementsByClassName("paragraph");
+
+function seeMore(num) {
+  moreBtn[num].style.display = "none";
+  paragraph[num].style.maxWidth = "640px";
+  paragraph[num].style.lineHeight = "auto";
+  paragraph[num].style.whiteSpace = "normal";
+  paragraph[num].style.overflow = "visible";
+}
+
+for (let k = 0; k < paragraph.length; k++) {
+  moreBtn[k].addEventListener("click", () => seeMore(k));
 }
