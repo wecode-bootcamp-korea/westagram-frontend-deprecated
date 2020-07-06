182b77dd27bcc9fe860fd1708fd5e909a7a7bea5
limo-sum <i01029407043@gmail.com>
2020-07-06 19:12:49 +0900

    Refactor: review 반영

--- students/10th/jiyunlee/js/login.js
+++ students/10th/jiyunlee/js/login.js
@@ -1,22 +1,20 @@
-const btn_login = document.getElementsByClassName('btn_login')[0];
+const btn_login = document.getElementsByClassName("btn_login")[0];
 const info = document.getElementsByTagName("input");
-const idvalue = document.getElementById("inputid");
-const pwvalue = document.getElementById("inputpw");
+const inInput = document.getElementById("inputid");
+const pwInput = document.getElementById("inputpw");
 const inputLogin = document.getElementById("inputLogin");
 
-function handleBtnColor() {
-    (idvalue.value !== "" && pwvalue.value !== "") 
-    ? (btn_login.style.backgroundColor = "skyblue",
-      btn_login.disabled = false,
-      pressEnter())
-    : (btn_login.style.backgroundColor = "lightblue",
-      btn_login.disabled = true)
+function handleBtnColor(e) {
+  inInput.value !== "" && pwInput.value !== ""
+    ? ((btn_login.style.backgroundColor = "skyblue"),
+      (btn_login.disabled = false),
+      pressEnter(e))
+    : ((btn_login.style.backgroundColor = "lightblue"),
+      (btn_login.disabled = true));
 }
 
-idvalue.addEventListener("keyup", handleBtnColor);
-pwvalue.addEventListener("keyup", handleBtnColor);
+inputLogin.addEventListener("keyup", handleBtnColor);
 
-function pressEnter(){
-  const keyCode = window.event.keyCode;
-  (keyCode == 13) ? inputLogin.submit() : undefined
+function pressEnter(e) {
+  e.keyCode == 13 ? inputLogin.submit() : undefined;
 }
