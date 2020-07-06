e080e5f95276db6a7f8747de941678edfe568773
bhoon-coding <bhooncoding@gmail.com>
2020-07-06 13:52:39 +0900

    {이병훈} Add : 로그인,메인,레이아웃 잡음.

--- students/10th/ByeongHoonLee/js/login.js
+++ students/10th/ByeongHoonLee/js/login.js
@@ -4,8 +4,10 @@ const btnLogin = document.querySelector("button");
 const idPwInput = document.querySelector(".id_pw_form");
 
 idPwInput.addEventListener("keyup", function () {
+
   let pwInputValue = pwInput.value;
   let idInputValue = idInput.value;
+
   if (idInputValue !== "" && pwInputValue !== "") {
     btnLogin.style.backgroundColor = "#0095f6";//어두운
   } else {
