e2de74413dac6f5e2292dbe1a049b80f8eddc59d
Taehee Choi <taeheechoi@Taeheeui-MacBookPro.local>
2020-07-07 13:30:02 +0900

    modify- id명 오타수정,불필요한주석및console.log삭제,img태그에alt속성추가

--- students/10th/taehee choi/westagram/js/login.js
+++ students/10th/taehee choi/westagram/js/login.js
@@ -1,14 +1,9 @@
-// 1. id input 태그, pw input 태그 dom 가져오기
-// 2. 각각의 dom에 이벤트리스너 달기
-
-const idInput = document.getElementsByClassName("login_id")[0]; //.value 는 string
+const idInput = document.getElementsByClassName("login_id")[0]; //.value method는 string 타입으로 변환됨 
 const pwInput = document.getElementsByClassName("login_pw")[0];
 const btn = document.getElementsByClassName("login_button")[0];
 
 idInput.addEventListener("keyup", changeColor);
 pwInput.addEventListener("keyup", changeColor);
-
-console.log(idInput.value);
 
 
 function changeColor() {
@@ -20,12 +15,3 @@ function changeColor() {
         btn.style.backgroundColor = "lightblue";
     }
 }
-
-
-
-
-// id, pw 에 각각 한 글자 이상 써야 버튼이 활성화 되도록 해주세요.
-// 원래 연한 파란색이었다가 -> 활성화 되면 파란색으로!
-
-//function changeButtonColor() {
-//  document.getElementById("myButton").style.backgroundColor = "green";}
