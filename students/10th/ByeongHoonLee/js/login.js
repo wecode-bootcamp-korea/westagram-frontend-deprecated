59df0fee28e3b068cfe51056b7b2509e09ccdbf4
bhoon-coding <bhooncoding@gmail.com>
2020-07-07 20:29:24 +0900

    [이병훈] Refactor :html,css들여쓰기 삼항연산자 사용

--- students/10th/ByeongHoonLee/js/login.js
+++ students/10th/ByeongHoonLee/js/login.js
@@ -1,18 +1,21 @@
-const idInput = document.querySelector(".id_input");
-const pwInput = document.querySelector(".pw_input");
-const btnLogin = document.querySelector("button");
-const idPwInput = document.querySelector(".id_pw_form");
+const inputId = document.querySelector(".id_input");
+const inputPw = document.querySelector(".pw_input");
+const loginButton = document.querySelector("button");
+const idPwForm = document.querySelector(".id_pw_form");
 
-idPwInput.addEventListener("keyup", function () {
+idPwForm.addEventListener("keyup", function () {
 
-  let pwInputValue = pwInput.value;
-  let idInputValue = idInput.value;
+  let inputPwValue = inputPw.value;
+  let inputIdValue = inputId.value;
 
-  if (idInputValue !== "" && pwInputValue !== "") {
-    btnLogin.style.backgroundColor = "#0095f6";//어두운
-  } else {
-    btnLogin.style.backgroundColor = "rgba(0, 149, 246, 0.3)";//밝은
-  }
-});
+  inputIdValue !== "" && inputPwValue !== "" ? loginButton.style.backgroundColor = "#0095f6"
+    : loginButton.style.backgroundColor = "rgba(0, 149, 246, 0.3)";
+}
+//   if (inputIdValue !== "" && inputPwValue !== "") {
+//     loginButton.style.backgroundColor = "#0095f6";//어두운
+//   } else {
+//     loginButton.style.backgroundColor = "rgba(0, 149, 246, 0.3)";//밝은
+//   }
+// });
 
 
