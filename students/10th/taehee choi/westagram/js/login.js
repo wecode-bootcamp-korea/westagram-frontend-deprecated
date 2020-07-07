3bac52454110b214180b99eac26ea1a7dff1f138
Taehee Choi <taeheechoi@Taeheeui-MacBookPro.local>
2020-07-07 15:24:54 +0900

    modify: id 셀렉터를 class셀럭터로 변경, 불필요한 띄어쓰기 삭제, 오타수정

--- students/10th/taehee choi/westagram/js/login.js
+++ students/10th/taehee choi/westagram/js/login.js
@@ -1,17 +1,15 @@
 const idInput = document.getElementsByClassName("login_id")[0]; //.value method는 string 타입으로 변환됨 
 const pwInput = document.getElementsByClassName("login_pw")[0];
 const btn = document.getElementsByClassName("login_button")[0];
+const logInBox = document.getElementsByClassName("box");
 
-idInput.addEventListener("keyup", changeColor);
-pwInput.addEventListener("keyup", changeColor);
-
+logInBox.addEventListener("keyup", changeColor);
 
 function changeColor() {
     if (idInput.value !== "" && pwInput.value !== "") {
         btn.style.backgroundColor = "green";
         document.querySelector(".login_button").removeAttribute("disabled");
-    }
-    else {
+    } else {
         btn.style.backgroundColor = "lightblue";
     }
 }
