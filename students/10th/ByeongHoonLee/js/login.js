29c78ec9b6cca3f296d03ffcf5157bbded93fc9a
bhoon-coding <bhooncoding@gmail.com>
2020-07-08 20:42:09 +0900

    [이병훈] Refactor : 불필요한 주석 , 중복코드 , 요약 완료

--- students/10th/ByeongHoonLee/js/login.js
+++ students/10th/ByeongHoonLee/js/login.js
@@ -4,18 +4,15 @@ const loginButton = document.querySelector("button");
 const idPwForm = document.querySelector(".id_pw_form");
 
 idPwForm.addEventListener("keyup", function () {
+  let isBtnActive = inputPw.value && inputId.value;
 
-  let inputPwValue = inputPw.value;
-  let inputIdValue = inputId.value;
-
-  loginButton.style.backgroundColor = (inputIdValue && inputPwValue) ?
-    "#0095f6" : "rgba(0, 149, 246, 0.3)";
+  loginButton.style.backgroundColor = isBtnActive
+    ? "#0095f6"
+    : "rgba(0, 149, 246, 0.3)";
 });
-  // if (inputIdValue && inputPwValue) {
-  //   loginButton.style.backgroundColor = "#0095f6";//어두운
-  // } else {
-  //   loginButton.style.backgroundColor = "rgba(0, 149, 246, 0.3)";//밝은
-  // }
+// if (inputIdValue && inputPwValue) {
+//   loginButton.style.backgroundColor = "#0095f6";//어두운
+// } else {
+//   loginButton.style.backgroundColor = "rgba(0, 149, 246, 0.3)";//밝은
+// }
 // });
-
-
