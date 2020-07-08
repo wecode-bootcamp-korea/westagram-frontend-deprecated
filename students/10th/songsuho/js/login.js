1203869bb5ecdb2d05d449aa1736bc4ba085cc1f
kasumil <ridou99@gmail.com>
2020-07-08 21:44:17 +0900

    add 초기세팅완료(commit msg 수정)

--- students/10th/songsuho/js/login.js
+++ students/10th/songsuho/js/login.js
@@ -1,7 +1,6 @@
 let info = document.getElementsByClassName('idInput')[0];
 let pwd = document.getElementsByClassName('pwInput')[0];
 
-console.log(info, pwd)
 document.addEventListener('keyup',function(e) {
     if (info.value !== "" && pwd.value !== "") { 
             let color = document.getElementById('loginbtn');
--- students/10th/songsuho/js/main.js
+++ students/10th/songsuho/js/main.js
@@ -3,8 +3,6 @@ let input_Text = document.getElementsByClassName('comment_Input')[0];
 let pickup_idcontents=document.getElementsByClassName('id_contents')[0];
 let comment_value = document.getElementsByClassName('comment_Input');
 
-/// 밸류 값을 제대로 출력하지 못하는 에러가 출력됨.(해결)
-
 input_Text.addEventListener('keyup', function(e) {
     if (e.keyCode === 13) {
         let comment_Box = document.createElement('div');
@@ -38,11 +36,6 @@ push.addEventListener('click', function() {
     }
 })
 
-
-
-///////// 찾아보니 3가지의 기능이 요구됨. 1. 엔터나 게시를 눌러야 댓글이 추가되는 기능(해결)
-///// 푸쉬하고 나서 인풋의 내용삭제가 요구됨(해결)
-///// 친 내용이 해당 피드 섹션부분에 올라가야할 것.(해결)
 
 
 
