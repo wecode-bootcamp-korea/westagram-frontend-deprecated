58333e7e31cad2d0cd740d852f4db01516f9a85d
Yea JIn Park <yeajinpark@MacBook-Pro-71.local>
2020-07-14 06:34:45 +0900

    Refactor: 로그인 기능구현 코드 수정

--- students/10th/yeajinpark/westagram/js/login.js
+++ students/10th/yeajinpark/westagram/js/login.js
@@ -2,11 +2,17 @@ document.addEventListener("keyup", function() {
     const pw = document.getElementById('id').value;
     const id = document.getElementById('pw').value;
 
-    if (pw !== "" && id!=="") {
+    if (pw!=="" && id!=="") {
         const loginButton = document.getElementsByClassName('login-btn')[0];
-        loginButton.style.color = '#3e95ef';
+        loginButton.style.backgroundColor = '#3e95ef';
     }
-});
+        else if (pw=="" && id=="") {
+
+        const loginButton = document.getElementsByClassName('login-btn')[0];
+        loginButton.style.backgroundColor = '#c5dffa';
+        }
+    
+    });
 
 // keyup (누르고 난 후에 값이 입력됨.)
 // const는 함수 밖에 선언을 하면 id, pw가 다 빈값임.
