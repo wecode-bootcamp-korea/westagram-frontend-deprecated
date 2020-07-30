9e7d6e71f714dbf6102bbb7a2b1342e0fa4b05be
최예흠 <cyheum@naver.com>
2020-07-30 20:59:50 +0900

    backgroun-image url 수정 완료

--- students/11th/yeheumChoi/js/login.js
+++ students/11th/yeheumChoi/js/login.js
@@ -1,3 +1,4 @@
+// 아이디, 비밀번호(6자이상) 입력시 버튼 활성화
 const idText = document.getElementsByClassName("loginidText")[0];
 const pwText = document.getElementsByClassName("loginpwText")[0];
 const loginBtn = document.getElementsByClassName("loginButton")[0];
