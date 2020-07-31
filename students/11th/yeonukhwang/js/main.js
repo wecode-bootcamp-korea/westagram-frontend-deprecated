25776c0e71cb0c7f13a6590fdc57e7ed2c3aa36e
Yeonuk <younuk23@gmail.com>
2020-07-31 13:58:44 +0900

    Add : 메인페이지에 좋아요 기능 추가 - 하트버튼 누를시 하트 이미지 빨간하트로 변경기능 추가 - 하트버튼 누를 시 좋아요 한 사람 숫자 변경기능 추가

--- students/11th/yeonukhwang/js/main.js
+++ students/11th/yeonukhwang/js/main.js
@@ -1,68 +1,10 @@
-// let commentBtn = document.getElementById('commentBtn'); //get commentbtn
-// let txt = document.getElementsByClassname('comment')[0]; // get input
-// function appendComment() {
-//   let commentbox = document.createElement('li'); //li tag make
-//   let commentListLeft = document.createElement('div');//div tag make
-//   let commentAuthor = document.createElement('a');//a tag make
-//   let comment = document.createElement('span');// span tag make
-//   let heart = document.createElement('img'); // img tag make
-//   let txt = document.getElementById('comment');
-//   let commentValue = document.getElementById('comment').value; // get textarea value
-//   let commentList = document.getElementsByClassname('commentList')[0]; // get Ul tag
-//   comment.innerHTML = `&nbsp${commentValue}`; // insert comment into span
-//   heart.setAttribute('src','/img/heart.png');
-//   heart.setAttribute('alt','like-to-comment');
-//   commentAuthor.setAttribute('class', 'name')
-//   commentAuthor.innerHTML = 'sotonghaeyo' // make author name, default , 나중에 로그인 한 사람으로 바꾸자
-//   commentListLeft.appendChild(commentAuthor); // a > div
-//   commentListLeft.appendChild(comment); // span > div
-//   commentbox.appendChild(commentListLeft); // div > li
-//   commentbox.appendChild(heart) // heart > li
-//   commentList.appendChild(commentbox); // li > ul
-//   txt.value = "";
-// }
-
-// commentBtn.addEventListener('click',appendComment)
-// txt.addEventListener('keydown', (e) => {
-//   let enter = e.keyCode? e.keyCode : e.which;
-//   if(enter === 13){
-//     appendComment();
-//   }
-// });
-
-// // for Each
-// const ul = article[i].getElementsByTagname('ul'); // get ul
-// const commentBtn = article[i].getElementsByTagname('button')[0]; // get buttton
-// const comment = article[i].getElementsByTagname('textarea')[0]; // get textarea
-// let newCommentValue = comment.value; // get textarea Value
-// const commentbox = document.createElement('li'); //li tag make
-// const commentListLeft = document.createElement('div');//div tag make
-// const commentAuthor = document.createElement('a');//a tag make
-// const newComment = document.createElement('span');// span tag make
-// const heart = document.createElement('img'); // img tag make
-// newComment.innerHTML = `&nbsp${newCommentValue}`; //
-// heart.setAttribute('src','/img/heart.png');
-// heart.setAttribute('alt','like-to-comment');
-// commentAuthor.setAttribute('class', 'name')
-// commentListLeft.setAttribute('class','commentListLeft');
-// commentAuthor.innerHTML = 'sotonghaeyo' // make author name, default , 나중에 로그인 한 아이디로 바꾸는 로직 추가
-// commentListLeft.appendChild(commentAuthor); // a > div
-// commentListLeft.appendChild(newComment); // span > div
-// commentbox.appendChild(commentListLeft); // div > li
-// commentbox.appendChild(heart) // heart > li
-// commentList.appendChild(commentbox); // li > ul
-// comment.value = "";
-// // article이 여러개일때? 어떻게 처리??, 배열로 인덱스넘버 이용해서 각 인덱스넘버다마다 할당?
-// 1. 아티클을 배열 형태로 받는다
-// 2. 아티클에 forEach문을 이용해서 각각 추가
-// 3. 그러면 document가 아니라 querySelector를 이용해서, comment, textarea value, Ul tag를 받고,
-// 4. 아티클 본문에다가 합쳐진 것을 추가하면 된다
-// 부모태그를 인덱스태그로 받고, 데이터를 받고 추가하는 대상을 인덱스넘버로 다시 변수설정??
-
+// get feedlist to array
 const articlelist = document.getElementsByClassName("feed");
 const article = Array.from(articlelist);
 
+// feed별 기능 구현
 article.forEach((ele) => {
+  // 댓글기능
   const ul = ele.getElementsByTagName("ul"); // get ul
   const commentBtn = ele.getElementsByClassName("commentBtn")[0]; // get buttton
   const comment = ele.getElementsByClassName("comment")[0]; // get textarea
@@ -93,5 +35,23 @@ article.forEach((ele) => {
     if (enter === 13) {
       appendComment();
     }
+  });
+
+  // - 좋아요 기능-
+  const heartIcon = ele.getElementsByClassName("heartIcon")[0];
+  const likeCount = ele.getElementsByClassName("likeCount")[0];
+  let likeCountNum = Number(likeCount.textContent);
+  let clickCount = 0;
+  heartIcon.addEventListener("click", () => {
+    clickCount++;
+    if (clickCount % 2 === 0) {
+      heartIcon.setAttribute("src", "./img/heart.png");
+      likeCountNum--;
+    }
+    if (clickCount % 2 === 1) {
+      heartIcon.setAttribute("src", "./img/redheart.png");
+      likeCountNum++;
+    }
+    likeCount.textContent = likeCountNum;
   });
 });
