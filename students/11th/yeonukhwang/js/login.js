6c3370cc2430f803e0c8d0cb294d2d9c349271c6
Yeonuk <younuk23@gmail.com>
2020-07-31 10:44:35 +0900

    main.html 'head'여는태그 두번 들어간 것 확인 삭제, html에 적용한 설정 불필요하다 판단 후 삭제, 자동완성 기능 위해 각 css파일마다 상단에 넣어놓은 변수설정들 commom.css에만 적용 후 삭제, prettier 플러그인 이용 코드 들여쓰기 및 사이 공백 포맷팅 최적화

--- students/11th/yeonukhwang/js/login.js
+++ students/11th/yeonukhwang/js/login.js
@@ -1,21 +1,18 @@
-const button = document.getElementsByClassName('loginBtn')[0];
-const id = document.getElementById('id');
-const pw = document.getElementById('password');
-const form = document.getElementsByClassName('rightLogin')[0];
+const button = document.getElementsByClassName("loginBtn")[0];
+const id = document.getElementById("id");
+const pw = document.getElementById("password");
+const form = document.getElementsByClassName("rightLogin")[0];
 
 const changeBtnStatus = () => {
   const idV = id.value;
   const pwV = pw.value;
-  if(idV.length&&pwV.length){
-    button.setAttribute('class','loginBtn-active')
+  if (idV.length && pwV.length) {
+    button.setAttribute("class", "loginBtn-active");
     button.disabled = false;
-  }else{
-    button.setAttribute('class','loginBtn');
+  } else {
+    button.setAttribute("class", "loginBtn");
     button.disabled = true;
   }
-}
+};
 
-form.addEventListener('keyup',changeBtnStatus);
-
-
-
+form.addEventListener("keyup", changeBtnStatus);
--- students/11th/yeonukhwang/js/main.js
+++ students/11th/yeonukhwang/js/main.js
@@ -1,5 +1,5 @@
 // let commentBtn = document.getElementById('commentBtn'); //get commentbtn
-// let txt = document.getElementsByClassname('comment')[0]; // get input 
+// let txt = document.getElementsByClassname('comment')[0]; // get input
 // function appendComment() {
 //   let commentbox = document.createElement('li'); //li tag make
 //   let commentListLeft = document.createElement('div');//div tag make
@@ -7,7 +7,7 @@
 //   let comment = document.createElement('span');// span tag make
 //   let heart = document.createElement('img'); // img tag make
 //   let txt = document.getElementById('comment');
-//   let commentValue = document.getElementById('comment').value; // get textarea value 
+//   let commentValue = document.getElementById('comment').value; // get textarea value
 //   let commentList = document.getElementsByClassname('commentList')[0]; // get Ul tag
 //   comment.innerHTML = `&nbsp${commentValue}`; // insert comment into span
 //   heart.setAttribute('src','/img/heart.png');
@@ -40,7 +40,7 @@
 // const commentAuthor = document.createElement('a');//a tag make
 // const newComment = document.createElement('span');// span tag make
 // const heart = document.createElement('img'); // img tag make
-// newComment.innerHTML = `&nbsp${newCommentValue}`; // 
+// newComment.innerHTML = `&nbsp${newCommentValue}`; //
 // heart.setAttribute('src','/img/heart.png');
 // heart.setAttribute('alt','like-to-comment');
 // commentAuthor.setAttribute('class', 'name')
@@ -59,66 +59,39 @@
 // 4. 아티클 본문에다가 합쳐진 것을 추가하면 된다
 // 부모태그를 인덱스태그로 받고, 데이터를 받고 추가하는 대상을 인덱스넘버로 다시 변수설정??
 
-const articlelist = document.getElementsByClassName('feed');
+const articlelist = document.getElementsByClassName("feed");
 const article = Array.from(articlelist);
 
-article.forEach((ele)=>{
-  const ul = ele.getElementsByTagName('ul'); // get ul
-  const commentBtn = ele.getElementsByClassName('commentBtn')[0]; // get buttton
-  const comment = ele.getElementsByClassName('comment')[0]; // get textarea
-  const appendComment = function() {
+article.forEach((ele) => {
+  const ul = ele.getElementsByTagName("ul"); // get ul
+  const commentBtn = ele.getElementsByClassName("commentBtn")[0]; // get buttton
+  const comment = ele.getElementsByClassName("comment")[0]; // get textarea
+  const appendComment = function () {
     let newCommentValue = comment.value; // get textarea Value
-    const commentbox = document.createElement('li'); //li tag make
-    const commentListLeft = document.createElement('div');//div tag make
-    const commentAuthor = document.createElement('a');//a tag make
-    const newComment = document.createElement('span');// span tag make
-    const heart = document.createElement('img'); // img tag make
+    const commentbox = document.createElement("li"); //li tag make
+    const commentListLeft = document.createElement("div"); //div tag make
+    const commentAuthor = document.createElement("a"); //a tag make
+    const newComment = document.createElement("span"); // span tag make
+    const heart = document.createElement("img"); // img tag make
     newComment.innerHTML = `&nbsp${newCommentValue}`;
-    heart.setAttribute('src','./img/heart.png');
-    heart.setAttribute('alt','like-to-comment');
-    commentAuthor.setAttribute('class', 'name')
-    commentListLeft.setAttribute('class','commentListLeft');
-    commentAuthor.innerHTML = 'sotonghaeyo' // make author default name, 나중에 로그인 한 아이디로 바꾸는 로직 추가하면 좋을 듯
+    heart.setAttribute("src", "./img/heart.png");
+    heart.setAttribute("alt", "like-to-comment");
+    commentAuthor.setAttribute("class", "name");
+    commentListLeft.setAttribute("class", "commentListLeft");
+    commentAuthor.innerHTML = "sotonghaeyo"; // make author default name, 나중에 로그인 한 아이디로 바꾸는 로직 추가하면 좋을 듯
     commentListLeft.appendChild(commentAuthor); // a > div
     commentListLeft.appendChild(newComment); // span > div
     commentbox.appendChild(commentListLeft); // div > li
-    commentbox.appendChild(heart) // heart > li
+    commentbox.appendChild(heart); // heart > li
     ul[0].appendChild(commentbox); // li > ul
     comment.value = ""; // reset textarea value
-  }
+  };
 
-  commentBtn.addEventListener('click',appendComment);
-  comment.addEventListener('keydown',(e) => {
-    let enter = e.keyCode? e.keyCode : e.which;
-    if(enter === 13){
+  commentBtn.addEventListener("click", appendComment);
+  comment.addEventListener("keydown", (e) => {
+    let enter = e.keyCode ? e.keyCode : e.which;
+    if (enter === 13) {
       appendComment();
     }
-  })
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  });
+});
