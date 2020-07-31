94a59bfda624fdb47a7c9394cd322aec2e632bc7
sanghunLee <cloudlee711@gmail.com>
2020-07-31 15:30:38 +0900

    Add: 로그인페이지 id,pw validation 구현 - id(@포함 5글자 이상),pw(5글자이상) 시 로그인 버튼 활성화기능 구현

--- students/11th/sanghunlee/js/login.js
+++ students/11th/sanghunlee/js/login.js
@@ -5,16 +5,22 @@ const btnColorChangeid = () => {
     const idInput = id.value;
     const pwInput = pw.value;
     const btn = document.querySelector(".loginBtn"); 
-    if(idInput.length>=1 && pwInput.length>=1){
-        btn.style.backgroundColor = "blue";
-    }
-    else
-        btn.style.backgroundColor = "#C0DFFD";
-    }
+    const makeLink = document.createElement("a");
 
+    if(idInput.length >=5 && pwInput.length >=5 && idInput.search("@") !=-1){
+        btn.style.backgroundColor = "blue";
+        btn.appendChild(makeLink);
+        btn.disabled = false;
+    }
+    else if(idInput.length>=1 && pwInput.length>=1){
+        btn.style.backgroundColor ="blue";
+        btn.disabled = true;
+    }
+    else{
+        btn.style.backgroundColor = "#C0DFFD";
+        btn.disabled = true;
+    }
+}
 id.addEventListener("keyup",btnColorChangeid);
 pw.addEventListener("keyup",btnColorChangeid);
-
-
-
-
+/*logic 완성*/
