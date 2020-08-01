6836da662b90371da5a742ce0ff8a15246863a03
codeamor <65898889+codeamor@users.noreply.github.com>
2020-08-01 12:02:31 +0900

    Update login.js

--- students/11th/suminson/login/login.js
+++ students/11th/suminson/login/login.js
@@ -1,21 +1,21 @@
 let userId = document.querySelector('.user-id');
 let userPw = document.querySelector('.user-pw');
-let btn = document.querySelector('.button-login');
+let btnLogin = document.querySelector('.button-login');
 
 userId.focus();
 
 document.addEventListener('keyup', function () {
     if (userId.value.length >= 1 && userPw.value.length >= 1) {
-        btn.disabled = "false";
-        btn.style.backgroundColor = "blue";
+        btnLogin.disabled = "";
+        btnLogin.style.backgroundColor = "blue";
     } else {
-        btn.disabled = "true";
-        btn.style.backgroundColor = "#B2DFFC"
+        btnLogin.disabled = "true";
+        btnLogin.style.backgroundColor = "#B2DFFC"
     }
 
 });
 
-btn.addEventListener('click', function (e) {
+btnLogin.addEventListener('click', function (e) {
     e.preventDefault();
 
     if (userId.value.includes("@") && userPw.value.length > 4) {
