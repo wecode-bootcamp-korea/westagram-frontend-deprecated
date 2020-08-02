219b27199ff8ba29facd55438ee655ca895e165b
Joeun Lee <jjo.niixxx@gmail.com>
2020-08-02 17:58:33 +0900

    Modify: main 포지션 수정 - feedsRight에 float속성, mainRight에 position: fixed로 수정

--- students/11th/JoeunLee/js/main.js
+++ students/11th/JoeunLee/js/main.js
@@ -8,3 +8,25 @@ function changeColor() {
         replyUploadBtn.style.color="rgba(0,149,246,1)";
     } else replyUploadBtn.style.color="rgba(0,149,246,.3)";
 }
+
+// const enterBtn = document.querySelector('button.replyUploadBtn');
+// enterBtn.addEventListener('click', appendReply);
+// function appendReply() {
+//     const divList = document.querySelector('div.replyList')
+//     const newDiv = document.createElement('div');
+//     const newId = document.createElememt('a');
+//     const newSpan = document.createElement('span');
+//     const newImg = document.createElement('img');
+//     const textareaVal = document.querySelector('textarea').value;
+//     newDiv.classList.add('reply');
+//     newDiv.appendChild(newId);
+//     newDiv.appendChild(newSpan);
+//     newSpan.textContent = textareaVal;
+//     newImg.src = 'https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png';
+//     newImg.style.width = '12px';
+//     newImg.style.height = '12px';
+//     newImg.style.position = 'absolute';
+//     newImg.style.right = '0';
+//     newDiv.appendChild(newImg);
+//     divList.appendChild('newDiv');
+// }
