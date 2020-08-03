d95b67c74be926aee98b8ab29a1ab3b537b5d59b
HwiChanYu <dbgnlcks34@gmail.com>
2020-08-04 02:32:00 +0900

    Refactor: 메인 js 파일 리팩토링 - 메인 js 파일 게스트 이름추가 - 메인 js 파일 1글자 이상 작성시 게시버튼 활성화 - html, css 파일 변수명 수정

--- students/11th/YUhwichan/js/main.js
+++ students/11th/YUhwichan/js/main.js
@@ -1,31 +1,60 @@
+// const submit = document.querySelector("#submit");
+// const commentArea = document.querySelectorAll("input")[1];
+
+// const addComment = () => {
+//   const commentVal = commentArea.value;
+//   const commentList = document.querySelector(".commentList");
+//   const commentLi = document.createElement("li");
+//   const guestName = document.createElement("div");
+//   guestName.textContent = "guest_who";
+//   const comment = document.createElement("span");
+//   comment.textContent = " " + commentVal;
+//   commentLi.append(guestName, comment);
+//   commentList.append(commentLi);
+// };
+
+// submit.addEventListener("click", addComment);
+
+// commentArea.addEventListener("keydown", function (e) {
+//   if (e.keyCode === 13) {
+//     const commentValue = commentArea.value;
+//     const commentList = document.querySelector(".commentList");
+//     const commentLi = document.createElement("li");
+//     const guestName = document.createElement("div");
+//     guestName.textContent = "guest_who";
+//     const comment = document.createElement("span");
+//     comment.textContent = " " + commentValue;
+//     commentLi.append(guestName, comment);
+//     commentList.append(commentLi);
+//   }
+// });
+// 주석 코드는 스스로 작성했고, 아래 코드는 저의 코드의 반복되는 부분을 줄이려 고민하다가 동기분의 도움을 받았습니다
+
 const submit = document.querySelector("#submit");
+const commentArea = document.querySelectorAll("input")[1];
 
-const addcomment = () => {
+const addComment = () => {
+  const commentVal = commentArea.value;
   const commentList = document.querySelector(".commentList");
-  const writeArea = document.querySelectorAll("input")[1];
-  const writeVal = writeArea.value;
+  const commentLi = document.createElement("li");
+  commentLi.innerHTML = `<div>guest_who</div><span> ${commentVal}</span>`;
+  commentList.append(commentLi);
+};
+// li 태그에 값을 채우고 append 할수도 있고, append 하고 값을 채울 수도 있다.
 
-  const LI = document.createElement("li");
-  const DIV = document.createElement("div");
-  DIV.textContent = "guest_who";
-  const SPAN = document.createElement("span");
-  SPAN.textContent = " " + writeVal;
-  LI.append(DIV, SPAN);
+submit.addEventListener("click", addComment);
 
-  commentList.append(LI);
+commentArea.addEventListener("keydown", function (e) {
+  if (e.keyCode === 13) {
+    addComment();
+  }
+});
+
+const changeColor = () => {
+  const commentVal = commentArea.value;
+  if (commentVal) {
+    submit.style.opacity = "1";
+  }
 };
 
-submit.addEventListener("click", addcomment);
-
-// const submitBtn = document.querySelector("#submit");
-
-// submitBtn.addEventListener("click", function () {
-//   const commentArea = document.querySelectorAll("input")[1];
-//   const comment = commentArea.value;
-//   const commentPlace = document.querySelector(".commentList");
-
-//   let makeUserCommentPart = document.createElement("li");
-//   makeUserCommentPart.textContent = comment;
-
-//   commentPlace.appendChild(makeUserCommentPart);
-// });
+commentArea.addEventListener("keyup", changeColor);
