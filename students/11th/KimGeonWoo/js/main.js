af1649dd7f7b10ea0db2b69ba527bede0c3dc484
김건우 <gunoo0126@gmail.com>
2020-08-03 15:50:00 +0900

    Refactor: 멘토 리뷰 반영 - 주석 제거 - tab size 2칸 수정 - 색상 hex코드로 작성 - 가독성을 고려하여 if문으로 변경 - main페이지  템플릿 리터럴 수정 적용

--- students/11th/KimGeonWoo/js/main.js
+++ students/11th/KimGeonWoo/js/main.js
@@ -4,17 +4,17 @@ const comment_section_user_text = document.querySelector('.comment_section_user_
 const search_bar_container = document.querySelector('.search_bar_container');
 
 upload_btn.addEventListener('click',function(){
-    let tag_list=document.createElement('li');
+  const tag_list=document.createElement('li');
 
-    if(input_area.value.length==0){
-        alert("댓글을 입력해주세요");
-    }else{    
-        tag_list.innerHTML=`WeCode `+`${input_area.value}`;
-        comment_section_user_text.appendChild(tag_list);
-        input_area.value="";
-    }
+	if(input_area.value.length){
+		tag_list.innerHTML=`WeCode ${input_area.value}`;
+		comment_section_user_text.appendChild(tag_list);
+		input_area.value= "";
+	}else{    
+		alert("댓글을 입력해주세요");
+	}
 })
 
 search_bar_container.addEventListener("click",function(){
-    search_bar_container.remove();
+	search_bar_container.remove();
 })
