3dc9e004bd17790e04fb58721a1efd1179925db2
Doyoonear <doyoonleee@gmail.com>
2020-08-03 16:46:43 +0900

    Finished main css

--- students/11th/doyoonlee/js/main.js
+++ students/11th/doyoonlee/js/main.js
@@ -1,14 +1,48 @@
 
 let input = document.querySelector('.comment_input'); 
 let button = document.querySelector('.comment_button');
+let ul = document.querySelector('.comments_loaded');
 
 function addComment() {
     let inputValue = input.value;
-    let ul = document.querySelector('.comments_loaded');
-    let li = document.createElement('li'); // 새로운 li element를 만들고
-    li.innerHTML = inputValue; // inputBox의 value값을 가져와서 li의 안에 텍스트로 써주기
-    ul.appendChild(li); // ul 에 넣기
+    let li = document.createElement('li'); 
+    li.innerHTML = inputValue; 
+    ul.appendChild(li); 
+    let img = document.createElement('img');
+    console.log(img);
+    img.setAttribute('src', './img/close.svg');
+    img.className = 'delete';
+    li.appendChild(img);
     input.value="";
 }
 
-button.addEventListener('click', addComment); // 인풋박스에 이벤트리슨하다가 이벤트핸들러를 실행
+button.addEventListener('click', addComment); 
+
+input.addEventListener('keyup', function(e){
+    if(e.keyCode === 13){
+        let inputValue = input.value;
+        let ul = document.querySelector('.comments_loaded');
+        let li = document.createElement('li'); 
+        li.innerHTML = inputValue; 
+        ul.appendChild(li);
+        let img = document.createElement('img');
+        img.setAttribute('src', './img/close.svg');
+        img.className = 'delete';
+        li.appendChild(img);
+        input.value="";
+    } 
+});
+
+function rmComment() {
+        let li = document.getElementsByClassName('li');
+        let img = document.getElementsByClassName('img');
+        li.removeChild(img);
+}
+
+
+img.addEventListener('click', rmComment);
+console.log(img);
+
+
+
+
