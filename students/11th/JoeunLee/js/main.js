f1881d1faf8cf405f38c719c78c00d0c16b8b372
Joeun Lee <jjo.niixxx@gmail.com>
2020-08-03 11:05:55 +0900

    Add: 메인 필수 구현 기능

--- students/11th/JoeunLee/js/main.js
+++ students/11th/JoeunLee/js/main.js
@@ -9,24 +9,27 @@ function changeColor() {
     } else replyUploadBtn.style.color="rgba(0,149,246,.3)";
 }
 
-// const enterBtn = document.querySelector('button.replyUploadBtn');
-// enterBtn.addEventListener('click', appendReply);
-// function appendReply() {
-//     const divList = document.querySelector('div.replyList')
-//     const newDiv = document.createElement('div');
-//     const newId = document.createElememt('a');
-//     const newSpan = document.createElement('span');
-//     const newImg = document.createElement('img');
-//     const textareaVal = document.querySelector('textarea').value;
-//     newDiv.classList.add('reply');
-//     newDiv.appendChild(newId);
-//     newDiv.appendChild(newSpan);
-//     newSpan.textContent = textareaVal;
-//     newImg.src = 'https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png';
-//     newImg.style.width = '12px';
-//     newImg.style.height = '12px';
-//     newImg.style.position = 'absolute';
-//     newImg.style.right = '0';
-//     newDiv.appendChild(newImg);
-//     divList.appendChild('newDiv');
-// }
+const enterBtn = document.querySelector('button.replyUploadBtn');
+enterBtn.addEventListener('click', e => {e.preventDefault();
+    const divList = document.querySelector('div.replyList')
+    const newDiv = document.createElement('div');
+    const newId = document.createElement('a');
+    const newSpan = document.createElement('span');
+    const newImg = document.createElement('img');
+    const textareaVal = document.querySelector('textarea').value;
+    newDiv.classList.add('reply');
+    newId.textContent = "seojun.park"
+    newId.style.marginRight = '2px';
+    newDiv.appendChild(newId);
+    newSpan.style.fontSize = '14px';
+    newDiv.appendChild(newSpan);
+    newSpan.textContent = textareaVal;
+    newImg.src = 'https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png';
+    newImg.style.width = '12px';
+    newImg.style.height = '12px';
+    newImg.style.position = 'absolute';
+    newImg.style.right = '0';
+    newDiv.appendChild(newImg);
+    divList.appendChild(newDiv);
+});
+
