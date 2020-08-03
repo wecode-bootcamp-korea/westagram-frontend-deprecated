1f25d8d0c026506a94a789cc508a036b5cf4932a
박주엽 <wnduq8@gmail.com>
2020-08-03 19:46:28 +0900

    Add: login page 레이아웃 및 기능 추가 - login page 레이아웃 및 Id,password 입력 시 login 버튼 활성화
    Add: main page 레이아웃 추가
    - main page 레이아웃 추가

---
+++ students/11th/juyeoppark/js/login.js
@@ -0,0 +1,16 @@
+const loginContent = document.querySelector('.login_content');
+const loginId = document.querySelector('.login_input-id');
+const loginPw = document.querySelector('.login_input-pw');
+const loginBtn = document.querySelector('.login_btn');
+
+loginContent.addEventListener('keyup', () => {
+  let idValue = loginId.value;
+  let pwValue = loginPw.value;
+  const painBtn = 'login_paintBtn';
+
+  if (idValue && pwValue) {
+    loginBtn.classList.add(painBtn);
+  } else {
+    loginBtn.classList.remove(painBtn);
+  }
+})
