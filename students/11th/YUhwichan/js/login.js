be94c040c704fe6711e86134b0cc8797b9047b2c
HwiChanYu <dbgnlcks34@gmail.com>
2020-08-03 20:32:19 +0900

    Fix: 로그인 페이지 기능구현 - id, pw 각각 1글자 이상 작성시 버튼 활성화

--- students/11th/YUhwichan/js/login.js
+++ students/11th/YUhwichan/js/login.js
@@ -1,8 +1,14 @@
-const userID = document.querySelector(".write");
-const userPW = document.querySelectorAll(".write")[1];
+const getVal = document.getElementsByTagName("form")[0];
 
-userID &&
-  userPW.addEventListener("keyup", function () {
-    const loginColor = document.querySelector("button");
-    loginColor.style.backgroundColor = "rgb(0, 149, 246)";
-  });
+const changeColor = () => {
+  const idVal = document.querySelector(".writeID").value;
+  const pwVal = document.querySelector(".writePW").value;
+  const btn = document.querySelector("button");
+  if (idVal && pwVal) {
+    btn.style.backgroundColor = "rgb(0, 149, 246)";
+  }
+};
+
+getVal.addEventListener("keyup", changeColor);
+
+// 각 두 인풋에 이벤트를 걸려면 두 인풋을 포함하고 있는 컨테이너에 에게 걸면 된다.
