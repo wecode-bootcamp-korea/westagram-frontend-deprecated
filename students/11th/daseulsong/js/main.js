1f56821558dba6fdcf834abde1cf7cb7ea447515
daseulsong <vates0328@gmail.com>
2020-08-04 16:06:35 +0900

    modify: 수정요청사항 반영

--- students/11th/daseulsong/js/main.js
+++ students/11th/daseulsong/js/main.js
@@ -1,40 +1,54 @@
 'use strict';
-
+//  유저가 코멘트 '입력하는' 위치
 const commentInputBox = document.querySelector('.comment-box');
-const newCommentBox = document.querySelector('.content__text');
-const commentContent = document.createTextNode(commentInputBox.value);
-const commentUsername = document.querySelector('.content__id');
-const commentFirstClass = document.querySelector('.info__comment');
 
+// 새로 '생성된 코멘트가 들어갈' 위치
+const newCommentBox = document.querySelector('.contentText');
+const commentUsername = document.querySelector('.contentId');
+const commentFirstClass = document.querySelector('.comment__container');
+
+// 댓글 버튼 색깔 이벤트
 commentInputBox.addEventListener('keyup', (e) => {
-  const postBtn = document.querySelector('.post-btn');
-  commentInputBox.value !== ''
-    ? (postBtn.style.color = '#2980b9')
-    : (postBtn.style.color = '#bbdefb');
+  const postBtn = document.querySelector('.postBtn');
+  if (commentInputBox.value !== '') {
+    postBtn.style.color = '#2980b9';
+  } else {
+    postBtn.style.color = '#bbdefb';
+  }
 });
 
+// 댓글 등록 이벤트
 commentInputBox.addEventListener('keypress', (e) => {
   if (e.keyCode == 13) {
     e.preventDefault();
+    // '엔터' 받으면 댓글 칸에 내용 지움
+    commentInputBox.value = e.target.value;
+
+    // code below means pushing content user types to InputBox
+    const infoComment = document.createElement('div');
+    const contentLeft = document.createElement('div');
+    const contentId = document.createElement('span');
+    const contentText = document.createElement('p');
+    const contentHeart = document.createElement('span');
+
+    infoComment.classList.add('infoComment');
+    contentLeft.classList.add('contentLeft');
+    contentId.classList.add('contentId');
+    contentText.classList.add('contentText');
+    contentHeart.classList.add('contentHeart');
+
+    // 아이디 자리에 댓글다는 유저이름 넣기
+    contentId.innerHTML = 'daseulsongme';
+    // 입력된 댓글내용을 dom으로 생성된 위치에 넣기
+    contentText.innerHTML = commentInputBox.value;
+
+    commentFirstClass.appendChild(infoComment);
+    contentLeft.appendChild(contentId);
+    infoComment.appendChild(contentLeft);
+    contentLeft.appendChild(contentText);
+    infoComment.appendChild(contentHeart);
+
     commentInputBox.value = '';
-    commentUsername.innerHTML = 'daseulsong';
-
-    const ul = document.createElement('ul');
-    const div = document.createElement('div');
-    const li = document.createElement('li');
-    const span = document.createElement('span');
-    const p = document.createElement('p');
-
-    ul.classList.add('comment__content');
-    commentFirstClass.appendChild(ul);
-    div.classList.add('content__left');
-    ul.appendChild(div);
-    div.appendChild(li);
-    span.classList.add('content__id');
-    li.appendChild(span);
-    p.classList.add('content__text');
-    div.appendChild(p);
-    div.className = 'content__left';
-    newCommentBox.appendChild(commentContent);
+    contentHeart.innerHTML = '<i class="far fa-heart"></i>';
   }
 });
