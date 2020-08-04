64d051d70b298a2c8365bf15bd69aaf661623f3d
masangwon <ssamwonn@gmail.com>
2020-08-04 14:31:51 +0900

    피드백 주신내용 모두 수정했습니다!
    -console.log ,주석,빈칸  모두 삭제
    -JS 컨벤션 변수명 camelcas형태로변경
    -태그로 감싸진않은 내용들 태그로감싸기
    -클래스명 camelCase로 변경
    -클래스명 오타 수정
    -div 태그 시멘틱태그로 수정
    -<li> 태그만썼던 부분 < ul>태그로 감싸기

--- students/11th/masangwon/js/login.js
+++ students/11th/masangwon/js/login.js
@@ -2,15 +2,12 @@ const inputUserName = document.querySelector('.inputUserName');
 const loginButton = document.querySelector('.loginButton');
 const inputPassWord = document.querySelector('.inputPassWord');
 
-
 inputPassWord.addEventListener('keyup', function () {
-    console.log(inputPassWord.value)
     if (inputUserName.value && inputPassWord.value) {
         loginButton.style.backgroundColor = 'rgb(53,149,246)';
 
     } else {
         loginButton.style.backgroundColor = 'rgb(191, 223, 253)';
     }
-
 })
 
--- students/11th/masangwon/js/main.js
+++ students/11th/masangwon/js/main.js
@@ -1,55 +1,26 @@
 const inputComment = document.querySelector(".inputComment"),
-    submitButton = document.querySelector(".submitButton");  //게시 버튼 
-let commentList = document.querySelector(".commentList"); // 댓글 받아서 저장하는 리스트
+    submitButton = document.querySelector(".submitButton");
+let commentList = document.querySelector(".commentList");
 
-
-// let text_comment;
-
-//  인풋값을 저장,삭제 기능이 담긴  함수  
 function paintList(text) {
-    const CRE_LI = document.createElement("li");
-    const USER_ID = document.createElement("span");
-    const USER_COMMENT = document.createElement("span");
-    const LIKE_IMG = document.createElement("span");
-    LIKE_IMG.innerHTML = `
+    const createList = document.createElement("li");
+    const userId = document.createElement("span");
+    const userComment = document.createElement("span");
+    const likeImg = document.createElement("span");
+    likeImg.innerHTML = `
     <img src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png" />
     `
-    LIKE_IMG.className = "LIKE_IMG"
-    USER_ID.className = "USER_NEW";
-    USER_ID.innerHTML = "ssamwonn";
-    USER_COMMENT.innerHTML = text;
-    CRE_LI.appendChild(USER_ID);
-    CRE_LI.appendChild(USER_COMMENT);
-    CRE_LI.appendChild(LIKE_IMG)
-    commentList.appendChild(CRE_LI);
-
+    likeImg.className = "likeImg"
+    userId.className = "useNew";
+    userId.innerHTML = "ssamwonn";
+    userComment.innerHTML = text;
+    createList.appendChild(userId);
+    createList.appendChild(userComment);
+    createList.appendChild(likeImg)
+    commentList.appendChild(createList);
 }
-
 submitButton.addEventListener('click', function (e) {
     paintList(inputComment.value);
     inputComment.value = '';
-
-
 })
 
-//리스트 삭제할때                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
-// delBtn.addEventListener('click', function () {
-//     const btn = event.target;
-//     const li = btn.parentNode;
-//     comment_list.remove(li);
-
-// });
-
-
-
-
-
-//인풋값을 전달 받아 이벤트 발생시키는 함수 
-
-
-
-
-
-
-
-//리스트에 추가된 내용 삭제하는 함수 
