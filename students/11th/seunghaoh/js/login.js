e9794beb194169e49a87241dd5210a88870f3f5a
오승하 <lilyoh628@gmail.com>
2020-08-04 17:33:14 +0900

    Add: 메인 페이지 내 댓글 추가 기능 구현 - 댓글 작성 후 '게시' 버튼 클릭 시 댓글 추가 기능 구현

--- students/11th/seunghaoh/js/login.js
+++ students/11th/seunghaoh/js/login.js
@@ -1,20 +1,36 @@
-const id = document.getElementsByClassName("inputId")[0];
-const pw = document.getElementsByClassName("inputPw")[0];
+// const id = document.getElementsByClassName("inputId")[0];
+// const pw = document.getElementsByClassName("inputPw")[0];
+const inputs = document.querySelectorAll(".inputs");
 const thisIsBtn = document.getElementsByClassName("loginBtn")[0];
 
-id.addEventListener("keyup", function () {
+// id.addEventListener("keyup", function () {
+//   loginBtnActive();
+// });
+
+// pw.addEventListener("keyup", function () {
+//   loginBtnActive();
+// });
+
+inputs[0].addEventListener("keyup", function () {
   loginBtnActive();
 });
 
-pw.addEventListener("keyup", function () {
+inputs[1].addEventListener("keyup", function () {
   loginBtnActive();
 });
 
 function loginBtnActive() {
-  const idVal = id.value;
-  const pwVal = pw.value;
-
-  if (idVal && pwVal) {
+  // const idVal = id.value;
+  // const pwVal = pw.value;
+  // if (idVal && pwVal) {
+  //   thisIsBtn.setAttribute("style", "opacity: 1");
+  //   thisIsBtn.disabled = false;
+  // } else {
+  //   thisIsBtn.setAttribute("style", "opacity: 0.4");
+  //   thisIsBtn.disabled = true;
+  // // }
+  // const inputsVal = inputs.value;
+  if (inputs[0].value && inputs[1].value) {
     thisIsBtn.setAttribute("style", "opacity: 1");
     thisIsBtn.disabled = false;
   } else {
