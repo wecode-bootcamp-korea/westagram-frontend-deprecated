bf02041125d871ad4b34cbde8e23264e4a096759
김영지 <yjkim7257@gmail.com>
2020-08-04 14:48:32 +0900

    Add: main 페이지 nav바 고정 -dwtail

---
+++ students/11th/youngjikim/index.js
@@ -0,0 +1,11 @@
+const thisisbutton = document.getElementById('loginButton');
+
+thisisbutton.addEventListener('keyup',function(){
+    const idinput = document.getElementById('idmail').Value;
+    const pwinput = document.getElementById('password1').value;
+    
+    
+})
+
+//var btn = document.getElementById("Button");
+// btn.disabled = false;
