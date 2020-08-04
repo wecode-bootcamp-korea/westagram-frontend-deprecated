35821c3f8c7c1575b63b6aa727566e2542591d2a
yoonminsohappy@gmail.com <yoonminsohappy@gmail.com>
2020-08-05 01:27:53 +0900

    아이디, 비밀번호 입력시 로그인 버튼 활성화 기능 구현

--- students/11th/yoonmincho/js/login.js
+++ students/11th/yoonmincho/js/login.js
@@ -1,10 +1,25 @@
-const idButtonValue = document.getElementsByClassName('input11').value
-const pwButtonValue = document.getElementsByClassName('input22').value
+const id = document.getElementsByClassName('input11')[0]
+const pwd = document.getElementsByClassName('input22')[0]
+const button = document.getElementsByClassName('loginbutton')[0]
 
-idButton.addEventListner('onkeypress', function(){
-    if ( idButtonValue.length >= 1 && pwButtonValue.length >=1  ) { 
-        return;
-    }
-  const result = document.getElementsByClassName('loginbutton').style.backgroundColor = #0095f6;
-  return result;
+id.addEventListener('keyup',function(){
+  if( id.value && pwd.value ) { 
+    button.style.backgroundColor = '#0095f6';
+    console.log('hello');
+  }
+  else {
+    button.style.backgroundColor = 'rgba(0,149,246,.3)';
+  }
 })
+
+pwd.addEventListener('keyup',function(){
+  if( id.value && pwd.value ) { 
+    button.style.backgroundColor = '#0095f6';
+    console.log('hi');
+  }
+  else {
+    button.style.backgroundColor = 'rgba(0,149,246,.3)';
+  }
+})                        
+
+
