a9c1d6fc3379db067d574dbdcf144be09e9026f9
류상욱 <ryuinkyoto@gmail.com>
2020-08-04 15:11:17 +0900

    Add: main page 기능 구현 완료

--- students/11th/SangwookRyu/js/main.js
+++ students/11th/SangwookRyu/js/main.js
@@ -5,6 +5,53 @@ btnOnOffComment.addEventListener('keyup', function() {
   const btn = document.getElementsByClassName('add-comment')[0];
 
   btn.disabled = comment.length > 0 ? false : true;
-  console.log("hello");
+});
 
+const commentBtnClick = document.getElementsByClassName('add-comment')[0];
+
+commentBtnClick.addEventListener('click', function (){
+  const commentValue = document.getElementsByClassName('write-comment')[0];
+  const commentBox = document.getElementsByClassName('comment-box')[0];
+  const commentUl = document.createElement('ul');
+  const commentTitle = document.createElement('li');
+  const commentText = document.createElement('li');
+  const btn = document.getElementsByClassName('add-comment')[0];
+
+  commentUl.className = "comment-text";
+  commentTitle.innerHTML = "ryuvVv";
+  commentTitle.className = "viewer-id";
+  commentText.innerHTML = commentValue.value;
+  commentText.className = "viewer-comment";
+  commentUl.appendChild(commentTitle);
+  commentUl.appendChild(commentText);
+  commentBox.appendChild(commentUl);
+
+  btn.disabled = commentValue.length > 0 ? false : true;
+  commentValue.value = "";
+  commentValue.focus();
+});
+
+const commentBtnEnter = document.getElementsByClassName('write-comment')[0];
+
+commentBtnEnter.addEventListener('keypress', function(e) {
+  const commentValue = document.getElementsByClassName('write-comment')[0];
+  const commentBox = document.getElementsByClassName('comment-box')[0];
+  const commentUl = document.createElement('ul');
+  const commentTitle = document.createElement('li');
+  const commentText = document.createElement('li');
+  const btn = document.getElementsByClassName('add-comment')[0];
+
+  if (e.keyCode === 13 && btn.disabled ===false) {
+  commentUl.className = "comment-text";
+  commentTitle.innerHTML = "ryuvVv";
+  commentTitle.className = "viewer-id";
+  commentText.innerHTML = commentValue.value;
+  commentText.className = "viewer-comment";
+  commentUl.appendChild(commentTitle);
+  commentUl.appendChild(commentText);
+  commentBox.appendChild(commentUl);
+
+  btn.disabled = commentValue.length > 0 ? false : true;
+  commentValue.value = "";
+  }
 });
