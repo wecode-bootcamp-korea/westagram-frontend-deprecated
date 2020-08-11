dc1ef6a56d00b4d5eef8151e5496c6741355a52b
sangkuOH <sangku824@gmail.com>
2020-08-11 20:23:05 +0900

    Refactor : main.js, login.js 수정요구사항 완료 - main.js 재할당 하지 않는 signIn => const 선언 - login.js 10번 줄 들여쓰기 수정 - main.js div.feeds 들여쓰기 수정 - main.js 34번~37번줄 if문 삼항연산자 수정

--- students/11th/snagkuoh/js/login.js
+++ students/11th/snagkuoh/js/login.js
@@ -1,4 +1,4 @@
-let signIn = document.querySelector('.loginFormWrap');
+const signIn = document.querySelector('.loginFormWrap');
 let inputId = document.querySelector('.inputId');
 let inputPw = document.querySelector('.inputPw');
 let btn = document.querySelector('.loginBoxBtn');
@@ -6,14 +6,14 @@ let btn = document.querySelector('.loginBoxBtn');
 // 버튼 제어 //
 const btnOn = () => {
     if (inputId.value && inputPw.value) {
-    btn.disabled = false; 
-    btn.classList ='btn-active';
-    btn.classList.remove = 'btn-off';
+        btn.disabled = false; 
+        btn.classList ='btnActive';
+        btn.classList.remove = 'btnOff';
     }
     else if (!inputId.value || !inputPw.value) {
-    btn.disabled = 'disabled';
-    btn.classList = 'btn-off';
-    btn.classList.remove = 'btn-active';
+        btn.disabled = 'disabled';
+        btn.classList = 'btnOff';
+        btn.classList.remove = 'btnActive';
     }
 };
 
