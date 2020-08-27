6234b407af2261bff4a88619140850f4faf71983
Youngbin Kwon <39914288+youngkwon315@users.noreply.github.com>
2020-08-27 18:09:48 +0900

    modify : login 버튼 활성화 기능 수정

--- students/12th/youngbinkwon/js/login.js
+++ students/12th/youngbinkwon/js/login.js
@@ -1,13 +1,12 @@
 const idInput = document.body.querySelector(".userId");
 const pwdInput = document.body.querySelector(".userPassword");
+const loginBtn = document.body.querySelector(".login_btn");
 
 // 로그인 버튼 색 변경 함수
 const buttonColorChange = () => {
   return idInput.value.includes("@") && pwdInput.value.length >= 5
-    ? (document.body.querySelector(".login_btn").style.backgroundColor =
-        "rgb(84,160,229)")
-    : (document.body.querySelector(".login_btn").style.backgroundColor =
-        "rgb(185,221,251)");
+    ? (loginBtn.style.backgroundColor = "rgb(84,160,229)")
+    : (loginBtn.style.backgroundColor = "rgb(185,221,251)");
 };
 
 // 요소 이벤트 리스너
