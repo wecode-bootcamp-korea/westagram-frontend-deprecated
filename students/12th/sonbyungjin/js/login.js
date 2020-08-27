c447731a1f42d673752926b1b03123cccf536e76
SonByungjin <sgyos000@gmali.com>
2020-08-27 20:04:42 +0900

    세션 수정사항 반영

--- students/12th/sonbyungjin/js/login.js
+++ students/12th/sonbyungjin/js/login.js
@@ -2,7 +2,6 @@ const thisIsLogin = document.getElementsByClassName('login__input__text');
 const thisIsButton = document.getElementsByTagName('button')[0];
 
 // 로그인 버튼 활성화(+ 유효성 검사)
-// querryselectorAll foreach 가능?
 for (const el of thisIsLogin){
     el.addEventListener('keyup', function(){
         ((thisIsLogin[0].value.indexOf('@') !== -1) &&
--- students/12th/sonbyungjin/js/main.js
+++ students/12th/sonbyungjin/js/main.js
@@ -1,18 +1,62 @@
 const thisIsInput = document.getElementById('commentInput');
 const thisIsButton = document.getElementsByTagName('button')[0];
 const thisIsUl = document.getElementsByTagName('ul')[0];
+let thisIsLike = '';
 
-// 댓글(+ 좋아요, 삭제) 입력
+
 thisIsInput.addEventListener('keypress', function(e){
     if (e.key === 'Enter' && thisIsInput.value !== ''){
         const thisIsLi = document.createElement('li');
         const commentOption = document.createElement('div');
-        const commentLike = document.createElement('span');
+        const commentLike = document.createElement('img');
         const commentRemove = document.createElement('span');
         commentLike.className = 'comment--like';
         commentRemove.className = 'comment--remove';
-        commentLike.innerHTML = '좋아요';
+        commentLike.src = 'https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png';
+        commentLike.style = 'cursor: pointer';
         commentRemove.innerHTML = '삭제';
+        commentRemove.style = "cursor: pointer";
+        commentOption.appendChild(commentLike);
+        commentOption.appendChild(commentRemove);
+        thisIsLi.innerHTML = thisIsInput.value;
+        thisIsLi.appendChild(commentOption);
+        thisIsUl.appendChild(thisIsLi);
+        thisIsInput.value = null;
+
+        thisIsLike = '';
+        thisIsLike = document.querySelectorAll('.comment--like');
+        console.log(thisIsLike);
+
+        thisIsLike.forEach((el, index) => {
+            console.log(el, index);
+
+            el.addEventListener('click', function(e){
+                console.log(e);
+
+                e.target.src = './img/heart.png';
+        })})
+
+        const thisIsRemove = document.getElementsByClassName('comment--remove');
+        for (let el of thisIsRemove){
+            el.addEventListener('click', function(){
+                el.parentElement.parentElement.remove();
+        })}
+}})
+
+
+
+thisIsButton.addEventListener('click', function(){
+    if (thisIsInput.value !== ''){
+        const thisIsLi = document.createElement('li');
+        const commentOption = document.createElement('div');
+        const commentLike = document.createElement('img');
+        const commentRemove = document.createElement('span');
+        commentLike.className = 'comment--like';
+        commentRemove.className = 'comment--remove';
+        commentLike.src = 'https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png';
+        commentLike.style = 'cursor: pointer';
+        commentRemove.innerHTML = '삭제';
+        commentRemove.style = "cursor: pointer";
         commentOption.appendChild(commentLike);
         commentOption.appendChild(commentRemove);
         thisIsLi.innerHTML = thisIsInput.value;
@@ -20,20 +64,3 @@ thisIsInput.addEventListener('keypress', function(e){
         thisIsUl.appendChild(thisIsLi);
         thisIsInput.value = null;
 }})
-thisIsButton.addEventListener('click', function(){
-    const thisIsLi = document.createElement('li');
-    const commentOption = document.createElement('div');
-    const commentLike = document.createElement('span');
-    const commentRemove = document.createElement('span');
-    commentLike.className = 'comment--like';
-    commentRemove.className = 'comment--remove';
-    commentLike.innerHTML = '좋아요';
-    commentRemove.innerHTML = '삭제';
-    commentOption.appendChild(commentLike);
-    commentOption.appendChild(commentRemove);
-    thisIsLi.innerHTML = thisIsInput.value;
-    thisIsLi.appendChild(commentOption);
-    thisIsUl.appendChild(thisIsLi);
-    thisIsInput.value = null;
-})
-
