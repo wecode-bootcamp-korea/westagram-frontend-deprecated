1931e142539874d6e91a748b53a4263ea99d0c4b
ko jae won <sis9410@gmail.com>
2020-08-28 10:52:18 +0900

    ADD: westargram Login page - HTML 구현 - CSS 구현

---
+++ students/12th/koJaeWon/js/login.js
@@ -0,0 +1,11 @@
+const identify = document.getElementsByClassName('log');
+const passW = document.getElementsByClassName('pass');
+
+document.addEventListener('keyup', function(e) {
+  if(!identify.value) {
+  } 
+  if(passW.value) {
+    const color = document.querySelector = 'button';
+    color.style.color = 'blue';
+  }
+});l
