d2eedc162c8b3dba8669224c74b16553ba4f84eb
Moon-Sangho <skym887@gmail.com>
2020-08-29 00:09:11 +0900

    * 일반 사항 및 공통 사항 - index.html, README.md 파일(불필요한 파일) 삭제 - body background-color 추가
    * 로그인 페이지
    - login.html 파일 내 태그 간 불필요한 간격 삭제
    - class name을 camel case로 변경
    - CSS 작성 순서 정리 (종류별로)
    - Body에 css 추가, reset.css 추가
    - Javascript code refactoring
    
    * 메인 페이지
    -  nav bar, main의 width 간격 동일하게 설정
    -  nav bar img tag 중 alt 값 작성 및 위치 이동
    -  css 리뷰사항 반영
    -  작업 중

--- students/12th/sanghomoon/js/login.js
+++ students/12th/sanghomoon/js/login.js
@@ -1,12 +1,12 @@
-const passwordInput = document.querySelector('.passwordInput');
+const loginForm = document.querySelector('.loginForm');
 const idInput = document.querySelector('.idInput');
+const passwordInput = document.querySelector('.passwordInput');
 const loginBtn = document.querySelector('.loginBtn');
 
 const btnActive = function() {
-    const idInputValue = document.querySelector('.idInput').value;
-    const passwordInputValue = document.querySelector('.passwordInput').value;
-
-    if (idInputValue && passwordInputValue.length > 5) {
+    const isValid = idInput.value && passwordInput.value.length > 5;
+    
+    if (isValid) {
         loginBtn.style.backgroundColor = "#0095F6";
         loginBtn.style.cursor = "pointer";
         loginBtn.removeAttribute("disabled");
@@ -17,13 +17,11 @@ const btnActive = function() {
     }
 }
 
-loginBtn.onclick = function() {
+// 준식님 git session 듣고 코드 위치를 btnActive 상단으로 올려봤으나 실행 안됐었음
+// hoisting에 대해 찾아본 결과 변수(상수) 안에 담긴 함수는 hoisting 되지 않는다고 함
+loginForm.addEventListener('keyup', btnActive);
+
+loginBtn.addEventListener('click', function() {
     location.href = "./main.html";
-}
+});
 
-function init() {
-    passwordInput.addEventListener('keyup', btnActive);
-    idInput.addEventListener('keyup', btnActive);
-}
-
-init();
---
+++ students/12th/sanghomoon/js/main.js
@@ -0,0 +1,27 @@
+// 추후 시간 남을 때 다시 시도 예정
+// const account = document.querySelectorAll('div.account');
+// const accountLengthLimit = 9;
+// const dot = "...";
+
+
+// console.log(account[1].innerHTML);
+// console.log(account.length);
+
+// for (let i = 0; i<account.length; i++) {
+//     console.log(account[i]);
+//     console.log(account[i].innerHTML);
+// }
+
+// function replaceToDot(str, len, dot) {
+//     for (let i = 0; i<str.length; i++) {
+//         const strValue = str[i].innerHTML;
+//         if (strValue.length > len) {
+//             return strValue = str.substr(0, 9) + dot;
+//         }
+//     }
+// }
+
+// replaceToDot(account, accountLengthLimit, dot);
+
+// console.log(replaceToDot(account, accountLengthLimit, dot));
+
