d6693315d10b815aa52bf2fbab0fc238c358f0f8
SonByungjin <sgyos000@gmali.com>
2020-08-28 13:12:01 +0900

    피드백 반영하였습니다

--- students/12th/sonbyungjin/js/login.js
+++ students/12th/sonbyungjin/js/login.js
@@ -1,11 +1,21 @@
-const thisIsLogin = document.getElementsByClassName('login__input__text');
-const thisIsButton = document.getElementsByTagName('button')[0];
+const loginInput = document.getElementsByClassName('login__input__text');
+const loginButton = document.getElementsByTagName('button')[0];
 
 // 로그인 버튼 활성화(+ 유효성 검사)
-for (const el of thisIsLogin){
+for (const el of loginInput){
     el.addEventListener('keyup', function(){
-        ((thisIsLogin[0].value.indexOf('@') !== -1) &&
-        (thisIsLogin[1].value.length > 4)) ?
-        thisIsButton.style.backgroundColor = '#0095f6' :
-        thisIsButton.style.backgroundColor = 'lightblue'
+        ((loginInput[0].value.indexOf('@') !== -1) &&
+        (loginInput[1].value.length > 4)) ?
+        loginButton.style.backgroundColor = '#0095f6' :
+        loginButton.style.backgroundColor = 'lightblue'
 })}
+// 하단 배너 이미지 링크(클릭이벤트)
+const advertisementImage = document.querySelectorAll('.advertisement img');
+advertisementImage[0].addEventListener('click', function(){
+    newin = window.open('about:blank');
+    newin.location.href='https://apps.apple.com/app/instagram/id389801252?vt=lo';
+})
+advertisementImage[1].addEventListener('click', function(){
+    newin = window.open('about:blank');
+    newin.location.href='https://play.google.com/store/apps/details?id=com.instagram.android&referrer=utm_source%3Dinstagramweb%26utm_campaign%3DloginPage%26ig_mid%3DC13899FA-7527-4501-8B59-C857E8CB1A00%26utm_content%3Dlo%26utm_medium%3Dbadge';
+})
--- students/12th/sonbyungjin/js/main.js
+++ students/12th/sonbyungjin/js/main.js
@@ -1,66 +1,59 @@
-const thisIsInput = document.getElementById('commentInput');
-const thisIsButton = document.getElementsByTagName('button')[0];
-const thisIsUl = document.getElementsByTagName('ul')[0];
-let thisIsLike = '';
+const commentInput = document.getElementById('commentInput');
+const commentButton = document.getElementsByTagName('button')[0];
+const commentList = document.getElementsByTagName('ul')[0];
 
-
-thisIsInput.addEventListener('keypress', function(e){
-    if (e.key === 'Enter' && thisIsInput.value !== ''){
-        const thisIsLi = document.createElement('li');
-        const commentOption = document.createElement('div');
-        const commentLike = document.createElement('img');
-        const commentRemove = document.createElement('span');
-        commentLike.className = 'comment--like';
-        commentRemove.className = 'comment--remove';
-        commentLike.src = 'https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png';
-        commentLike.style = 'cursor: pointer';
-        commentRemove.innerHTML = '삭제';
-        commentRemove.style = "cursor: pointer";
-        commentOption.appendChild(commentLike);
-        commentOption.appendChild(commentRemove);
-        thisIsLi.innerHTML = thisIsInput.value;
-        thisIsLi.appendChild(commentOption);
-        thisIsUl.appendChild(thisIsLi);
-        thisIsInput.value = null;
-
-        thisIsLike = '';
-        thisIsLike = document.querySelectorAll('.comment--like');
-        console.log(thisIsLike);
-
-        thisIsLike.forEach((el, index) => {
-            console.log(el, index);
-
-            el.addEventListener('click', function(e){
-                console.log(e);
-
-                e.target.src = './img/heart.png';
-        })})
-
+// 댓글 삽입 함수
+function appendComment(){
+    if (commentInput.value !== ''){
+        commentList.innerHTML = `
+            ${commentList.innerHTML}
+            <li>
+                ${commentInput.value}
+                <div>
+                    <img 
+                    class = 'comment--like'
+                    alt = 'heartImg'
+                    src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png"
+                    style = 'cursor:pointer'>
+                    <span class = 'comment--remove'
+                    style = 'cursor:pointer'>
+                    삭제
+                    </span>
+                </div>        
+            </li>
+        `
+        // 댓글 입력 값 초기화
+        commentInput.value = null;
+        // 댓글 삭제 기능
         const thisIsRemove = document.getElementsByClassName('comment--remove');
-        for (let el of thisIsRemove){
-            el.addEventListener('click', function(){
-                el.parentElement.parentElement.remove();
+        removeComment(thisIsRemove);
+        // 댓글 좋아요 기능
+        let thisIsLike = document.querySelectorAll('.comment--like');
+        likeComment(thisIsLike);
+}}
+
+// 댓글 삭제 함수(이벤트)
+function removeComment(parameter){
+    for (let el of parameter){
+        el.addEventListener('click', function(){
+            el.parentElement.parentElement.remove();
+        })
+    }
+}
+
+// 댓글 하트 함수(이벤트)
+function likeComment(parameter){
+    parameter.forEach( el => {
+        el.addEventListener('click', function(e){
+            e.target.src="./img/heart.png"
         })}
-}})
+    )
+}
 
-
-
-thisIsButton.addEventListener('click', function(){
-    if (thisIsInput.value !== ''){
-        const thisIsLi = document.createElement('li');
-        const commentOption = document.createElement('div');
-        const commentLike = document.createElement('img');
-        const commentRemove = document.createElement('span');
-        commentLike.className = 'comment--like';
-        commentRemove.className = 'comment--remove';
-        commentLike.src = 'https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png';
-        commentLike.style = 'cursor: pointer';
-        commentRemove.innerHTML = '삭제';
-        commentRemove.style = "cursor: pointer";
-        commentOption.appendChild(commentLike);
-        commentOption.appendChild(commentRemove);
-        thisIsLi.innerHTML = thisIsInput.value;
-        thisIsLi.appendChild(commentOption);
-        thisIsUl.appendChild(thisIsLi);
-        thisIsInput.value = null;
-}})
+// 엔터 및 클릭 이벤트
+commentInput.addEventListener('keypress', function(e){
+    if(e.key === 'Enter'){
+        appendComment();
+    }
+})
+commentButton.addEventListener('click', appendComment);
