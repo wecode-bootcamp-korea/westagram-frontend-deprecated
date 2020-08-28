2d6a3444543c202ca340fc2303e7d2e1e19b5ec9
namjun <nnamjun94@gmail.com>
2020-08-28 13:26:21 +0900

    Fix: login.css  main.css  login.html 수정 - main.html 인라인 스타일 수정, img태그 alt속성 우선순위 변경 - login.css  main.css  login.html 줄 간격 조정 - login.js main.js const , 로 선언했던 변수 줄 마다 const 사용하는 것으로 수정 - main.js innerHTML 불필요한 글자,주석 삭제 Refactor: login.js - 버튼 활성화 함수 함수 하나로 바꿈 Error: login.js line9 자기 혼자 줄 바꿈 되서 수정 불가

--- students/12th/namjunpark/js/login.js
+++ students/12th/namjunpark/js/login.js
@@ -1,60 +1,48 @@
 // imgslide show
-const articleImg = document.querySelectorAll('.image__slide');
+const articleImg = document.querySelectorAll(".image__slide");
 let slideIndex = 0;
 (function slideShow() {
+  for (let i = 0; i < articleImg.length; i++) {
+    articleImg[i].style.opacity = 0;
+  }
+  slideIndex++;
+  if (slideIndex > articleImg.length) {
+    slideIndex = 1;
+  }
 
-    for (let i = 0; i < articleImg.length; i++) {
-        articleImg[i].style.opacity = 0;
-    }
-    slideIndex++;
-    if (slideIndex > articleImg.length) { slideIndex = 1 }
-
-    articleImg[slideIndex - 1].style.opacity = 1;
-    setTimeout(slideShow, 3000);
+  articleImg[slideIndex - 1].style.opacity = 1;
+  setTimeout(slideShow, 3000);
 })();
 
 // button active
 // 로그인 버튼 활성화 함수 패스워드 문자 6개 이상 && 아이디 문자 1개 이상
-// ++ 비활성화도 포함 ++ 마우스 커서  
-const loginBtn = document.querySelector('button.login__button'),
-    loginId = document.querySelector('.loginform__input__id'),
-    loginPassword = document.querySelector('.loginform__input__password'),
-    passwordLength = loginPassword.value.length,
-    formTag = document.querySelector('#loginform');
+const formTag = document.querySelector("#loginform");
+const loginBtn = document.querySelector("button.login__button");
+const loginId = document.querySelector(".loginform__input__id");
+const loginPassword = document.querySelector(".loginform__input__password");
 
-formTag.addEventListener('keyup', e => {
-    activeButton();
-    if (loginId.value.length >= 1 && loginPassword.value.length >= 6 && e.code ===
-        "Enter") {
-        loginBtn.style.opacity = 1;
-    }
-});
-
-function activeButton() {
+(function activeButton() {
+  formTag.addEventListener("keyup", () => {
     if (loginId.value.length >= 1 && loginPassword.value.length >= 6) {
-        loginBtn.style.opacity = 1;
-        loginBtn.classList.add('btnActive');
+      loginBtn.style.opacity = 1;
+      loginBtn.classList.add("btnActive");
+    } else {
+      loginBtn.style.opacity = 0.3;
+      loginBtn.classList.remove("btnActive");
     }
-    if (!(loginId.value.length >= 1 && loginPassword.value.length >= 6)) {
-        loginBtn.style.opacity = 0.3;
-        loginBtn.classList.remove('btnActive');
-    }
-    // loginId.value.length >= 1 && loginPassword.value.length > 5 ? (loginBtn.style.opacity = 1 && loginBtn.classList.add('btnActive')) : (loginBtn.style.opacity = 0.3 && loginBtn.classList.remove('btnActive'))
-    // 위의 코드를 삼항 연산자로 바꾸는 거 진행중 
-}
+  });
+})();
 
-
-//  ID / PW validation 
-const errorText = document.createElement('div'),
-    BlockErrorMessage = document.querySelector('div.login__errorMessage');
-
-errorText.innerHTML = `<p class="error__message">잘못된 비밀번호입니다. 다시 확인하세요.</p> `;
+//  ID / PW validation
+const BlockErrorMessage = document.querySelector("div.login__errorMessage");
 
 function loginValidation() {
-    loginId.value.includes('@') === true && loginPassword.value.length > 5 ? console.log('good') : BlockErrorMessage.appendChild(errorText);
+  loginId.value.includes("@") === true && loginPassword.value.length > 5
+    ? console.log("good")
+    : (BlockErrorMessage.innerHTML = `<p class="error__message">잘못된 비밀번호입니다. 다시 확인하세요.</p>`);
 }
 
-formTag.addEventListener('submit', e => {
-    e.preventDefault();
-    loginValidation();
-})
+formTag.addEventListener("submit", (e) => {
+  e.preventDefault();
+  loginValidation();
+});
