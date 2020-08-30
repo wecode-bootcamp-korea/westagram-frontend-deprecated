4b4ae1e357bd3457003c05aba06a8cc49da9bd8e
woosukkim <olie1014.@gmail.com>
2020-08-30 21:02:41 +0900

    Add: 메인페이지 구현완료 및 댓글기능 구현 완료 - 메인페이지 html css 구현완료 - 댓글 게시 기능 (버튼 클릭 및 엔터키로 추가하는 기능) Refactor: 멘토님 조언대로 수정완료

--- students/12th/woosukkim/js/login.js
+++ students/12th/woosukkim/js/login.js
@@ -1,27 +1,27 @@
 const account = document.querySelector(".account");
 const password = document.querySelector(".password");
-const login = document.querySelector(".login");
+const loginBtn = document.querySelector(".login");
 
-function controlbutton() {
-    let passValue = password.value;
-    let accValue = account.value;
+function handleBtn() {
+    const passValue = password.value;
+    const accValue = account.value;
 
     if(accValue && passValue.length >= 6) {
-        login.style.backgroundColor = "#0095f6";
-        login.removeAttribute('disabled');
-        login.style.cursor ='pointer';
+        loginBtn.style.backgroundColor = "#0095f6";
+        loginBtn.removeAttribute('disabled');
+        loginBtn.style.cursor ='pointer';
     } 
     else {
-        login.style.backgroundColor = "#B9E0FC";
-        login.setAttribute('disabled', "");
-        login.style.cursor ='auto'; 
+        loginBtn.style.backgroundColor = "#B9E0FC";
+        loginBtn.setAttribute('disabled', "");
+        loginBtn.style.cursor ='auto'; 
     }
 }
 
 
 function init() {
-    password.addEventListener('keyup',controlbutton);
-    account.addEventListener('keyup',controlbutton);
+    password.addEventListener('keyup',handleBtn);
+    account.addEventListener('keyup',handleBtn);
 }
 
 init();
