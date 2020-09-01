272b214cc933d115245f97f608ec08e364162bd2
Juhee Jang <juheejang.dev@gmail.com>
2020-09-01 17:01:57 +0900

    Refactor : 멘토 리뷰 반영

--- students/12th/juheejang/js/login.js
+++ students/12th/juheejang/js/login.js
@@ -3,15 +3,9 @@ const userPassword = document.getElementsByClassName('userPassword')[0];
 const loginForm = document.getElementsByClassName('loginForm')[0];
 
 loginForm.addEventListener('keyup', function(event) {
-    const id = document.getElementsByClassName('userAccount')[0].value;
-    const pw = document.getElementsByClassName('userPassword')[0].value;
+    const id = userAccount.value;
+    const pw = userPassword.value;
     const loginButton = document.getElementsByClassName('loginButton')[0];
-    
-    if(id.length > 0 && pw.length > 5) {
-        loginButton.disabled = false;
-        loginButton.style.opacity = 1;
-    }else{
-        loginButton.disabled = true;
-        loginButton.style.opacity = 0.3;
-    }
+
+    (id.length > 0 && pw.length > 5) ? (loginButton.disabled = false, loginButton.style.opacity = 1) : (loginButton.disabled = true , loginButton.style.opacity = 0.3) ;
 });
