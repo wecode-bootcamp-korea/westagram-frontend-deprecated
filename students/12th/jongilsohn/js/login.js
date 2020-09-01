10663ff811b4e9147b40531d82c42d4cd80ec002
JongilSohn <sji7532@gmail.com>
2020-09-01 14:54:38 +0900

    [refactor] 리뷰에 대한 사항 전체 완료

--- students/12th/jongilsohn/js/login.js
+++ students/12th/jongilsohn/js/login.js
@@ -16,42 +16,25 @@ function handlekey() {
 }
 
 function init(){
-    let i = 0
     document.addEventListener("keyup", handlekey);      //키이벤트 발생시 핸들키함수 호출
     img_change = setInterval(function() {             //3초에 한뻔씩 호출하는 함수
-        const img1 = document.querySelector(".phone_move1")
-        const img2 = document.querySelector(".phone_move2")
-        const img3 = document.querySelector(".phone_move3")
-        const img4 = document.querySelector(".phone_move4")
-        const img5 = document.querySelector(".phone_move5")
-        if( i == 0 ) {              //첫번째 사진 출력
-            img1.classList.add('phone_img_hide')
-            img2.classList.remove('phone_img_hide')
-            i += 1
+        let img_num = document.getElementsByClassName('phone_animation')
+        const img_hide= "phone_img_hide"
+
+        for (var i = 0; i < img_num.length; i++){
+            if ( img_num[i].classList.length === 1) {
+                if (img_num[i].nextElementSibling){
+                    img_num[i].nextElementSibling.classList.remove(img_hide)
+                    img_num[i].classList.add(img_hide)
+                }
+                else {
+                    img_num[0].nextElementSibling.classList.remove(img_hide)
+                    img_num[i].classList.add(img_hide)
+                }
+                return
+            }
         }
-        else if (i == 1) {              //두번째 사진 출력
-            img2.classList.add('phone_img_hide')
-            img3.classList.remove('phone_img_hide')
-            i += 1
-        }
-        else if (i == 2) {              //세번째 사진 출력
-            img3.classList.add('phone_img_hide')
-            img4.classList.remove('phone_img_hide')
-            i += 1
-        }
-        else if (i == 3) {              //네번째 사진 출력
-            img4.classList.add('phone_img_hide')
-            img5.classList.remove('phone_img_hide')
-            i += 1
-        }
-        else if ( i == 4) {              //다섯번째 사진 출력
-            img5.classList.add('phone_img_hide')
-            img1.classList.remove('phone_img_hide')
-            i += 1
-        }
-        else {
-            i=0
-        }
-     }, 3000, i);              //셋인터벌 3초로 지정
+     }, 3000);              //셋인터벌 3초로 지정
     }
 init()
+
