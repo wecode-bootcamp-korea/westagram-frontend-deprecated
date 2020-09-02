4f060cc1d5dae5be9c38c1f0a777476691828cea
Heungsoo Lee <enjoywater@Heungsooui-MacBookPro.local>
2020-09-02 21:05:05 +0900

    [12기 이흥수] Refactor: 추가 수정사항 수정 - 삼항연산자 코드축소 - css position이 적용된 곳  속성 추가

--- students/12th/heungsoolee/js/login.js
+++ students/12th/heungsoolee/js/login.js
@@ -5,11 +5,11 @@ const loginBtn = document.getElementsByClassName("login_button")[0];
 const slideImg = document.getElementsByClassName("slide_img");
 
 const changeLoginBtn = () => {
-  inputPassword.value.length > 0 && inputId.value.length > 0
-    ? ((loginBtn.style.backgroundColor = "#0095F6"),
-      (loginBtn.style.cursor = "pointer"))
-    : ((loginBtn.style.backgroundColor = "#C0DFFD"),
-      (loginBtn.style.cursor = ""));
+  const valueLength =
+    inputPassword.value.length > 0 && inputId.value.length > 0;
+
+  loginBtn.style.backgroundColor = valueLength ? "#0095F6" : "#C0DFFD";
+  loginBtn.style.cursor = valueLength ? "pointer" : "";
 };
 
 inputId.addEventListener("keyup", changeLoginBtn);
@@ -21,6 +21,7 @@ const slideShow = () => {
   if (nowImg) {
     nowImg.classList.remove("slide_now");
     const nextImg = nowImg.nextElementSibling;
+
     if (nextImg) return nextImg.classList.add("slide_now");
     if (!nextImg) return slideImg[0].classList.add("slide_now");
   }
