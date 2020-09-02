4a4bcd5faa248ac37a78fb0b412b19ddf6a71015
JongilSohn <sji7532@gmail.com>
2020-09-02 16:26:13 +0900

    [refactor] 멘토님 리뷰 반영 수정

--- students/12th/jongilsohn/js/login.js
+++ students/12th/jongilsohn/js/login.js
@@ -1,36 +1,35 @@
 function handlekey() {
-    const user_id = document.querySelector(".login_id").value
-    const user_password = document.querySelector(".login_password").value
-    const login_button = document.querySelector(".btn_change")
+    const idValue = document.querySelector(".login_id").value
+    const pwValue = document.querySelector(".login_password").value
+    const loginBtn = document.querySelector(".btn_change")
 
-    if(user_id.length){
-        if(user_password.length != 0){
-            login_button.classList.add('login_btn_change')
-            login_button.classList.remove('login_btn')
+    if(idValue && pwValue){
+        loginBtn.classList.add('login_btn_change')
+        loginBtn.classList.remove('login_btn')
         }
-        else if(user_password.length === 0){
-            login_button.classList.remove('login_btn_change')
-            login_button.classList.add('login_btn')
+        else {
+            loginBtn.classList.remove('login_btn_change')
+            loginBtn.classList.add('login_btn')
         }
     }
-}
+
 
 function init(){
     document.addEventListener("keyup", handlekey);      //키이벤트 발생시 핸들키함수 호출
     
-    img_change = setInterval(function() {             //3초에 한뻔씩 호출하는 함수
-        let img_num = document.getElementsByClassName('phone_animation')
-        const img_hide= "phone_img_hide"
+    imgChange = setInterval(function() {             //3초에 한뻔씩 호출하는 함수
+        let imgNum = document.getElementsByClassName('phone_animation')
+        const imgHide= "phone_img_hide"
 
-        for (let i = 0; i < img_num.length; i++){
-            if ( img_num[i].classList.length === 1) {
-                if (img_num[i].nextElementSibling){
-                    img_num[i].nextElementSibling.classList.remove(img_hide)
-                    img_num[i].classList.add(img_hide)
+        for (let i = 0; i < imgNum.length; i++){
+            if ( imgNum[i].classList.length === 1) {
+                if (imgNum[i].nextElementSibling){
+                    imgNum[i].nextElementSibling.classList.remove(imgHide)
+                    imgNum[i].classList.add(imgHide)
                 }
                 else {
-                    img_num[0].nextElementSibling.classList.remove(img_hide)
-                    img_num[i].classList.add(img_hide)
+                    imgNum[0].nextElementSibling.classList.remove(imgHide)
+                    imgNum[i].classList.add(imgHide)
                 }
                 return
             }
