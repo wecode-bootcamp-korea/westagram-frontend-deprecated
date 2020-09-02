fafdef2cd742af43da15f35020595cbf60428c50
Jieun <zieun.na@gmail.com>
2020-09-02 15:41:08 +0900

    Refactor: 멘토리뷰반영

--- students/12th/najieun/westagram/js/login.js
+++ students/12th/najieun/westagram/js/login.js
@@ -1,17 +1,27 @@
 const button = document.getElementsByClassName('loginBtn')[0];
-const input = document.getElementsByClassName('input')[0];
 const name = document.getElementsByClassName("inputName")[0];
 const pw = document.getElementsByClassName("inputPw")[0];
 
 function changeColor() {
-    if(name.value && pw.value) {
+    const nameValue = name.value;
+    const pwValue = pw.value;
+
+    if (nameValue && pwValue.length >= 6) {
         button.removeAttribute('disabled');
-        button.style.backgroundColor='#0095F6';
-    } 
+        button.style.backgroundColor = '#0095F6';
+    }
+    else {
+        button.setAttribute("disabled", "")
+        button.style.backgroundColor = '#B9E0FC';
+    }
+
 }
 
-input.addEventListener('keyup', changeColor);
+name.addEventListener('keyup', changeColor);
+pw.addEventListener('keyup', changeColor);
 
-button.onclick=function() {
-    window.location.href="../main.html";
-}
+
+
+button.addEventListener('click', function () {
+    window.location.href = "../main.html";
+})
