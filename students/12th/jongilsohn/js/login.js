c2ffc855137cde72f21c9df1dd3fa782c02eb8f4
JongilSohn <sji7532@gmail.com>
2020-09-02 10:40:55 +0900

    [refactor] 리뷰 반영 사항 추가하여 수정 완료

--- students/12th/jongilsohn/js/login.js
+++ students/12th/jongilsohn/js/login.js
@@ -2,8 +2,8 @@ function handlekey() {
     const user_id = document.querySelector(".login_id").value
     const user_password = document.querySelector(".login_password").value
     const login_button = document.querySelector(".btn_change")
-    if(user_id.length > 0){
-        console.log(user_id.value)
+
+    if(user_id.length){
         if(user_password.length != 0){
             login_button.classList.add('login_btn_change')
             login_button.classList.remove('login_btn')
@@ -17,11 +17,12 @@ function handlekey() {
 
 function init(){
     document.addEventListener("keyup", handlekey);      //키이벤트 발생시 핸들키함수 호출
+    
     img_change = setInterval(function() {             //3초에 한뻔씩 호출하는 함수
         let img_num = document.getElementsByClassName('phone_animation')
         const img_hide= "phone_img_hide"
 
-        for (var i = 0; i < img_num.length; i++){
+        for (let i = 0; i < img_num.length; i++){
             if ( img_num[i].classList.length === 1) {
                 if (img_num[i].nextElementSibling){
                     img_num[i].nextElementSibling.classList.remove(img_hide)
