e464a24d7a9a121e617920a140383b1a29f25b40
realryankim <ryankimofficial@gmail.com>
2020-09-03 13:56:37 +0900

    [12기 김상준] 로그인 페이지 레이아웃/로그인 활성화 구현

--- students/12th/sangjunkim/js/login.js
+++ students/12th/sangjunkim/js/login.js
@@ -1,1 +1,11 @@
 "use strict";
+
+const thisIsId = document.getElementsByClassName("idPasswordForm")[0];
+const thisIsPw = document.getElementsByClassName("idPasswordForm")[1];
+const thisIsBtn = document.getElementsByClassName("loginBtn")[0];
+
+document.addEventListener("keyup", function () {
+  if (thisIsId.value.length > 0 && thisIsPw.value.length > 0) {
+    thisIsBtn.style.backgroundColor = "#0295F6";
+  }
+});
