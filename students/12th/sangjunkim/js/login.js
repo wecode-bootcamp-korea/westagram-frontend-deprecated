f3b590aede20dee897929ca9276606f2752b3e68
realryankim <ryankimofficial@gmail.com>
2020-09-05 22:57:51 +0900

    멘토리뷰반영

--- students/12th/sangjunkim/js/login.js
+++ students/12th/sangjunkim/js/login.js
@@ -2,13 +2,13 @@
 
 const idInput = document.getElementsByClassName("idPasswordForm")[0];
 const pwInput = document.getElementsByClassName("idPasswordForm")[1];
-const btnInput = document.getElementsByClassName("loginBtn")[0];
-const btnColor = document.getElementsByClassName("loginInput")[0];
+const btnInput = document.getElementsByClassName("loginInput")[0];
+const btnColor = document.getElementsByClassName("loginBtn")[0];
 
-btnColor.addEventListener("keyup", function () {
+btnInput.addEventListener("keyup", function () {
   if (idInput.value && pwInput.value) {
-    btnInput.style.backgroundColor = "#0295F6";
-  } else if (idInput.value || pwInput.value === 0) {
-    btnInput.style.backgroundColor = "";
+    btnColor.style.backgroundColor = "#0295F6";
+  } else {
+    btnColor.style.backgroundColor = "#C1DFFC";
   }
 });
