a63c410a18ea52ba70607604a55256886baa1507
suyeon-DV <suyeon.dv@gmail.com>
2020-09-24 16:28:43 +0900

    Add: git 초기 세팅

---
+++ students/13th/seosuyeon/js/login.js
@@ -0,0 +1,20 @@
+// Valiables
+const inputId = document.getElementById('id');
+const inputPw = document.getElementById('pw');
+
+// Events
+inputPw.addEventListener("input", changeLoginBtnColor);
+inputId.addEventListener("input", changeLoginBtnColor);
+
+// Functions
+function changeLoginBtnColor() {
+    const loginBtn = document.getElementById('login_btn');
+    let inputIdValue = document.getElementById('id').value;
+    let inputPwValue = document.getElementById('pw').value;
+
+    if (inputIdValue.length && inputPwValue.length > 5) {
+        loginBtn.style.backgroundColor = '#0095f6';
+    } else {
+        loginBtn.style.backgroundColor = '#b2dffc';
+    }
+}
