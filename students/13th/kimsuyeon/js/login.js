fabafa53cafc5cd80a68dfd7036b45e85582a3e4
Link717 <ksy95433@gmail.com>
2020-09-25 22:10:45 +0900

    Refactor - 코드 리팩토링(멘토 리뷰 반영)

--- students/13th/kimsuyeon/js/login.js
+++ students/13th/kimsuyeon/js/login.js
@@ -1,27 +1,22 @@
 "use strict";
 
-const userid_location = document.getElementById("login").children[0];
-const userpw_location = document.getElementById("login").children[1];
+const userId = document.getElementsByTagName("input")[0];
+const userPw = document.getElementsByTagName("input")[1];
+const loginBtn = document.getElementsByTagName("button")[0];
+
+userId.addEventListener("keyup", function (event) {
+  const isVaild = userId.value.length > 0 && userPw.value.length > 0;
+  if (isVaild) {
+    loginBtn.style.backgroundColor = "#0095F6";
+  } else loginBtn.style.backgroundColor = "#b2dffc";
+});
+
+userPw.addEventListener("keyup", function (event) {
+  const isVaild = userId.value.length > 0 && userPw.value.length > 0;
+  if (isVaild) {
+    loginBtn.style.backgroundColor = "#0095F6";
+  } else loginBtn.style.backgroundColor = "#b2dffc";
+});
 
 /*keydown, keypress event는 다음 key가 눌려야 그 직전 event가 종료된 것으로 인식하는듯하다.
-keydown, keypress를 사용할 경우, 예상보다 key를 한번 더 눌러야 배경색이 변경됨*/
-
-userid_location.addEventListener("keyup", function (e) {
-  const userid = document.getElementById("login").children[0].value;
-  const userpw = document.getElementById("login").children[1].value;
-  const loginbtn = document.getElementById("login").children[2];
-
-  if (userid.length > 0 && userpw.length > 0) {
-    loginbtn.style.backgroundColor = "#0095F6";
-  } else loginbtn.style.backgroundColor = "#b2dffc";
-});
-
-userpw_location.addEventListener("keyup", function (event) {
-  const userid = document.getElementById("login").children[0].value;
-  const userpw = document.getElementById("login").children[1].value;
-  const loginbtn = document.getElementById("login").children[2];
-
-  if (userid.length > 0 && userpw.length > 0) {
-    loginbtn.style.backgroundColor = "#0095F6";
-  } else loginbtn.style.backgroundColor = "#b2dffc";
-});
+keydown/keypress를 사용할 경우, 예상보다 key를 한번 더 눌러야 배경색이 변경된다.*/
--- students/13th/kimsuyeon/js/main.js
+++ students/13th/kimsuyeon/js/main.js
@@ -5,12 +5,14 @@ const thisIsReplyButton = document.getElementsByTagName("button");
 // thisIsReplyButton.addEventListener("click", function (post) {
 //   const comment = document.getElementsByTagName("input")[1].value;
 
-//   if(comment.length > 0;) {
-//     const replyInputLocation = document.getElementsByClassName('reply_contents');
+//   if (comment.length > 0) {
+//     const replyInputLocation = document.getElementsByClassName(
+//       "reply_content"
+//     )[0];
 
-//     let createReply = replyInputLocation.createElement('p');
-//     createReply.className = 'reply_user_comment';
-//     createReply.innerHTML = 'comment';
-//     document.body.
+//     let createReply = document.createElement("p");
+//     createReply.className = "reply_user_comment";
+//     createReply.innerHTML = comment;
+//     document.body.appendChild(replyInputLocation);
 //   }
 // });
