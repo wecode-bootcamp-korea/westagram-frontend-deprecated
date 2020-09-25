5fbdb6b29fec99d0a0621787223d7487160ec742
Link717 <ksy95433@gmail.com>
2020-09-25 11:05:30 +0900

    Add: login page, main page 작업 내용 추가

---
+++ students/13th/kimsuyeon/js/main.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const thisIsReplyButton = document.getElementsByTagName("button");
+
+// thisIsReplyButton.addEventListener("click", function (post) {
+//   const comment = document.getElementsByTagName("input")[1].value;
+
+//   if(comment.length > 0;) {
+//     const replyInputLocation = document.getElementsByClassName('reply_contents');
+
+//     let createReply = replyInputLocation.createElement('p');
+//     createReply.className = 'reply_user_comment';
+//     createReply.innerHTML = 'comment';
+//     document.body.
+//   }
+// });
