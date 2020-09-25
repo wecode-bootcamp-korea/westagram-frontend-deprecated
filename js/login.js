b9ffc5ed8ecee138a9d78ff24c95970d93544ec7
lemongucci <developerdawnnn@gmail.com>
2020-09-25 16:25:00 +0900

    Remove: 불필요 컴포넌트 삭제

--- js/login.js
+++ js/login.js
@@ -1,27 +1,11 @@
-//log-in function
-//Get the elements that you will add an event
 let user = document.getElementById('user');
 let pswd = document.getElementById('pswd');
 let bttn = document.getElementById('login');
 
-//Input Event for both, ID and Password
-
-//Activation of the button
-/*
-function btnActive () {
-    bttn.style = 'opacity: 1';
-    bttn[disabled] = false;
-};
-*/
-
-//condition ? exprIfTrue : exprIfFalse
-/*
-
-user.addEventListener("change", function(event) {
-  if (event) {
-    bttn.disabled = false;
-  } else {
-    bttn.disabled = true;
-  }
-});
-*/
+pswd.addEventListener('input', function(e){
+  if(e !== '') {
+      bttn.disabled = false;
+      bttn.style.opacity = '1';
+      bttn.style.cursor = 'pointer';
+  };
+})
