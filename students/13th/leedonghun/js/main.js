dde8617670511bb49bbe50c6717570af4fccfcb4
Peter D Lee <dhlee9111@gmail.com>
2020-09-27 04:05:29 +0900

    modified the icon color change functions into a single 'changeIconColor' function to be used for all icon classes

--- students/13th/leedonghun/js/main.js
+++ students/13th/leedonghun/js/main.js
@@ -16,102 +16,49 @@ function activatePostButton(e) {
 const postCommentBox = document.querySelector('.post-comment-box');
 postCommentBox.addEventListener('input', activatePostButton);
 
-
-function isLiked(e) {
-  if (e.target.getAttribute('src') == 'img/main/nav_menu_heart_icon.png') {
-    return false;
-  } else if (e.target.getAttribute('src') == 'img/main/onclick_icons/nav_menu_heart_icon_red.png') {
-    return true;
-  }
-}
-
-function isIconEnlarged(e) {
-  const iconWidth = e.target.style.width;
-  if (parseInt(iconWidth.substr(0,2)) <= 120) {
-    return false;
-  } else {
-    return true;
-  }
-}
-
-function changeHeartColor(e) {
-  console.log(e.target.getAttribute('src'));
-  if (!isLiked(e)) {
-    e.target.setAttribute('src', 'img/main/onclick_icons/nav_menu_heart_icon_red.png');
-    e.target.style.width = '120%';
-  } else {
-    e.target.setAttribute('src', 'img/main/nav_menu_heart_icon.png');
-    e.target.style.width = '100%';
-  }
-}
-
 const feedElement = document.querySelector('.feed-element');
 const feedActionHeartIcon = feedElement.querySelectorAll('.feed-action-heart');
-
-if (feedActionHeartIcon.length > 0) {
-  feedActionHeartIcon.forEach(el => el.addEventListener('click', changeHeartColor));
-}
-
-
 const feedCommentHeartIcon = feedElement.querySelectorAll('.feed-comment-heart');
-if (feedCommentHeartIcon.length > 0) {
-  feedCommentHeartIcon.forEach(el => el.addEventListener('click', changeHeartColor));
-}
+const feedActionTagIcon = feedElement.querySelectorAll('.feed-action-tag');
 
+let iconImgSrcOriginal = {};
+feedActionHeartIcon.forEach(el => iconImgSrcOriginal[el.className] = 'img/main/nav_menu_heart_icon.png');
+feedCommentHeartIcon.forEach(el => iconImgSrcOriginal[el.className] = 'img/main/nav_menu_heart_icon.png');
+feedActionTagIcon.forEach(el => iconImgSrcOriginal[el.className] = 'img/main/nav_menu_tag_icon.png');
 
-function isTagSelected(e) {
-  if (e.target.getAttribute('src') == 'img/main/nav_menu_tag_icon.png') {
-    return false;
-  } else if (e.target.getAttribute('src') == 'img/main/onclick_icons/nav_menu_tag_icon_black.png') {
-    return true;
-  }
-}
-
-function changeTagColor(e) {
-  console.log(e.target.getAttribute('src'));
-  if (!isTagSelected(e)) {
-    e.target.setAttribute('src', 'img/main/onclick_icons/nav_menu_tag_icon_black.png');
-    e.target.style.width = '120%';
-  } else {
-    e.target.setAttribute('src', 'img/main/nav_menu_tag_icon.png');
-    e.target.style.width = '100%';
-  }
-}
-
-const iconOriginal = {
-  'heart': {
-    'img-src': ''
-  }
-}
-
-const iconAfterSelect = {
-  'img-src': ''
-}
+let iconImgSrcAfterSelect = {};
+feedActionHeartIcon.forEach(el => iconImgSrcAfterSelect[el.className] = 'img/main/onclick_icons/nav_menu_heart_icon_red.png');
+feedCommentHeartIcon.forEach(el => iconImgSrcAfterSelect[el.className] = 'img/main/onclick_icons/nav_menu_heart_icon_red.png');
+feedActionTagIcon.forEach(el => iconImgSrcAfterSelect[el.className] = 'img/main/onclick_icons/nav_menu_tag_icon_black.png');
 
 function isIconSelected(e) {
-  if (e.target.getAttribute('src') == iconOriginal['img-src']) {
+  if (e.target.getAttribute('src') === iconImgSrcOriginal[e.target.className]) {
     return false;
-  } else if (e.target.getAttribute('src') == iconAfterSelect['img-src']) {
+  } else if (e.target.getAttribute('src') === iconImgSrcAfterSelect[e.target.className]) {
     return true;
   }
 }
 
 function changeIconColor(e) {
   if (!isIconSelected(e)) {
-    e.target.setAttribute('src', iconAfterSelect(e));
+    e.target.setAttribute('src', iconImgSrcAfterSelect[e.target.className]);
+    console.log(iconImgSrcAfterSelect[e.target.className]);
     e.target.style.width = '120%';
   } else {
-    e.target.setAttribute('src', iconOriginal(e));
+    e.target.setAttribute('src', iconImgSrcOriginal[e.target.className]);
+    console.log(iconImgSrcOriginal[e.target.className]);
     e.target.style.width = '100%';
   }
 }
 
-// const feedActionTagIcon = feedElement.querySelectorAll('.feed-action-tag');
-// if (feedActionTagIcon.length > 0) {
-//   feedActionTagIcon.forEach(el => el.addEventListener('click', changeTagColor));
-// }
+if (feedActionHeartIcon.length > 0) {
+  feedActionHeartIcon.forEach(el => el.addEventListener('click', changeIconColor));
+}
 
-const feedActionTagIcon = feedElement.querySelectorAll('.feed-action-tag');
+if (feedCommentHeartIcon.length > 0) {
+  feedCommentHeartIcon.forEach(el => el.addEventListener('click', changeIconColor));
+}
+
 if (feedActionTagIcon.length > 0) {
-  feedActionTagIcon.forEach(el => el.addEventListener('click', changeTagColor));
+  feedActionTagIcon.forEach(el => el.addEventListener('click', changeIconColor));
 }
