1aae2e1d8d83767e6bbe92b5d1d2de4eba7ae530
deokyeong1020@gmail.com <deokyeong1020@gmail.com>
2020-09-26 18:05:28 +0900

    innerHTML을 활용해서 불필요한 코드의 양을 줄일 수 있었습니다.

--- students/13th/deokhyeong/js/main.js
+++ students/13th/deokhyeong/js/main.js
@@ -1,72 +1,25 @@
 "use strict";
 
-const commentInputBtn = document.getElementsByClassName("comment_upload")[0];
+// 댓글을 붙이는(post)라는 의미로 변수명 변경
+const commentPostBtn = document.getElementsByClassName("comment_upload")[0];
 // let blackHeartList = document.getElementsByClassName("blackheart")[0];
 // let redHeartList = document.getElementsByClassName("redheart")[0];
 // let likeBtns;
 // let likeBtnsLength;
 
 function commentFun() {
-  const commentText = document.getElementsByClassName("comment_input")[0].value;
+  const commentText = document.querySelector(".comment_input").value;
   if (commentText === "") {
     return;
   }
-  const commentsContainer = document.getElementsByClassName("comments")[0];
-  const commentNewListTag = document.createElement("li");
-  commentNewListTag.className = "new_comment_list";
-  const commentListNewIdTag = document.createElement("a");
-  commentListNewIdTag.className = "new_comment_id";
-  const commentListNewContentTag = document.createElement("span");
-  commentListNewContentTag.className = "new_comment_content";
-  // const commentsLikeBtn = document.createElement("button");
-  // commentsLikeBtn.className = "likeBtn";
-  // const blackBtnImg = document.createElement("img");
-  // blackBtnImg.className = "blackheart";
-  // const redBtnImg = document.createElement("img");
-  // redBtnImg.className = "redheart";
+  const newCommentList = document.createElement("li");
+  newCommentList.innerHTML = `<a>heodeok<a/><span> ${commentText}</span>`;
+  document.querySelector(".comments_container").appendChild(newCommentList);
 
-  commentsContainer.appendChild(commentNewListTag);
-
-  let commentNumbers = document.getElementsByClassName("new_comment_list")
-    .length;
-
-  document
-    .getElementsByClassName("new_comment_list")
-    [commentNumbers - 1].appendChild(commentListNewIdTag);
-  document
-    .getElementsByClassName("new_comment_list")
-    [commentNumbers - 1].appendChild(commentListNewContentTag);
-  // document
-  //   .getElementsByClassName("new_comment_list")
-  //   [commentNumbers - 1].appendChild(commentsLikeBtn);
-
-  // document
-  //   .getElementsByClassName("likeBtn")
-  //   [commentNumbers - 1].appendChild(blackBtnImg);
-  // document.getElementsByClassName("blackheart")[commentNumbers - 1].src =
-  //   "./deokhyeong/img/blackheart.png";
-  // document
-  //   .getElementsByClassName("likeBtn")
-  //   [commentNumbers - 1].appendChild(redBtnImg);
-  // document.getElementsByClassName("redheart")[commentNumbers - 1].src =
-  //   "./deokhyeong/img/redheart.png";
-
-  document.getElementsByClassName("new_comment_id")[
-    commentNumbers - 1
-  ].innerHTML = "heddeok";
-  document.getElementsByClassName("new_comment_content")[
-    commentNumbers - 1
-  ].innerHTML = ` ${commentText}`;
-
-  // likeBtns = document.querySelectorAll(".likeBtn");
-  // blackHeartList = document.getElementsByClassName("blackheart");
-  // redHeartList = document.getElementsByClassName("redheart");
-  // likeBtnsLength = likeBtns.length;
   document.getElementsByClassName("comment_input")[0].value = "";
-  // play();
 }
 
-commentInputBtn.addEventListener("click", function () {
+commentPostBtn.addEventListener("click", function () {
   commentFun();
 });
 
