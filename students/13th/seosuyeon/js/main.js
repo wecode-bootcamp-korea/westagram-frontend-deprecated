d93545931bbede28ba606dd911df43f713c5576b
suyeon <66218824+suyeon-DV@users.noreply.github.com>
2020-09-27 15:33:51 +0900

    Add: 댓글 달기 기능 추가 - input에 댓글 작성 후 버튼 클릭시 댓글 추가 기능 구현

--- students/13th/seosuyeon/js/main.js
+++ students/13th/seosuyeon/js/main.js
@@ -1,5 +1,24 @@
 // Variables
+const commentInput = document.querySelector('#comment');
+const commentBtn = document.querySelector('.commentbtn');
+const commentList = document.querySelector('#comments_list');
 
 // Events
+commentBtn.addEventListener('click', addComment);
 
 // Functions
+function addComment() {
+    const commentText = commentInput.value;
+    const createLi = document.createElement('li');
+
+    createLi.innerHTML = commentText;
+    createLi.className = 'comments_comment';
+    commentList.appendChild(createLi);
+};
+/*
+입력 => value값
+li 생성, 
+value값은 li로
+class name > comments_comment (스타일 적용)
+li를 ul로 붙인다
+*/
