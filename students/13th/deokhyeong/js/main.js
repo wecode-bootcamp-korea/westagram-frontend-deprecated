fd08a190d32670916b594ae4eb6a319dd4a6c868
deokyeong1020@gmail.com <deokyeong1020@gmail.com>
2020-09-27 14:45:27 +0900

    for문을 이용한  del버튼을 각각의 버튼 생성시 이벤트를 담든 방법으로 바꿈

--- students/13th/deokhyeong/js/main.js
+++ students/13th/deokhyeong/js/main.js
@@ -2,18 +2,18 @@
 
 const commentPostBtn = document.getElementsByClassName("comment_upload")[0];
 const commentsContainer = document.querySelector(".comments_container");
-let likeBtnEvent;
-let delBtnEvent;
-let newAddCommentLists;
-let commentDelBtn;
 
 function commentAdd() {
   const commentText = document.querySelector(".comment_input").value;
   if (commentText === "") {
     return;
   }
+
   const newCommentList = document.createElement("li");
+  newCommentList.className = "deleteTarget";
   const likeBtn = document.createElement("button");
+  const delBtn = document.createElement("button");
+
   likeBtn.className = "good_Btn";
   likeBtn.innerHTML = "좋아요";
   likeBtn.addEventListener("click", () => {
@@ -26,16 +26,19 @@ function commentAdd() {
         break;
     }
   });
-  newCommentList.classList.add("newAddCommentList");
-  newCommentList.innerHTML = `<a>heodeok</a><span> ${commentText}</span><div style="display : inline-block;"><button class="delete">삭제</button></div>`;
-  newCommentList.appendChild(likeBtn);
-  commentsContainer.appendChild(newCommentList);
-  commentDelBtn = document.querySelectorAll(".delete");
-  newAddCommentLists = document.querySelectorAll(".newAddCommentList");
-  document.getElementsByClassName("comment_input")[0].value = "";
-  console.log(commentDelBtn);
 
-  delBtnEvent();
+  delBtn.className = "del_Btn";
+  delBtn.innerHTML = "삭제";
+  delBtn.addEventListener("click", () => {
+    newCommentList.remove();
+  });
+
+  newCommentList.innerHTML = `<a>heodeok</a><span> ${commentText}</span><div style="display : inline-block;">`;
+  newCommentList.appendChild(likeBtn);
+  newCommentList.appendChild(delBtn);
+  commentsContainer.appendChild(newCommentList);
+
+  document.getElementsByClassName("comment_input")[0].value = "";
 }
 
 commentPostBtn.addEventListener("click", function () {
@@ -49,11 +52,3 @@ document
       commentAdd();
     }
   });
-
-delBtnEvent = () => {
-  for (let i = 0; i < commentDelBtn.length; i++) {
-    commentDelBtn[i].addEventListener("click", () => {
-      newAddCommentLists[i].remove();
-    });
-  }
-};
