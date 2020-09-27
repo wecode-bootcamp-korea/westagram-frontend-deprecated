76c78c4d8df8ac5eedd9cb45e9092b807983a1f7
Peter D Lee <dhlee9111@gmail.com>
2020-09-27 20:31:14 +0900

    Refactor: moved 'idBox' and 'pwBox' variable declarations to above the function declaration that uses them. Deleted unnecessary codes that were commented out temporarily.

--- students/13th/leedonghun/js/login.js
+++ students/13th/leedonghun/js/login.js
@@ -1,3 +1,5 @@
+const idBox = document.querySelector('.username-box');
+const pwBox = document.querySelector('.pw-box');
 
 function activateLoginButton() {
   const loginButton = document.querySelector('.login-button');
@@ -11,37 +13,8 @@ function activateLoginButton() {
   }
 }
 
-const idBox = document.querySelector('.username-box');
-const pwBox = document.querySelector('.pw-box');
-
 idBox.addEventListener('input', activateLoginButton);
 pwBox.addEventListener('input', activateLoginButton);
-
-/*
-function inputOutlineOnClick(e) {
-  e.target.style.outline = '0.5px solid #a8a8a8';
-  function revertInputOutline(e) {
-    e.target.style.outline = 'none';
-  }
-  e.target.addEventListener('mouseout', revertInputOutline);
-}
-
-idBox.addEventListener('click', inputOutlineOnClick);
-pwBox.addEventListener('click', inputOutlineOnClick);
-*/
-
-
-/*
-function activateLoginButton() {
-  const loginButton = document.getElementById('login-button');
-  const idBox = document.getElementById('username-box');
-  const pwBox = document.getElementById('pw-box');
-  if (idBox.value && pwBox.value) {
-    loginButton.style.backgroundColor = '#0095f6';
-  } else {
-    loginButton.stylebackgroundColor = '#b2dffc';
-  }
-}*/
 
 
 
