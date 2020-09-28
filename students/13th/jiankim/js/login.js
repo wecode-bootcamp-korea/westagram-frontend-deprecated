1abf2ad5fcd0bb657b6dffea00d44099596b5ff8
김지안 <jian7061@gmail.com>
2020-09-28 21:25:32 +0900

    login button color change

--- students/13th/jiankim/js/login.js
+++ students/13th/jiankim/js/login.js
@@ -5,11 +5,21 @@ idInfo.addEventListener('keyup',function() {
     if (idInfo.value.length >= 1 && pwInfo.value.length >=1) {
         const btnColor = document.getElementById('loginBtn');
         btnColor.style.backgroundColor = "#0095F6";
-    }
+    } else {
+        const btnColor = document.getElementById('loginBtn');
+        btnColor.style.backgroundColor = "#C1DFFC";}
 });
 pwInfo.addEventListener('keyup',function() {
     if (idInfo.value.length >= 1 && pwInfo.value.length >=1) {
         const btnColor = document.getElementById('loginBtn');
         btnColor.style.backgroundColor = "#0095F6";
-    }
+    } else {
+        const btnColor = document.getElementById('loginBtn');
+        btnColor.style.backgroundColor = "#C1DFFC";}
 });
+
+
+
+// idInfo.value.length >= 1 && pwInfo.value.length >=1 ? const btnColor = document.getElementById('loginBtn');
+// btnColor.style.backgroundColor = "#0095F6"; : const btnColor = document.getElementById('loginBtn');
+// btnColor.style.backgroundColor = "#C1DFFC";}
