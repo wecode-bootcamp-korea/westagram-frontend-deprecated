95826d0af97818062515bd36a41125f6d08c9567
이지영 <leejyart@leejyart.local>
2020-09-28 15:00:16 +0900

    Add: 수정사항 반영 완료

--- students/13th/jiyoung/js/main.js
+++ students/13th/jiyoung/js/main.js
@@ -11,26 +11,18 @@ function addComment(e) {
         let span2 = document.createElement('span');
         let createDiv = document.createElement('div');
         let bttn = document.createElement('button')
-
         const comments = document.querySelector(".comments");
         const content = commment.value;
-
         createDiv.appendChild(span1);
         createDiv.appendChild(span2);
         createDiv.appendChild(bttn);
-
         createDiv.classname = "line4"
         bttn.className = "deleteBtn"
-
-
         span1.innerHTML = id;
         span2.innerHTML = " " + content;
         bttn.innerHTML = "delete";
-
         createDiv.firstChild = id;
         createDiv.firstChild.className = "bold";
-
-
         comments.appendChild(createDiv);
 
 
@@ -50,35 +42,25 @@ commment.addEventListener('keyup', (e) => {
 
 
 let search = document.querySelector("#검색");
-
 let searchbar = document.querySelector(".검색");
 
 
 function extendSearchbar(e) {
-    
-    
+
+
 
     if (search.value.includes("we"))
 
-    {   
+    {
         let span = document.createElement('span');
         let div = document.createElement('div');
-
-
         div.appendChild(span);
-
         div.className = "wecode";
         span.className = "wecode_bootcamp";
         span.innerHTML = "Wecode_bootcamp";
-
-
-
         searchbar.appendChild(div);
-    
     }
-
- return;
+    return;
 }
-
 
 search.addEventListener('keyup', (e) => extendSearchbar(e))
