e8cb55456016df92e53a6f65f778ced1fda7e319
김지안 <jian7061@gmail.com>
2020-09-28 22:09:54 +0900

    login button simpler code

--- students/13th/jiankim/js/login.js
+++ students/13th/jiankim/js/login.js
@@ -1,25 +1,30 @@
+// const idInfo = document.getElementById('userID');
+// const pwInfo = document.getElementById('userPW');
+
+// idInfo.addEventListener('keyup',function() {
+//     if (idInfo.value.length >= 1 && pwInfo.value.length >=1) {
+//         const btnColor = document.getElementById('loginBtn');
+//         btnColor.style.backgroundColor = "#0095F6";
+//     } else {
+//         const btnColor = document.getElementById('loginBtn');
+//         btnColor.style.backgroundColor = "#C1DFFC";}
+// });
+// pwInfo.addEventListener('keyup',function() {
+//     if (idInfo.value.length >= 1 && pwInfo.value.length >=1) {
+//         const btnColor = document.getElementById('loginBtn');
+//         btnColor.style.backgroundColor = "#0095F6";
+//     } else {
+//         const btnColor = document.getElementById('loginBtn');
+//         btnColor.style.backgroundColor = "#C1DFFC";}
+// });
+
+
+
 const idInfo = document.getElementById('userID');
 const pwInfo = document.getElementById('userPW');
+const btnColor = document.getElementById('loginBtn');
+const form = document.querySelector('form')
 
-idInfo.addEventListener('keyup',function() {
-    if (idInfo.value.length >= 1 && pwInfo.value.length >=1) {
-        const btnColor = document.getElementById('loginBtn');
-        btnColor.style.backgroundColor = "#0095F6";
-    } else {
-        const btnColor = document.getElementById('loginBtn');
-        btnColor.style.backgroundColor = "#C1DFFC";}
+form.addEventListener('keyup',function() {
+    idInfo.value.length >= 1 && pwInfo.value.length >=1? btnColor.style.backgroundColor = "#0095F6":btnColor.style.backgroundColor = "#C1DFFC";
 });
-pwInfo.addEventListener('keyup',function() {
-    if (idInfo.value.length >= 1 && pwInfo.value.length >=1) {
-        const btnColor = document.getElementById('loginBtn');
-        btnColor.style.backgroundColor = "#0095F6";
-    } else {
-        const btnColor = document.getElementById('loginBtn');
-        btnColor.style.backgroundColor = "#C1DFFC";}
-});
-
-
-
-// idInfo.value.length >= 1 && pwInfo.value.length >=1 ? const btnColor = document.getElementById('loginBtn');
-// btnColor.style.backgroundColor = "#0095F6"; : const btnColor = document.getElementById('loginBtn');
-// btnColor.style.backgroundColor = "#C1DFFC";}
