112f34e645e30b60a238e73744a066d456309735
김지안 <jian7061@gmail.com>
2020-09-28 20:56:27 +0900

    login

--- students/13th/jiankim/js/login.js
+++ students/13th/jiankim/js/login.js
@@ -1,9 +1,15 @@
 const idInfo = document.getElementById('userID');
 const pwInfo = document.getElementById('userPW');
 
-document.addEventListener('keyup', function() {
-    if (idInfo.value.length >= 1 && pwInfo.value.length >= 1) {
-        let color = document.getElementById('loginBtn');
-        color.style.backgroundColor = "#0095F6";
+idInfo.addEventListener('keyup',function() {
+    if (idInfo.value.length >= 1 && pwInfo.value.length >=1) {
+        const btnColor = document.getElementById('loginBtn');
+        btnColor.style.backgroundColor = "#0095F6";
     }
-})
+});
+pwInfo.addEventListener('keyup',function() {
+    if (idInfo.value.length >= 1 && pwInfo.value.length >=1) {
+        const btnColor = document.getElementById('loginBtn');
+        btnColor.style.backgroundColor = "#0095F6";
+    }
+});
