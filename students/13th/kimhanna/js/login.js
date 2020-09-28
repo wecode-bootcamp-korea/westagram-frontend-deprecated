9ac168b6587406aba6f6000f5357d819b2df685e
nana-kim <likesomemore90@gmail.com>
2020-09-28 14:51:37 +0900

    jaebal

---
+++ students/13th/kimhanna/js/login.js
@@ -0,0 +1,10 @@
+let input = document.querySelector('input');
+let log = document.getElementsByID('values');
+
+document.addEventListener('keyup',function(e){
+    if(info.vlaue !=="") {
+        if(PaymentAddress.vlaue !== "") {
+            
+        }
+    }
+})
