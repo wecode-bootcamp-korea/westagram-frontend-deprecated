c8abaa12cee81369601557ab01b2e1fe446e626b
Suhyun_Lim <suhuyn19@naver.com>
2020-09-28 18:16:13 +0900

    dd: main html/css 추가 -main.html 레이아웃 진행중입니다. -main.css 진행 중입니다.
    Refactor: 멘토리뷰반영
    -login.js 리팩토링했습니다.
    -login.html 불필요한 div 삭제 했습니다.
    :wq

---
+++ js/login.js
@@ -0,0 +1,13 @@
+const idInput = document.getElementById('ID')
+const pwInput = document.getElementById('PW')
+
+function booleanCheck() {
+    if  (!!idInput.value === true && !!pwInput.value === true) {
+        btn.style.backgroundColor = 'Blue'
+    } else {
+        btn.style.backgroundColor = '#C4E1FB'
+    }
+};
+
+pwInput.addEventListener('input', booleanCheck);
+idInput.addEventListener('input', booleanCheck);
