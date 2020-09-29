995852cb6d1e63af47b352b0c3389211a003cbed
김지안 <jian7061@gmail.com>
2020-09-29 15:52:05 +0900

    comment post button color change

---
+++ students/13th/jiankim/js/main.js
@@ -0,0 +1,15 @@
+//input의 길이가 1이상이면 '게시'버튼의 색 바뀜
+const commentInput = document.querySelector("#commentsInput");
+const commentPostBtn = document.querySelector("#commentsPost");
+
+document.addEventListener('keyup',function() {
+    commentInput.value.length >= 1? commentPostBtn.style.opacity = "1" : commentPostBtn.style.opacity = "0.3";
+})
+
+//게시'버튼을 누르면 기존의 댓글목록에 추가됨
+
+
+//추가된 댓글까지 화면에 렌더링
+
+
+//댓글을 게시한 후 input에 남아있는 value 삭제
