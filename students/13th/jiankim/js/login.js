30f0ea80c6b516baeb2382573ab3e4bee3a9af29
김지안 <jian7061@gmail.com>
2020-09-29 15:16:53 +0900

    mainpage html css done

--- students/13th/jiankim/js/login.js
+++ students/13th/jiankim/js/login.js
@@ -1,25 +1,3 @@
-// const idInfo = document.getElementById('userID');
-// const pwInfo = document.getElementById('userPW');
-
-// idInfo.addEventListener('keyup',function() {
-//     if (idInfo.value.length >= 1 && pwInfo.value.length >=1) {
-//         const btnColor = document.getElementById('loginBtn');
-//         btnColor.style.backgroundColor = "#0095F6";
-//     } else {
-//         const btnColor = document.getElementById('loginBtn');
-//         btnColor.style.backgroundColor = "#C1DFFC";}
-// });
-// pwInfo.addEventListener('keyup',function() {
-//     if (idInfo.value.length >= 1 && pwInfo.value.length >=1) {
-//         const btnColor = document.getElementById('loginBtn');
-//         btnColor.style.backgroundColor = "#0095F6";
-//     } else {
-//         const btnColor = document.getElementById('loginBtn');
-//         btnColor.style.backgroundColor = "#C1DFFC";}
-// });
-
-
-
 const idInfo = document.getElementById('userID');
 const pwInfo = document.getElementById('userPW');
 const btnColor = document.getElementById('loginBtn');
