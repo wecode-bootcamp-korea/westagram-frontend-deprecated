9808d211745353c6512934a639d1b4b181aa4285
Peter D Lee <dhlee9111@gmail.com>
2020-10-02 13:53:41 +0900

    Refactor: moved all data objects to the top of main.js - added comments to show the sections of the webpage the codes apply to

--- students/13th/leedonghun/js/main.js
+++ students/13th/leedonghun/js/main.js
@@ -1,54 +1,4 @@
-const body = document.querySelector('body');
-const navSearchBox = document.querySelector('.nav-search-box');
-const navSearchIcon = document.querySelector('img.nav-search-icon');
-const navSearchClearIcon = document.querySelector('img.nav-search-clear-icon');
-
-function navSearchBoxSelected() {
-  navSearchBox.style.textIndent = '12%';
-  navSearchBox.style.color = 'black';
-  navSearchIcon.style.left = '3%';
-  navSearchClearIcon.style.display = 'block';
-}
-
-function navSearchBoxUnselected(e) {
-  if (!e.target.matches('.nav-search-box, .nav-search-icon, .nav-search-clear-icon')) {
-    navSearchBox.style.textIndent = '41%';
-    navSearchBox.style.color = '#959595';
-    navSearchIcon.style.left = '31.5%';
-    navSearchClearIcon.style.display = 'none';
-  }
-}
-
-function clearSearch() {
-  navSearchBox.value = '';
-  body.addEventListener('click', navSearchBoxUnselected);
-  body.click();
-}
-
-navSearchBox.addEventListener('click', navSearchBoxSelected);
-body.addEventListener('click', navSearchBoxUnselected);
-navSearchClearIcon.addEventListener('click', clearSearch);
-
-
-
-const navMenuItems = document.querySelectorAll('.nav-menu-item');
-const navMenuAvatar = document.querySelector('.nav-menu-item.avatar');
-const navMenuAvatarBorder = document.querySelector('.nav-menu-item.avatar-outer-border');
-const navMenuHomeIcon = document.querySelector('.menu-item-icon.home-icon');
-const navMenuDropdown = document.querySelector('div.nav-menu-dropdown');
-
-function toggleDropdownDisplay() {
-  const isDropdownShown = navMenuDropdown.style.display !== 'none';
-  navMenuDropdown.style.display = isDropdownShown ? 'none' : 'block';
-  isDropdownShown ? navMenuHomeIcon.setAttribute('src', 'img/main/nav_menu_home_icon.png') : navMenuHomeIcon.setAttribute('src', 'img/main/onclick_icons/nav_menu_home_icon_white.png');
-  navMenuHomeIcon.style.width = isDropdownShown ? '130%' : '120%';
-  navMenuAvatar.style.marginRight = isDropdownShown ? '0' : '-7.5%';
-  navMenuAvatarBorder.style.border = isDropdownShown ? 'none' : '1.5px solid black';
-  navMenuAvatar.addEventListener('click', toggleDropdownDisplay);
-}
-
-navMenuAvatar.addEventListener('click', toggleDropdownDisplay);
-
+// **************** Westagram Data ****************
 
 const followersData = {
   'agst_1014': 'agst_1014.png',
@@ -90,114 +40,6 @@ for (let key in followersData) {
   followersIds.push(key);
   followersProfilePics.push(followersData[key]);
 }
-
-const story = document.querySelector('ul.story');
-const storyArrowButtonRight = document.querySelector('div.story-arrow-button.right');
-const storyArrowButtonLeft = document.querySelector('div.story-arrow-button.left');
-
-const storySectionWidth = 610;
-const storyItemWidth = 80;
-const storyLastItemRightMargin = 14;
-
-function slideStoryLeft() {
-  const storyPosition = story.style.transform;
-  const xCurrentStartIdx = (storyPosition.indexOf('-') === -1) ? storyPosition.indexOf('(') + 1 : storyPosition.indexOf('-') + 1;
-  const xCurrentEndIdx = storyPosition.indexOf('%');
-  const xCurrentAbs = Number(storyPosition.substring(xCurrentStartIdx, xCurrentEndIdx));
-  console.log('xCurrentAbs: ' + xCurrentAbs);
-  const xIncrement = ((storyItemWidth * 4) / storySectionWidth) * 100;
-  const xRemainder = 100 - xIncrement;
-  const storyTotalWidth = (((storyItemWidth * followersIds.length) + storyLastItemRightMargin) / storySectionWidth) * 100;
-  const isMaxReached = xCurrentAbs + xIncrement >=  storyTotalWidth - 100;
-  const xTranslated = isMaxReached ? (storyTotalWidth - 100) * -1 : (xCurrentAbs + xIncrement) * -1;
-  console.log('isMaxReached?: ' + isMaxReached);
-  console.log('xTranslated: ' + xTranslated);
-  story.style.transform = `translate(${xTranslated}%, 0)`;
-  story.style.transition = 'transform 600ms';
-  storyArrowButtonRight.style.display = isMaxReached ? 'none' : 'flex';
-  storyArrowButtonLeft.style.display = 'flex';
-}
-
-function slideStoryRight() {
-  const storyPosition = story.style.transform;
-  const xCurrentStartIdx = (storyPosition.indexOf('-') === -1) ? storyPosition.indexOf('(') + 1 : storyPosition.indexOf('-') + 1;
-  const xCurrentEndIdx = storyPosition.indexOf('%');
-  const xCurrentAbs = Number(storyPosition.substring(xCurrentStartIdx, xCurrentEndIdx));
-  console.log('xCurrentAbs: ' + xCurrentAbs);
-  const xIncrement = ((storyItemWidth * 4) / storySectionWidth) * 100;
-  const xRemainder = 100 - xIncrement;
-  const storyTotalWidth = (((storyItemWidth * followersIds.length) + storyLastItemRightMargin) / storySectionWidth) * 100;
-  const wasMaxReached = xCurrentAbs + (xIncrement - xRemainder) >= storyTotalWidth - 100;
-  const isMinReached = xCurrentAbs - xIncrement <= 0;
-  const xTranslated = wasMaxReached ? (xCurrentAbs - xRemainder) * -1 : (isMinReached ? 0 : (xCurrentAbs - xIncrement) * -1);
-  console.log('wasMaxReached?: ' + wasMaxReached);
-  console.log('isMinReached?: ' + isMinReached);
-  console.log('xTranslated: ' + xTranslated);
-  story.style.transform = `translate(${xTranslated}%, 0)`;
-  story.style.transition = 'transform 600ms';
-  storyArrowButtonLeft.style.display = isMinReached ? 'none' : 'flex';
-  storyArrowButtonRight.style.display = 'flex';
-}
-
-storyArrowButtonRight.addEventListener('click', slideStoryLeft);
-storyArrowButtonLeft.addEventListener('click', slideStoryRight);
-
-
-
-function generateStory(num) {
-  const storyProfilePicImg = document.createElement('img');
-  storyProfilePicImg.setAttribute('class', 'story-profile-pic');
-  storyProfilePicImg.setAttribute('src', `img/main/user_followers/${followersProfilePics[num]}`);
-  const storyProfilePicDiv = document.createElement('div');
-  storyProfilePicDiv.setAttribute('class', 'story-profile-pic');
-  storyProfilePicDiv.appendChild(storyProfilePicImg);
-  const storyProfilePicMidWhiteDiv = document.createElement('div');
-  storyProfilePicMidWhiteDiv.setAttribute('class', 'story-profile-pic-mid-white');
-  storyProfilePicMidWhiteDiv.appendChild(storyProfilePicDiv);
-  const storyProfilePicOuterGlow = document.createElement('div');
-  storyProfilePicOuterGlow.setAttribute('class', 'story-profile-pic-outer-glow');
-  storyProfilePicOuterGlow.appendChild(storyProfilePicMidWhiteDiv);
-  const storyProfileId = document.createElement('p');
-  storyProfileId.setAttribute('class', 'story-profile-id');
-  storyProfileId.innerHTML = `${followersIds[num]}`;
-  const storyElementLink = document.createElement('a');
-  storyElementLink.setAttribute('class', 'story-element-link');
-  storyElementLink.setAttribute('href', '#');
-  storyElementLink.appendChild(storyProfilePicOuterGlow);
-  storyElementLink.appendChild(storyProfileId);
-  const storyElement = document.createElement('li');
-  storyElement.setAttribute('class', `story-element story-${num}`);
-  storyElement.appendChild(storyElementLink);
-  story.appendChild(storyElement);
-}
-
-function generateOrderedArr(min, max) {
-  let newOrderedArr = [];
-  for (let i=min; i<max; i++) {
-    newOrderedArr.push(i);
-  }
-  return newOrderedArr;
-}
-
-function generateRandomOrderArr(orderedArr) {
-  let randomOrderArr = [];
-  while (orderedArr.length !== 0) {
-    let randIdx = Math.floor(Math.random() * orderedArr.length);
-    let randNum = orderedArr[randIdx];
-    orderedArr.splice(randIdx, 1);
-    randomOrderArr.push(randNum);
-  }
-  return randomOrderArr;
-}
-
-let followersNumArr = generateOrderedArr(0, followersIds.length);
-let followersRandNumArr = generateRandomOrderArr(followersNumArr);
-
-for (let i in followersRandNumArr) {
-  generateStory(followersRandNumArr[i]);
-}
-
-
 
 const placesData = [
   '',
@@ -304,6 +146,211 @@ const sampleCommentsData = [
   '👍🧡'
 ]
 
+const suggestedData = {
+  'rolex': 'rolex.png',
+  'wework': 'wework.png',
+  'nusr_et': 'nusr_et.png',
+  'davidbeckham': 'davidbeckham.png',
+  'lamborghini': 'lamborghini.png',
+  'ferrari': 'ferrari.png',
+  'seoyejidaily': 'seoyejidaily.png',
+  'porsche': 'porsche.png',
+  'victoriassecret': 'victoriassecret.png',
+  'disney': 'disney.png',
+  'bentleymotors': 'bentleymotors.png',
+  'cristiano': 'cristiano.png',
+  'hermes': 'hermes.png',
+  'palaceskateboards': 'palaceskateboards.png',
+  'nike': 'nike.png',
+  'rogerfederer': 'rogerfederer.png',
+  'kingjames': 'kingjames.png',
+  'lucascheon': 'lucascheon.png',
+  'google': 'google.png',
+  'apple': 'apple.png',
+  'cornelluniversity': 'cornelluniversity.png',
+  'teslamotors': 'teslamotors.png',
+  'thisisbillgates': 'thisisbillgates.png',
+  'momstouch_love': 'momstouch_love.png',
+  'subway': 'subway.png',
+  'kakaofriends_official': 'kakaofriends_official.png',
+  'hublot': 'hublot.png',
+  'patekphilippe': 'patekphilippe.png',
+  'audemarspiguet': 'audemarspiguet.png',
+  'yjoo_oh': 'yjoo_oh.png',
+  'yj_loves': 'yj_loves.png'
+}
+
+let suggestedIds = [];
+let suggestedProfilePics = [];
+
+for (let key in suggestedData) {
+  suggestedIds.push(key);
+  suggestedProfilePics.push(suggestedData[key]);
+}
+
+
+// **************** Nav Bar Codes ****************
+const body = document.querySelector('body');
+const navSearchBox = document.querySelector('.nav-search-box');
+const navSearchIcon = document.querySelector('img.nav-search-icon');
+const navSearchClearIcon = document.querySelector('img.nav-search-clear-icon');
+
+function navSearchBoxSelected() {
+  navSearchBox.style.textIndent = '12%';
+  navSearchBox.style.color = 'black';
+  navSearchIcon.style.left = '3%';
+  navSearchClearIcon.style.display = 'block';
+}
+
+function navSearchBoxUnselected(e) {
+  if (!e.target.matches('.nav-search-box, .nav-search-icon, .nav-search-clear-icon')) {
+    navSearchBox.style.textIndent = '41%';
+    navSearchBox.style.color = '#959595';
+    navSearchIcon.style.left = '31.5%';
+    navSearchClearIcon.style.display = 'none';
+  }
+}
+
+function clearSearch() {
+  navSearchBox.value = '';
+  body.addEventListener('click', navSearchBoxUnselected);
+  body.click();
+}
+
+navSearchBox.addEventListener('click', navSearchBoxSelected);
+body.addEventListener('click', navSearchBoxUnselected);
+navSearchClearIcon.addEventListener('click', clearSearch);
+
+
+const navMenuItems = document.querySelectorAll('.nav-menu-item');
+const navMenuAvatar = document.querySelector('.nav-menu-item.avatar');
+const navMenuAvatarBorder = document.querySelector('.nav-menu-item.avatar-outer-border');
+const navMenuHomeIcon = document.querySelector('.menu-item-icon.home-icon');
+const navMenuDropdown = document.querySelector('div.nav-menu-dropdown');
+
+function toggleDropdownDisplay() {
+  const isDropdownShown = navMenuDropdown.style.display !== 'none';
+  navMenuDropdown.style.display = isDropdownShown ? 'none' : 'block';
+  isDropdownShown ? navMenuHomeIcon.setAttribute('src', 'img/main/nav_menu_home_icon.png') : navMenuHomeIcon.setAttribute('src', 'img/main/onclick_icons/nav_menu_home_icon_white.png');
+  navMenuHomeIcon.style.width = isDropdownShown ? '130%' : '120%';
+  navMenuAvatar.style.marginRight = isDropdownShown ? '0' : '-7.5%';
+  navMenuAvatarBorder.style.border = isDropdownShown ? 'none' : '1.5px solid black';
+  navMenuAvatar.addEventListener('click', toggleDropdownDisplay);
+}
+
+navMenuAvatar.addEventListener('click', toggleDropdownDisplay);
+
+
+
+// **************** Story Section Codes ****************
+const story = document.querySelector('ul.story');
+const storyArrowButtonRight = document.querySelector('div.story-arrow-button.right');
+const storyArrowButtonLeft = document.querySelector('div.story-arrow-button.left');
+
+const storySectionWidth = 610;
+const storyItemWidth = 80;
+const storyLastItemRightMargin = 14;
+
+function slideStoryLeft() {
+  const storyPosition = story.style.transform;
+  const xCurrentStartIdx = (storyPosition.indexOf('-') === -1) ? storyPosition.indexOf('(') + 1 : storyPosition.indexOf('-') + 1;
+  const xCurrentEndIdx = storyPosition.indexOf('%');
+  const xCurrentAbs = Number(storyPosition.substring(xCurrentStartIdx, xCurrentEndIdx));
+  console.log('xCurrentAbs: ' + xCurrentAbs);
+  const xIncrement = ((storyItemWidth * 4) / storySectionWidth) * 100;
+  const xRemainder = 100 - xIncrement;
+  const storyTotalWidth = (((storyItemWidth * followersIds.length) + storyLastItemRightMargin) / storySectionWidth) * 100;
+  const isMaxReached = xCurrentAbs + xIncrement >=  storyTotalWidth - 100;
+  const xTranslated = isMaxReached ? (storyTotalWidth - 100) * -1 : (xCurrentAbs + xIncrement) * -1;
+  console.log('isMaxReached?: ' + isMaxReached);
+  console.log('xTranslated: ' + xTranslated);
+  story.style.transform = `translate(${xTranslated}%, 0)`;
+  story.style.transition = 'transform 600ms';
+  storyArrowButtonRight.style.display = isMaxReached ? 'none' : 'flex';
+  storyArrowButtonLeft.style.display = 'flex';
+}
+
+function slideStoryRight() {
+  const storyPosition = story.style.transform;
+  const xCurrentStartIdx = (storyPosition.indexOf('-') === -1) ? storyPosition.indexOf('(') + 1 : storyPosition.indexOf('-') + 1;
+  const xCurrentEndIdx = storyPosition.indexOf('%');
+  const xCurrentAbs = Number(storyPosition.substring(xCurrentStartIdx, xCurrentEndIdx));
+  console.log('xCurrentAbs: ' + xCurrentAbs);
+  const xIncrement = ((storyItemWidth * 4) / storySectionWidth) * 100;
+  const xRemainder = 100 - xIncrement;
+  const storyTotalWidth = (((storyItemWidth * followersIds.length) + storyLastItemRightMargin) / storySectionWidth) * 100;
+  const wasMaxReached = xCurrentAbs + (xIncrement - xRemainder) >= storyTotalWidth - 100;
+  const isMinReached = xCurrentAbs - xIncrement <= 0;
+  const xTranslated = wasMaxReached ? (xCurrentAbs - xRemainder) * -1 : (isMinReached ? 0 : (xCurrentAbs - xIncrement) * -1);
+  console.log('wasMaxReached?: ' + wasMaxReached);
+  console.log('isMinReached?: ' + isMinReached);
+  console.log('xTranslated: ' + xTranslated);
+  story.style.transform = `translate(${xTranslated}%, 0)`;
+  story.style.transition = 'transform 600ms';
+  storyArrowButtonLeft.style.display = isMinReached ? 'none' : 'flex';
+  storyArrowButtonRight.style.display = 'flex';
+}
+
+storyArrowButtonRight.addEventListener('click', slideStoryLeft);
+storyArrowButtonLeft.addEventListener('click', slideStoryRight);
+
+
+function generateStory(num) {
+  const storyProfilePicImg = document.createElement('img');
+  storyProfilePicImg.setAttribute('class', 'story-profile-pic');
+  storyProfilePicImg.setAttribute('src', `img/main/user_followers/${followersProfilePics[num]}`);
+  const storyProfilePicDiv = document.createElement('div');
+  storyProfilePicDiv.setAttribute('class', 'story-profile-pic');
+  storyProfilePicDiv.appendChild(storyProfilePicImg);
+  const storyProfilePicMidWhiteDiv = document.createElement('div');
+  storyProfilePicMidWhiteDiv.setAttribute('class', 'story-profile-pic-mid-white');
+  storyProfilePicMidWhiteDiv.appendChild(storyProfilePicDiv);
+  const storyProfilePicOuterGlow = document.createElement('div');
+  storyProfilePicOuterGlow.setAttribute('class', 'story-profile-pic-outer-glow');
+  storyProfilePicOuterGlow.appendChild(storyProfilePicMidWhiteDiv);
+  const storyProfileId = document.createElement('p');
+  storyProfileId.setAttribute('class', 'story-profile-id');
+  storyProfileId.innerHTML = `${followersIds[num]}`;
+  const storyElementLink = document.createElement('a');
+  storyElementLink.setAttribute('class', 'story-element-link');
+  storyElementLink.setAttribute('href', '#');
+  storyElementLink.appendChild(storyProfilePicOuterGlow);
+  storyElementLink.appendChild(storyProfileId);
+  const storyElement = document.createElement('li');
+  storyElement.setAttribute('class', `story-element story-${num}`);
+  storyElement.appendChild(storyElementLink);
+  story.appendChild(storyElement);
+}
+
+function generateOrderedArr(min, max) {
+  let newOrderedArr = [];
+  for (let i=min; i<max; i++) {
+    newOrderedArr.push(i);
+  }
+  return newOrderedArr;
+}
+
+function generateRandomOrderArr(orderedArr) {
+  let randomOrderArr = [];
+  while (orderedArr.length !== 0) {
+    let randIdx = Math.floor(Math.random() * orderedArr.length);
+    let randNum = orderedArr[randIdx];
+    orderedArr.splice(randIdx, 1);
+    randomOrderArr.push(randNum);
+  }
+  return randomOrderArr;
+}
+
+let followersNumArr = generateOrderedArr(0, followersIds.length);
+let followersRandNumArr = generateRandomOrderArr(followersNumArr);
+
+for (let i in followersRandNumArr) {
+  generateStory(followersRandNumArr[i]);
+}
+
+
+
+// **************** Feed Section Codes ****************
 const feedsDiv = document.querySelector('div.feeds');
 
 function generateFeed(picNum) {
@@ -390,8 +437,7 @@ function generateFeed(picNum) {
   for (let i in feedRandNumArr) {
     generateFeed(feedRandNumArr[i]);
   }
-  
-  
+
 
   const feedElements = feedsDiv.querySelectorAll('.feed-element');
   const feedActionHeartIcons = feedsDiv.querySelectorAll('.feed-action-heart');
@@ -484,48 +530,7 @@ function generateFeed(picNum) {
 
 
 
-const suggestedData = {
-  'rolex': 'rolex.png',
-  'wework': 'wework.png',
-  'nusr_et': 'nusr_et.png',
-  'davidbeckham': 'davidbeckham.png',
-  'lamborghini': 'lamborghini.png',
-  'ferrari': 'ferrari.png',
-  'seoyejidaily': 'seoyejidaily.png',
-  'porsche': 'porsche.png',
-  'victoriassecret': 'victoriassecret.png',
-  'disney': 'disney.png',
-  'bentleymotors': 'bentleymotors.png',
-  'cristiano': 'cristiano.png',
-  'hermes': 'hermes.png',
-  'palaceskateboards': 'palaceskateboards.png',
-  'nike': 'nike.png',
-  'rogerfederer': 'rogerfederer.png',
-  'kingjames': 'kingjames.png',
-  'lucascheon': 'lucascheon.png',
-  'google': 'google.png',
-  'apple': 'apple.png',
-  'cornelluniversity': 'cornelluniversity.png',
-  'teslamotors': 'teslamotors.png',
-  'thisisbillgates': 'thisisbillgates.png',
-  'momstouch_love': 'momstouch_love.png',
-  'subway': 'subway.png',
-  'kakaofriends_official': 'kakaofriends_official.png',
-  'hublot': 'hublot.png',
-  'patekphilippe': 'patekphilippe.png',
-  'audemarspiguet': 'audemarspiguet.png',
-  'yjoo_oh': 'yjoo_oh.png',
-  'yj_loves': 'yj_loves.png'
-}
-
-let suggestedIds = [];
-let suggestedProfilePics = [];
-
-for (let key in suggestedData) {
-  suggestedIds.push(key);
-  suggestedProfilePics.push(suggestedData[key]);
-}
-
+// **************** Side Section Codes ****************
 function generateSuggestions(num) {
   let followedByStatementOptions = [
     `Followed by ${followersIds[Math.floor(Math.random() * followersIds.length)]}`,
