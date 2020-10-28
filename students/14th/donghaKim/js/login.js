6e803229ed24db617c7db9dcc21100ca3f1105b3
dongha <dongha@email.com>
2020-10-28 21:14:16 +0900

    초기 세팅 완료

---
+++ students/14th/donghaKim/js/login.js
@@ -0,0 +1,52 @@
+const form = document.querySelector(".user-form")
+const IdInput = document.querySelector(".user-id")
+const passwordInput = document.querySelector(".password")
+const loginBtn = document.querySelector(".login-btn")
+const loginSuccess = document.querySelector(".after-login")
+const loginWelcome = document.querySelector(".login-welcome")
+let loginObj = []
+let timerId
+
+
+
+function userLogin(e) {
+    console.log(e.target)
+}
+
+form.addEventListener("keyup", function(e) {
+    let userPassword = passwordInput.value
+    userPassword.length > 6 ? loginBtn.style.backgroundColor = "#4e5dfc" : loginBtn.style.backgroundColor = ""
+})
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault()
+    let userId = IdInput.value
+    let userPassword = passwordInput.value
+    IdInput.value = ""
+    passwordInput.value = ""
+    let userInfo = createObj(userId, userPassword)
+    if (loginObj.length > 1) {
+        return
+    }
+    loginObj.push(userInfo)
+    validation()
+})
+
+
+
+function createObj(userId, userPassword) {
+    return {
+        userId,
+        userPassword
+    }
+}
+
+function validation() {
+    if (loginObj[0].userId === "immerfernweh_" && loginObj[0].userPassword === "1234") {
+        loginSuccess.style.display = "block"
+        timerId = setTimeout(function() { window.location = "main.html" }, 1000)
+
+    } else {
+        loginSuccess.style.display = ""
+    }
+}
---
+++ students/14th/donghaKim/js/main.js
@@ -0,0 +1,364 @@
+const heartWrap = document.querySelector(".heart-icon")
+const navHeartBefore = document.querySelector(".i-default")
+const navHeartAfter = document.querySelector(".full")
+
+const feedWrap = document.querySelector(".feed-bottom-icon-wrap")
+const feedHeartBefore = document.querySelector(".feed-default")
+const feedHearAfter = document.querySelector(".feed-full")
+
+const feedRightWrap = document.querySelector(".feed-right-icon-wrap")
+const feedRightBefore = document.querySelector(".feed-right-default")
+const feedRightAfter = document.querySelector(".feed-right-full")
+
+const storyAddBtn = document.querySelector(".story-add-btn")
+const storyAddDiv = document.querySelector(".story-add-wrap")
+const storyAddName = document.querySelector(".story-add-name")
+const storyAddImg = document.querySelector(".story-add-img")
+
+const navProfile = document.querySelector(".nav-profile")
+const toggledProfile = document.querySelector(".icons-wrap-toggle")
+const navLogout = document.querySelector(".logout")
+
+let LOCAL_KEY = "comment-list"
+let STORY_KEY = "story-key"
+let SEARCH_KEY = "search-key"
+let lists = JSON.parse(localStorage.getItem(LOCAL_KEY)) || []
+let userArr = JSON.parse(localStorage.getItem(STORY_KEY)) || []
+let serachArr = JSON.parse(localStorage.getItem(SEARCH_KEY)) || []
+
+let click = false;
+let storyClicked = false;
+
+let cnt = 7;
+let logutId;
+
+function navHeartHandler(e) {
+    const heart = e.target
+    if (heart.classList.contains("i-default")) {
+        navHeartAfter.style.display = "block"
+    } else if (heart.classList.contains("full")) {
+        navHeartAfter.style.display = "none"
+    }
+}
+
+function feedHeartHandler(e) {
+    const heart = e.target
+    if (heart.classList.contains("feed-default")) {
+        feedHearAfter.style.display = "block"
+        click = true;
+        heartCount()
+
+    } else if (heart.classList.contains("feed-full")) {
+        feedHearAfter.style.display = "none"
+        click = false;
+        heartCount()
+    }
+}
+
+function heartCount() {
+    if (click) {
+        cnt++
+    } else {
+        cnt--
+    }
+    let preHeart = document.querySelector(".feed-like").innerHTML
+    preHeart = `${cnt}`
+    document.querySelector(".feed-like").innerHTML = preHeart
+    console.log(preHeart)
+}
+heartCount()
+
+function storyAddHandler() {
+    storyAddDiv.classList.toggle("show-profile")
+}
+
+function navProdilehandler() {
+    toggledProfile.classList.toggle("show-profile")
+}
+
+function logoutHandler() {
+    logutId = setTimeout(function() { window.location = "login.html" }, 1000)
+}
+
+function createStoryObj(userId, url) {
+    return {
+        id: new Date().getTime().toString(),
+        userId: userId,
+        bg: "img/main/story_icon.png",
+        src: url
+    }
+}
+
+heartWrap.addEventListener("click", navHeartHandler)
+feedWrap.addEventListener("click", feedHeartHandler)
+heartWrap.addEventListener("click", storyAddHandler)
+storyAddDiv.addEventListener("submit", function(e) {
+    e.preventDefault()
+    let userId = storyAddName.value
+    let userUrl = storyAddImg.value
+    storyAddName.value = ""
+    storyAddImg.value = ""
+    let story = createStoryObj(userId, userUrl)
+    userArr.push(story)
+    renderStory()
+})
+navProfile.addEventListener("click", navProdilehandler)
+navLogout.addEventListener("click", logoutHandler)
+
+
+const listsContainer = document.querySelector(".feed-user-comment-list-wrap")
+const feedUserForm = document.querySelector(".feed-user-form-js")
+const feedUserInput = document.querySelector(".feed-user-comment-input")
+const showRefList = document.querySelector(".refList")
+const contentMore = document.querySelector(".feed-user-content-more")
+const userContent = document.querySelector(".feed-user-content")
+const commentList = document.querySelector(".feed-user-comment-count")
+const commentCountSpan = document.querySelector(".comment-count")
+const commentShowBtn = document.querySelector(".comment-show-btn")
+const storyListsWrap = document.querySelector(".story-lists-wrap")
+const storyDeleteBtn = document.querySelector(".story-delete")
+const storyWrapTarget = document.querySelector(".story-add-wrap")
+const searchForm = document.querySelector(".header-search-form")
+const formForBorder = document.querySelector(".form-wrap")
+const searchUserInput = document.querySelector(".header-search-input")
+const card = document.querySelector(".card")
+const insertBefore = document.querySelector(".feed-header-wrap")
+const insertAfter = document.querySelector(".bottom")
+const feedRender = document.querySelector(".feed-render")
+const refListWrap = document.querySelector(".refList-wrap")
+
+let tempStr = ""
+let submited = false
+let showContentFlag = false
+let showCommentFlag = true;
+console.log(lists)
+
+function renderlists() {
+    clearEelmenet(listsContainer)
+    hideContent()
+    hideComment()
+    lists.forEach(comment => {
+        const list = document.createElement("li")
+        console.log(comment)
+        list.classList.add("feed-user-comment-list")
+        list.dataset.listId = comment.id
+        list.innerHTML += ` <div class="feed-user-comment-list-left">
+                             <span class="feed-user-comment-list-id">${comment.userId}</span>
+                             <span class="feed-user-comment-list-ref"></span>
+                             <span class="feed-user-comment-list-content">${comment.comment}</span>
+                            </div>
+                           <div class="feed-user-comment-list-right">
+                            <div class="feed-right-icon-wrap">
+                            <i class="far fa-trash-alt"></i>
+                           </div>
+                          </div>
+                        </li>`
+        listsContainer.appendChild(list)
+    })
+    saveLists()
+}
+
+
+function renderFeed() {
+    if (serachArr[0].src === undefined) {
+        serachArr[0].src = "https://t1.daumcdn.net/cfile/tistory/215B644D56DCF5E82C"
+    } else {
+        serachArr = serachArr.splice(serachArr.length - 1, 1)
+        clearEelmenet(feedRender)
+        serachArr.forEach(search => {
+            const div = document.createElement("div")
+            div.classList.add("feed-img")
+            console.log(search)
+            div.innerHTML += `<img src="${search.src}" alt="">
+                         </div>`
+            feedRender.appendChild(div)
+            card.insertBefore(feedRender, insertAfter)
+        })
+    }
+    saveLists()
+}
+
+function renderStory() {
+    clearEelmenet(storyListsWrap)
+    userArr.forEach(story => {
+        const storyElement = document.createElement("li")
+        storyElement.classList.add("feed-user-comment-list")
+        storyElement.dataset.listId = story.id
+        storyElement.innerHTML += ` <img src="${story.bg}" alt="" class="circle">
+                                    <img src="${story.src}" alt="" class="ciicle-avatar">
+                                    <span class="avatar-name">${story.userId}</span>
+                                 </li>`
+        storyListsWrap.appendChild(storyElement)
+    })
+    saveLists()
+}
+
+function showList(userInputCommment, submited) {
+    if (userInputCommment[1]) {
+        if (!submited) {
+            let targetedUser = []
+            for (let i = 0; i < userArr[0].userId.length; i++) {
+                let targetUserId = userArr[i].userId
+                let newInput = userInputCommment.slice(1, userInputCommment.length)
+                if (targetUserId.indexOf(newInput) > -1) {
+                    targetedUser.push(userArr[i])
+                }
+            }
+            targetedUser.forEach(user => {
+                const div = document.createElement("div")
+                div.classList.add("refList-wrap")
+                div.innerHTML += `             
+                                  <div class="refList-avatar">
+                                      <img src="img/main/story_icon.png" alt="" class="refList-bg">
+                                      <img src=${user.src} alt="" class="refList-img">
+                                       </div>
+                                        <div class="ref-name">
+                                           <span class="ref-id-span">${user.userId}</span>
+                                   </div>
+                             </div >
+                        `
+                showRefList.appendChild(div)
+                console.log(user)
+            })
+        } else {}
+    }
+}
+
+function hideContent() {
+    let maxLength = 5
+    let contentText = userContent.innerText
+    tempStr = contentText
+    let copiedStr = ""
+    let contentLength = contentText.length
+    if (!showContentFlag) {
+        if (contentLength > maxLength) {
+            copiedStr = contentText.slice(0, 5) + "....."
+        }
+    }
+    userContent.innerText = copiedStr
+    console.log(tempStr, copiedStr)
+}
+
+
+function hideComment() {
+    let listsLength = lists.length
+    if (showCommentFlag) {
+        commentList.style.display = "none"
+        if (listsLength > 4) {
+            commentList.style.display = ""
+            commentCountSpan.innerHTML = `${listsLength}`
+            listsContainer.style.display = "none"
+        }
+    }
+}
+
+function showComment() {
+    showCommentFlag = false;
+    listsContainer.style.display = ""
+}
+
+function showMoreContent() {
+    showContentFlag = true
+    let contentText = tempStr
+    if (showContentFlag) {
+        userContent.innerText = contentText
+    }
+}
+searchForm.addEventListener("keyup", function(e) {
+    searchUserInput.classList.add("typed")
+})
+
+searchForm.addEventListener("submit", function(e) {
+    e.preventDefault()
+    let userInput = searchUserInput.value
+    let fomattedInput = createInput(userInput)
+    serachArr.push(fomattedInput)
+    searchUserInput.value = ""
+    renderFeed()
+    saveLists()
+
+
+})
+
+feedUserForm.addEventListener("keyup", function(e) {
+    const userInputCommment = feedUserInput.value
+    const submitBtn = document.querySelector(".feed-user-comment-submit")
+    userInputCommment.length > 1 ? submitBtn.style.color = "#4e5dfc" : submitBtn.style.color = ""
+    if (feedUserInput.value[0] === "@") {
+        showList(userInputCommment, submited)
+    }
+})
+
+
+feedUserForm.addEventListener("submit", function(e) {
+    e.preventDefault()
+    submited = true;
+    const userInputCommment = feedUserInput.value
+    if (userInputCommment === "" || userInputCommment === null) return
+    showList(userInputCommment, submited)
+    const comment = createList(userInputCommment)
+    lists.push(comment)
+    feedUserInput.value = ""
+    renderlists()
+    saveLists()
+})
+
+
+contentMore.addEventListener("click", showMoreContent)
+commentShowBtn.addEventListener("click", showComment)
+
+function createInput(url) {
+    return {
+        src: url
+    }
+}
+
+function createList(comment) {
+    return {
+        id: new Date().getTime().toString(),
+        userId: "immerferenweh_",
+        comment
+    }
+}
+
+listsContainer.addEventListener("click", function(e) {
+    if (e.target.classList.contains("far")) {
+        let targetedList = e.target.parentElement.parentElement.parentElement
+        console.log(targetedList)
+        let targetedListId = targetedList.getAttribute("data-list-id")
+        lists = lists.filter(list => list.id !== targetedListId)
+        renderlists()
+        commentCountSpan.innerHTML = lists.length
+        saveLists()
+    }
+})
+
+storyWrapTarget.addEventListener("click", function(e) {
+    console.log(e)
+    if (e.target.classList.contains("story-delete")) {
+        let targetedStory = e.target.parentElement.previousElementSibling.firstElementChild
+        userArr.splice(userArr.length - 1, 1)
+        renderStory()
+        saveLists()
+    }
+})
+
+function saveLists() {
+    localStorage.setItem(LOCAL_KEY, JSON.stringify(lists))
+    localStorage.setItem(STORY_KEY, JSON.stringify(userArr))
+    localStorage.setItem(SEARCH_KEY, JSON.stringify(serachArr))
+}
+
+
+function clearEelmenet(container) {
+    while (container.firstElementChild) {
+        container.removeChild(container.firstElementChild)
+    }
+}
+
+function render() {
+    renderStory()
+    renderlists()
+    renderFeed()
+}
+render()
