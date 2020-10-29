e710ce58cd63760a05e82fda092188fb006e66fb
공주민 <ley67890@naver.com>
2020-10-29 11:27:04 +0900

    로그인 페이지 구현

---
+++ students/14th/juminkong/script.js
@@ -0,0 +1,12 @@
+let input_ID = document.querySelector('.input_ID');
+let input_PW = document.querySelector('.input_PW');
+
+input_ID.addEventListener('keydown', () => {
+    let button = document.querySelector('button');
+    button.style.opacity = 1;
+});
+
+input_PW.addEventListener('keydown', () => {
+    let button = document.querySelector('button');
+    button.style.opacity = 1;
+});
