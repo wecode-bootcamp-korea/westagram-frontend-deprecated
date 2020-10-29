c04df7e90f56c0ab2d2ffb7a0adf8f3e2b9f0288
김태현 <polepole0733@gmail.com>
2020-10-29 20:56:48 +0900

    로그인 이벤트 삼항연산자 적용

--- students/14th/kimtaehyun/js/login.js
+++ students/14th/kimtaehyun/js/login.js
@@ -1,19 +1,21 @@
-
 const input = document.querySelectorAll('input');
-
-// 삼항연산자를 이용해서 로그인 작동
-// 삼항연산자를 어떻게 쓰지????히ㅣㅎ힣
-// 조건을 어떻게 잡냐???히히히
-
-input[0].addEventListener('keydown', () => {
-    make_blue();
-})
-
-input[1].addEventListener('keydown', () => {
-    make_blue();
-})
+const button = document.querySelector('button');
 
 function make_blue() {
     const button = document.querySelector('button');
     button.style.opacity = 1;
 }
+
+input[0].value.length < 0 ? input[0].addEventListener('keyup', () => {
+    button.style.opacity = 0.35;
+}) : input[0].addEventListener('keydown', () => {
+    make_blue();
+});
+
+input[1].value.length < 0 ? input[1].addEventListener('keyup', () => {
+    button.style.opacity = 0.35;
+}) : input[1].addEventListener('keydown', () => {
+    make_blue();
+});
+
+
