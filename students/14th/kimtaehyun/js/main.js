b5c39419ba74cd983675e49083eb930376726fee
김태현 <polepole0733@gmail.com>
2020-10-29 12:41:49 +0900

    Fix: main.js 코드수정

--- students/14th/kimtaehyun/js/main.js
+++ students/14th/kimtaehyun/js/main.js
@@ -1,62 +1,38 @@
 let enterEvent = document.querySelector('#input_comment');
 let clickEvent = document.querySelector('post_comment_btn');
-let next_comment = document.querySelector('.next_comment');
+let comment_container = document.querySelector('.comment_container');
 let now = new Date();
 
-enterEvent.addEventListener('keydown', (e) => {
-    // article_comments_2
-    let p = document.createElement('newcomments');
+function post_comment() {
+    let newcomments = document.createElement('newcomments');
     let comment = document.querySelector('.article_comments_1');
-    if (e.keyCode === 13) {
-        if (p.innerHTML === '') {
-            p.innerHTML = `<p>neceousecius<span>${enterEvent.value}</span></p>`;
-            next_comment.append(p);
-            enterEvent.value = '';
-            let time = document.querySelector('time');
-            let writeDay = new Date();
-            let passedTime = Math.floor(parseInt(now - writeDay) / 1000);
-            time.innerHTML = passedTime + "초 전";
-        } else if(p.innerHTML !== '') {
-            p.innerHTML = `<p>neceousecius<span>${enterEvent.value}</span></p>`;
-            next_comment.append(p);
-            enterEvent.value = '';
-            let time = document.querySelector('time');
-            let writeDay = new Date();
-            let passedTime = Math.floor(parseInt(now - writeDay) / 1000);
-            time.innerHTML = passedTime + "초 전";
-        }
-    }
-})
-
-clickEvent.addEventListener('click', () => {
-    let p = document.createElement('newcomments');
-    let comment = document.querySelector('.article_comments_1');
-    if (p.innerHTML === '') {
-        p.innerHTML = `
-        <p>neceousecius<span>${enterEvent.value}</span></p>`;
-        next_comment.append(p);
+    if (newcomments.innerHTML === '') {
+        newcomments.innerHTML = `<p>b2ng_9<span>${enterEvent.value}</span></p>`;
+        comment_container.append(newcomments);
         enterEvent.value = '';
-        let time = document.querySelector('time');
-        let writeDay = new Date();
-        let passedTime = Math.floor(parseInt(now - writeDay) / 1000);
-        time.innerHTML = passedTime + "초 전";
-    } else if(p.innerHTML !== '') {
-        p.innerHTML = `<p>neceousecius<span>${enterEvent.value}</span></p>`;
-        next_comment.append(p);
+    } else if(newcomments.innerHTML !== '') {
+        newcomments.innerHTML = `<p>b2ng_9<span>${enterEvent.value}</span></p>`;
+        comment_container.append(p);
         enterEvent.value = '';
-
-        let time = document.querySelector('time');
-        let writeDay = new Date();
-        let passedTime = Math.floor(parseInt(now - writeDay) / 1000);
-        time.innerHTML = passedTime + "초 전";
     }
+}
+
+function time_ticking() {
     let time = document.querySelector('time');
     let writeDay = new Date();
     let passedTime = Math.floor(parseInt(now - writeDay) / 1000);
     time.innerHTML = passedTime + "초 전";
+    now = new Date();
+}
+
+enterEvent.addEventListener('keydown', (e) => {
+    if(e.keyCode === 13) {
+        post_comment();
+        time_ticking();
+    }
 })
 
-
-
-
-
+clickEvent.addEventListener('click', () => {
+    post_comment();
+    time_ticking();
+})
