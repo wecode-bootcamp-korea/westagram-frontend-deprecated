ab6926597415583cd8aafb2be3f66c626ffa8198
김태현 <polepole0733@gmail.com>
2020-10-29 12:09:42 +0900

    Fix: login.js 코드수정 완료

--- students/14th/kimtaehyun/js/login.js
+++ students/14th/kimtaehyun/js/login.js
@@ -1,12 +1,18 @@
 let input_id = document.querySelector('.input_id');
 let input_pass = document.querySelector('.input_pass');
 
-input_id.addEventListener('keydown', () => {
+// input도 id, pass 나누지말고 하나로 주고싶은데 왜 안되지?? querySelectorAll 하면될거 같은데..
+// let input = document.querySelectorAll('input');
+
+function make_blue() {
     let button = document.querySelector('button');
     button.style.opacity = 1;
+}
+
+input_id.addEventListener('keydown', () => {
+    make_blue();
 })
 
 input_pass.addEventListener('keydown', () => {
-    let button = document.querySelector('button');
-    button.style.opacity = 1;
+    make_blue();
 })
