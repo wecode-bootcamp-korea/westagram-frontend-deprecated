2baacd647d6938ba68f4b5d041776b1368e10ead
김태현 <polepole0733@gmail.com>
2020-10-29 19:23:22 +0900

    삼항연산자에 조건을 어떻게 잡을지 고심중

--- students/14th/kimtaehyun/js/login.js
+++ students/14th/kimtaehyun/js/login.js
@@ -1,18 +1,19 @@
-const input_id = document.querySelector('.input_id');
-const input_pass = document.querySelector('.input_pass');
 
-// input도 id, pass 나누지말고 하나로 주고싶은데 왜 안되지?? querySelectorAll 하면될거 같은데..
-// let input = document.querySelectorAll('input');
+const input = document.querySelectorAll('input');
+
+// 삼항연산자를 이용해서 로그인 작동
+// 삼항연산자를 어떻게 쓰지????히ㅣㅎ힣
+// 조건을 어떻게 잡냐???히히히
+
+input[0].addEventListener('keydown', () => {
+    make_blue();
+})
+
+input[1].addEventListener('keydown', () => {
+    make_blue();
+})
 
 function make_blue() {
     const button = document.querySelector('button');
     button.style.opacity = 1;
 }
-
-input_id.addEventListener('keydown', () => {
-    make_blue();
-})
-
-input_pass.addEventListener('keydown', () => {
-    make_blue();
-})
