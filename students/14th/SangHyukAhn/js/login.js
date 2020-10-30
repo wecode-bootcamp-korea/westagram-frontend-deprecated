c07298e091b8a3d4566d9535834e2972ef6e9d1f
Xednicoder <kaaiinn4@gmail.com>
2020-10-30 15:04:08 +0900

    Modify : 로그인 기능 가독성 향상(변수 생성)

--- students/14th/SangHyukAhn/js/login.js
+++ students/14th/SangHyukAhn/js/login.js
@@ -4,22 +4,33 @@ const locationLoginButton = document.querySelector('.loginButton');
 const locationErrorMsg = document.querySelector('.errorMsg');
 
 locationLoginButton.addEventListener('click', loginFunction);
+
 function loginFunction() {
-  if (locationInputId.value === 'xedni' && locationInputPw.value === '123') {
+  let isUserInfoValid = {
+    id: 'xedni',
+    passWord: '123',
+  };
+  let isUserInputId = locationInputId.value;
+  let isUserInputPw = locationInputPw.value;
+  console.log(isUserInputPw);
+  if (
+    isUserInputId === isUserInfoValid.id &&
+    isUserInputPw === isUserInfoValid.passWord
+  ) {
     window.open('../main.html', '_self');
   } else if (
-    locationInputId.value !== 'xedni' &&
-    locationInputPw.value === '123'
+    isUserInputId !== isUserInfoValid.id &&
+    isUserInputPw === isUserInfoValid.passWord
   ) {
     locationErrorMsg.innerText = '계정을 잘못 입력 했잖아';
   } else if (
-    locationInputId.value === 'xedni' &&
-    locationInputPw.value !== '123'
+    isUserInputId === isUserInfoValid.id &&
+    isUserInputPw !== isUserInfoValid.passWord
   ) {
     locationErrorMsg.innerText = '비밀번호를 틀렸어';
   } else if (
-    locationInputId.value !== 'xedni' &&
-    locationInputPw.value !== '123'
+    isUserInputId !== isUserInfoValid.id &&
+    isUserInputPw !== isUserInfoValid.passWord
   ) {
     locationErrorMsg.innerText = '아이디가 없거나 비밀번호가 잘못 되었어';
   }
