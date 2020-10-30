c2953af7b828c96273ad748c753ca028610d225b
김태현 <polepole0733@gmail.com>
2020-10-30 20:39:37 +0900

    Fix: 댓글 하트구현

--- students/14th/kimtaehyun/js/login.js
+++ students/14th/kimtaehyun/js/login.js
@@ -4,14 +4,6 @@ const input_container = document.querySelector('.input_container');
 const button = document.querySelector('button');
 
 input_container.addEventListener('keyup', () => {
-    button.style.opacity = (id.value.length || pw.value.length) ? 1 : 0.35;
+    button.style.opacity = (id.value.indexOf("@") != -1 && pw.value.length >= 5) ? 1 : 0.35;
 })
 
-let input1 = input[0].value.length > 0 ? true : false;
-
-// input[0].addEventListener('keyup', () => {
-//     console.log(input[0].value);
-//     if(input[0].value.indexOf("@") != -1) {
-//         alert('@포함 O');
-//     }
-// })
--- students/14th/kimtaehyun/js/main.js
+++ students/14th/kimtaehyun/js/main.js
@@ -3,15 +3,36 @@ const clickEvent = document.querySelector('post_comment_btn');
 const comment_container = document.querySelector('.comment_container');
 let now = new Date();
 
+const empty_heart = document.querySelector('.empty_heart');
+const filled_heart = document.querySelector('.filled_heart');
+
+empty_heart.addEventListener('click', () => {
+    filled_heart.style.display = 'inline-block';
+})
+
+filled_heart.addEventListener('click', () => {
+    filled_heart.style.display = 'none';
+})
+
+// 각각의 댓글에서 어떻게 하트를 다 줄수 있을까? 무슨 배열이랑 반복문 써야할것 같은데
+
 function post_comment() {
     let newcomments = document.createElement('newcomments');
     const comment = document.querySelector('.article_comments_1');
     if (newcomments.innerHTML === '') {
-        newcomments.innerHTML = `<p>b2ng_9<span>${enterEvent.value}</span></p>`;
+        newcomments.innerHTML = `<p>b1ng9u<span>위워크에서 진행한 베이킹 클래스...</span><more>더 보기</more></p>
+        <div class="heart_container">
+          <img class="empty_heart" src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png">
+          <img class="filled_heart" src="img/filled_heart.png">
+        </div>`;
         comment_container.append(newcomments);
         enterEvent.value = '';
     } else if(newcomments.innerHTML !== '') {
-        newcomments.innerHTML = `<p>b2ng_9<span>${enterEvent.value}</span></p>`;
+        newcomments.innerHTML = `<p>b1ng9u<span>위워크에서 진행한 베이킹 클래스...</span><more>더 보기</more></p>
+        <div class="heart_container">
+          <img class="empty_heart" src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png">
+          <img class="filled_heart" src="img/filled_heart.png">
+        </div>`;
         comment_container.append(p);
         enterEvent.value = '';
     }
