338c9446732af25164c252b731a25299d7627293
류지혜 <ryujihye@lyujihyeui-MacBook-Pro.local>
2020-10-30 19:59:51 +0900

    Refactor - 메인페이지 반복되는 코드 -> map함수로 변경

--- students/14th/jihyeryu/js/main.js
+++ students/14th/jihyeryu/js/main.js
@@ -1,25 +1,24 @@
 const commentInputBtn = document.getElementById('comment-input-btn');
 const displayComment = document.getElementById('comment-side');
 const commentInputSide = document.getElementById('comment-input-side');
-// const activeBtn = commentInputBtn.classList.add("activeLogin");
-// const inactiveBtn = commentInputBtn.classList.add('inactiveLogin');
 
+// 댓글 작성시 게시버튼 활성화
 function checkComment() {
-//     commentInputSide.value !== '' ? commentInputBtn.classList.add("activeLogin") && commentInputBtn.classList.remove('inactiveLogin') : commentInputBtn.classList.add('inactiveLogin') && commentInputBtn.classList.remove("activeLogin") 
-// }
-if(commentInputSide.value !== ''){
-    commentInputBtn.classList.add("activeLogin") 
-    commentInputBtn.classList.remove('inactiveLogin')
-    console.log("sdsdf"); 
-}else {
-    commentInputBtn.classList.add('inactiveLogin')
-    commentInputBtn.classList.remove("activeLogin") 
-    console.log("123");
-}
+
+    if (commentInputSide.value !== '') {
+        commentInputBtn.classList.add("activeLogin")
+        commentInputBtn.classList.remove('inactiveLogin')
+
+    } else {
+        commentInputBtn.classList.add('inactiveLogin')
+        commentInputBtn.classList.remove("activeLogin")
+
+    }
 }
 
+// 댓글 게시
 function inputComment(e) {
-    checkComment() 
+    checkComment()
 
     if ((commentInputSide.value !== '' && e.target == commentInputBtn) || (commentInputSide.value !== '' && e.keyCode === 13)) {
 
@@ -38,21 +37,107 @@ function inputComment(e) {
     }
 }
 
-const icons = [
-    {iconImage : "home"},
-    {iconImage : "direct"},
-    {iconImage : "explore"},
-    {iconImage : "heart"},
-    {iconImage : "profile"}
+commentInputSide.addEventListener('keydown', inputComment);
+commentInputBtn.addEventListener('click', inputComment);
+
+const icons = [{
+        iconImage: "home"
+    },
+    {
+        iconImage: "direct"
+    },
+    {
+        iconImage: "explore"
+    },
+    {
+        iconImage: "heart"
+    },
+    {
+        iconImage: "profile"
+    }
 ]
 
 const iconGroup = document.getElementById('icon-group')
+const storyGroup = document.getElementsByClassName('story-place')[0]
+const commendGroup = document.getElementById('commend-group');
+let startNum = 9
 
-icons.map((icon)=> iconGroup.innerHTML += `
+// nav바 오른쪽 상단 아이콘 삽입
+icons.map((icon) => iconGroup.innerHTML += `
     <li class="icon-menu">
     <img src="./image/${icon.iconImage}.png" alt="${icon.iconImage}">
     </li>
 `)
 
-commentInputSide.addEventListener('keydown', inputComment);
-commentInputBtn.addEventListener('click', inputComment);
+// 스토리 삽입
+const storys = [{
+        personalStory: "eunbary_"
+    },
+    {
+        personalStory: "soaya_"
+    },
+    {
+        personalStory: "geemsuf"
+    },
+    {
+        personalStory: "dreamfeels"
+    },
+    {
+        personalStory: "geemsuf"
+    },
+    {
+        personalStory: "dodam_..."
+    },
+    {
+        personalStory: "jeo_mi.."
+    },
+    {
+        personalStory: "summer"
+    }
+]
+
+storys.map((story, index) => storyGroup.innerHTML +=
+    ` <div class="personal-story">
+<div class="story-image">
+    <img src="./image/${index+1}.jpg" alt="${index+1}">
+</div>
+<p class="normal story-name">${story.personalStory}</p>
+</div>`)
+
+
+// 친구 추천 리스트
+const commends = [
+    {
+        commendId : "honey081012", currentFriend : "xoxo"
+    },
+    {
+        commendId : "3__ming" , currentFriend : "inspire__"
+    },
+    {
+        commendId : "dooreplay" , currentFriend : "love_stbt"
+    },
+    {
+        commendId : "g_0_2" , currentFriend : "exdoeun"
+    },
+    {
+        commendId : "ohtaytayo" , currentFriend : "h.maniii_"
+    }
+]
+
+commends.map((commend,index) => commendGroup.innerHTML +=
+    `
+<li class="recommend">
+    <div class="other-profile">
+        <div class="other-profile-photo">
+        <img src="./image/${startNum+index}.jpg" alt="${startNum+index}">
+        </div>
+        <div class="other-account-info">
+        <p class="bold">${commend.commendId}</p>
+        <p class="normal">${commend.currentFriend}님 외 ${Math.floor((Math.random() * 15) + 1)}명이 팔로우합니다</p>
+        </div>
+    </div>
+        <div class="follow-btn">
+        <button>팔로우</button>
+        </div>
+</li>
+`)
