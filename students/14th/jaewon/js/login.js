b33c30fe74e58b36d936548b5a57202bdd523fa7
appWon <korkrip11@gmail.com>
2020-10-31 22:19:24 +0900

    필수 구현항목 MISSION8 , MISSION7 , MISSION6 추가 및 검색 리스트 데이터 추가

--- students/14th/jaewon/js/login.js
+++ students/14th/jaewon/js/login.js
@@ -1,33 +1,33 @@
-let chang_img = (x) => setTimeout(()=>{
+let chang_img = (cnt) => setTimeout(()=>{
     let img = document.querySelectorAll('.chang_img');
-    if(x === 0){
+    if(!cnt){
         img[2].classList.toggle('fade');
-        x = 2
+        cnt = 2
     }else{
-        img[x].classList.toggle('fade');
+        img[cnt].classList.toggle('fade');
     }
-    chang_img(x-1)
+    chang_img(cnt-1)
 },3000)
 
 window.onload = () => {
     chang_img(2);
 
-    const check_id = document.querySelector('#id_form');
-    const check_pwd = document.querySelector('#pwd_form');
-
-    check_id.addEventListener('keyup' ,(e)=>{
-        const help_id = document.querySelector('#id_form span');
-        const id_value = document.querySelector('#id');
+    const check_id = document.querySelector('.id_form');
+    const check_pwd = document.querySelector('.pwd_form');
+    
+    check_id.addEventListener('input' ,(e)=>{
+        const help_id = document.querySelector('.id_form span');
+        const id_value = document.querySelector('.input_id');
         const help_id_status = help_id.classList.contains('move_helper');
-        if(id_value.value != "" && !help_id_status){
+        if(id_value.value && !help_id_status){
             help_id.classList.toggle('move_helper');
             id_value.classList.toggle('move_input');
-        }else if(id_value.value == "" && help_id_status){
+        }else if(!id_value.value && help_id_status){
             help_id.classList.toggle('move_helper');
             id_value.classList.toggle('move_input');
         }
 
-        const pwd_value = document.querySelector('#pwd');
+        const pwd_value = document.querySelector('.input_pwd');
         let check_id = id_value.value.length >= 1 ? true : false;
         let check_pwd = pwd_value.value.length >= 6 ? true : false;
         let check_class = document.querySelector('.login_btn').classList;
@@ -38,22 +38,22 @@ window.onload = () => {
         }
     });
     check_id.addEventListener('click',(e)=>{
-        const id_value = document.querySelector('#id_form input');
+        const id_value = document.querySelector('.id_form input');
         id_value.focus();
     });
     
-    check_pwd.addEventListener('keyup' ,(e)=>{
-        const help_id = document.querySelector('#pwd_form span');
-        const pwd_value = document.querySelector('#pwd');
+    check_pwd.addEventListener('input' ,(e)=>{
+        const help_id = document.querySelector('.pwd_form span');
+        const pwd_value = document.querySelector('.input_pwd');
         const help_id_status = help_id.classList.contains('move_helper');
-        if(pwd_value.value != "" && !help_id_status){
+        if(pwd_value.value && !help_id_status){
             help_id.classList.toggle('move_helper');
             pwd_value.classList.toggle('move_input');
-        }else if(pwd_value.value == "" && help_id_status){
+        }else if(!pwd_value.value && help_id_status){
             help_id.classList.toggle('move_helper');
             pwd_value.classList.toggle('move_input');
         }
-        const id_value = document.querySelector('#id');
+        const id_value = document.querySelector('.input_id');
         let check_id = id_value.value.length >= 1 ? true : false;
         let check_pwd = pwd_value.value.length >= 6 ? true : false;
         let check_class = document.querySelector('.login_btn').classList;
@@ -64,10 +64,7 @@ window.onload = () => {
         }
     });
     check_pwd.addEventListener('click',(e)=>{
-        const in_txt_box = document.querySelector('#pwd_form input');
+        const in_txt_box = document.querySelector('.pwd_form input');
         in_txt_box.focus();
     })
-
-
 }
-
