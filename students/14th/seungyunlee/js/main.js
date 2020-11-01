111cdf392235d31b8e41a4ff029378b4ab99629a
yoonl <kyouk12a@gmail.com>
2020-11-02 01:04:42 +0900

    댓글추가,삭제 기능  구현

--- students/14th/seungyunlee/js/main.js
+++ students/14th/seungyunlee/js/main.js
@@ -12,6 +12,45 @@ mention.addEventListener("keyup", function () {
   }
 });
 
+enter.addEventListener("click", function () {
+  if (enter.disabled === false) {
+    let comment = document.getElementsByClassName("menTion")[0];
+    let makeDiv = document.createElement("div"); //하나의 리스트를 만듦
+    let makeUser = document.createElement("span"); //댓글을 단 사용자의 이름을 감싸는 span태그를 하나 만듦
+    let makeMsg = document.createElement("span"); // 댓을 내용을 감싸는 span태그
+    let makeBtn = document.createElement("BUTTON"); // 댓글 삭제기능을 담당할 버튼
+
+    makeDiv.className = "commentDiv";
+    makeUser.className = "commentUser";
+    makeMsg.className = "commentMsg";
+    makeBtn.className = "deleteBtn";
+
+    makeMsg.innerHTML = mention.value;
+    makeUser.innerHTML = "lee.sy";
+    makeBtn.innerHTML = "X";
+
+    comment.appendChild(makeDiv);
+    makeDiv.appendChild(makeUser);
+    makeDiv.appendChild(makeMsg);
+    makeDiv.appendChild(makeBtn);
+
+    makeBtn.addEventListener("click", (event) => {
+      // 삭제버튼을 클릭하면 이벤트 발생
+
+      const btn = event.target; // => 그 이벤트 발생의 'target'을 btn 변수에 선언
+      console.log(btn); // 콘솔창을 띄우면 버튼 태그가 출력됨
+
+      const deleteAll = btn.parentNode; // btn의 부모 노드 => commentDiv(버튼 태그를 감싸는 상위태그)
+
+      comment.removeChild(deleteAll);
+      // commentDiv를 감싸는 상위 태그 => comment,  comment의 자식 노드를 삭제한다 => deleteAll을(commentDiv태그를)
+
+      // 결과적으로 삭제버튼을 클릭하면 그 삭제버튼을 포함하는 부모태그 commentDiv 자체가 지워져서
+      // 원하는 댓글을 삭제할 수 있다.
+    });
+  }
+});
+
 // enter.addEventListener("click", uplodeCooment);
 
 // function uplodeCooment() {
@@ -39,26 +78,26 @@ mention.addEventListener("keyup", function () {
 //   }
 // }
 
-enter.addEventListener("click", function () {
-  if ((enter.disabled = false)) {
-    let comment = document.getElementsByClassName("menTion");
-    let makeDiv = document.createElement("div"); //하나의 리스트를 만듦
-    let makeUser = document.createElement("span"); //댓글을 단 사용자의 이름을 감싸는 span태그를 하나 만듦
-    let makeMsg = document.createElement("span"); // 댓을 내용을 감싸는 span태그
-    let makeBtn = document.createElement("BUTTON"); // 댓글 삭제기능을 담당할 버튼
+// enter.addEventListener("click", function () {
+//   if ((enter.disabled = false)) {
+//     let comment = document.getElementsByClassName("menTion");
+//     let makeDiv = document.createElement("div"); //하나의 리스트를 만듦
+//     let makeUser = document.createElement("span"); //댓글을 단 사용자의 이름을 감싸는 span태그를 하나 만듦
+//     let makeMsg = document.createElement("span"); // 댓을 내용을 감싸는 span태그
+//     let makeBtn = document.createElement("BUTTON"); // 댓글 삭제기능을 담당할 버튼
 
-    makeDiv.classNeame = "commentDiv";
-    makeUser.className = "commentUser";
-    makeMsg.className = "commentMsg";
-    makeBtn.className = "deleteBtn";
+//     makeDiv.classNeame = "commentDiv";
+//     makeUser.className = "commentUser";
+//     makeMsg.className = "commentMsg";
+//     makeBtn.className = "deleteBtn";
 
-    makeMsg.innerHTML = mention.value;
-    makeUser.innerHTML = "lee.sy";
-    makeBtn.innerHTML = "X";
+//     makeMsg.innerHTML = mention.value;
+//     makeUser.innerHTML = "lee.sy";
+//     makeBtn.innerHTML = "X";
 
-    comment.appendChild(makeDiv);
-    makeDiv.appendChild(makeUser);
-    makeDiv.appendChild(makeMsg);
-    makeDiv.appendChild(makeBtn);
-  }
-});
+//     comment.appendChild(makeDiv);
+//     makeDiv.appendChild(makeUser);
+//     makeDiv.appendChild(makeMsg);
+//     makeDiv.appendChild(makeBtn);
+//   }
+// });
