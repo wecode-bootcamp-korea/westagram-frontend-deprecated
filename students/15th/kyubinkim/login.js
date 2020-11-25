f067809ff7dfc9836fb5604658cbd44191e5e444
9bin08 <9bin08@naver.com>
2020-11-25 20:32:10 +0900

    Add: Mission 5 구현 완료

--- students/15th/kyubinkim/login.js
+++ students/15th/kyubinkim/login.js
@@ -1,11 +1,11 @@
 const info = document.getElementById("info");
 const pass = document.getElementById("pass");
 
-document.addEventListener("keyup", function (e) {
-  if (info.value !== "") {
-    console.log(info.value);
-    if (pass.value !== "") {
-      console.log(pass.value);
+document.addEventListener("keyup", function () {
+  //info 빈칸 이나 @ 포함
+  if (info.value !== "" && info.value.match("@")) {
+    //pass 5자 이상
+    if (pass.value.length >= 5) {
       let changeColor = document.getElementById("button");
       changeColor.style.backgroundColor = "#0095F6";
     }
--- students/15th/kyubinkim/main.js
+++ students/15th/kyubinkim/main.js
@@ -2,13 +2,14 @@ const list = document.getElementById("list");
 const button = document.getElementById("button");
 const comment = document.getElementById("textarea");
 
-comment.addEventListener("keydown", function (e) {
-  const init = comment.value;
-});
+// comment.addEventListener("keydown", function (e) {
+//   e = comment.value;
+// });
+const inputValue = (e) => {
+  comment.value;
+};
 
-function pushcomment(init) {
-  console.log(init);
-
+function pushcomment() {
   const li = document.createElement("li");
   li.innerHTML = `<li><span>UserName</span> ${comment.value}</li>`;
   document.getElementById("list").appendChild(li);
