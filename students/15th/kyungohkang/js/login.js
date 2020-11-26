0efb9d8fdc1104d451f2422adad85211f6f51aa8
강경오 <gang-gyeong-o@gang-gyeong-oui-MacBookPro.local>
2020-11-26 13:20:35 +0900

    login page JavaScript 완료

--- students/15th/kyungohkang/js/login.js
+++ students/15th/kyungohkang/js/login.js
@@ -1,18 +1,14 @@
+let loginbutton = document.querySelector('.loginbtn');
 
-let loginButton = document. getElementsByClassName('.loginbtn')[0];
-
-input.addEventListener('keypress', function(){
-
-    let idbox = document.getElementsByClassName('.txtbox')[0].value;
-    let passwordbox = document.getElementsByClassName('.passbox')[0].value;
-    
-    if(idbox!==null && passwordbox!==null){
-        loginButton.style.backgroundColor = 'blue';
-        return loginButton;
+let id = document.getElementsByClassName('txtbox')[0];
+let password = document.getElementsByClassName('passbox')[0];
+   
+(id,password).addEventListener('keyup', function(){
+    if(id.value !== null && password.value !== null){
+        loginbutton.style.backgroundColor = 'blue';
+        return;
     }
-
-let test = document.getElementsByTagName('header');
-test.innerHTML = 'adfddafsd';
-let par = document.getElementsByClassName('loginpage');
-par.appendChild(test);
 })
+
+
+
