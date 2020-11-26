b77a9c1a6c81fbba52413fc2732cd50898808ffd
Haiin05 <69622073+Haiin05@users.noreply.github.com>
2020-11-26 15:21:58 +0900

    필수구현사항 완료

--- students/15th/HaiinKim/js/main.js
+++ students/15th/HaiinKim/js/main.js
@@ -1,14 +1,44 @@
+//함수 분리하기
 
 
-const input = document.querySelector('.mainFeedCommentBoxInput')
-const btn = document.querySelector('.mainFeedBtn')
+const form = document.querySelector('.mainFeedCommentBox')
+const input = document.querySelector('.mainFeedCommentBoxInput');
+const id = document.querySelector('.mainFeedContent_name');
+const ul = document.querySelector('.mainFeedComments');
+console.log(ul)
+const li = document.createElement('li');
+li.classList.add('new_comment');
+const span1 = document.createElement('span');
+const span2 = document.createElement('span');
+span1.className = 'comment_user';
+span2.className = 'comment_comm';
+// span1.classList.add('comment_user')
+// span2.classList.add('comment_comm')
+console.log(span1, span2)
 
-input.addEventListener('keyup', moreThanOne)
+//쿼리셀렉터는 기존에 있던 코드를 셀렉해주는거다. 내가 지금만든 태그를 다시 셀렉해줄필요 없다는거!!
+//console.log(userSpan, commSpan)
 
-function moreThanOne () {
-    const comment = input.value;
+form.addEventListener('submit', newComment)
+
+
+function newComment (event) {
+    event.preventDefault();
+    span1.innerText = 'Haiin.k';
+    span2.innerHTML = input.value;
+    li.append(span1);
+    li.append(span2);
+    ul.appendChild(li);
+    input.value = "";
+} 
+// function userSpan () {
+//     const userNameSpan = document.createElement('span');
+//     const list = document.createElement('li');
+//     list.setAttribute('class', 'newComment');
+//     list.appendChild(userNameSpan);
+
+
    
-    if (comment.length >= 1) {
-        btn.style.color = "rgb(44, 134, 224)"
-    } 
-}
+    // if (comment.length >= 1) {
+    //     btn.style.color = "rgb(44, 134, 224)"
+    // };
