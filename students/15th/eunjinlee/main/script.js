bfc57ceaff2318da87f5e4a659957d26ba211b3f
Eunjin Lee <reginalee@Eunjins-MacBook-Pro-8.local>
2020-11-26 11:12:45 +0900

    댓글 삭제버튼 구현 완료

--- students/15th/eunjinlee/main/script.js
+++ students/15th/eunjinlee/main/script.js
@@ -1,6 +1,7 @@
 const commentForm = document.getElementById('comment-form')
 const commentInput = document.getElementById('comment-input')
 const commentBtn = document.getElementById('comment-btn')
+const commentDeleteBtn = document.getElementById('delete-btn')
 
 const commentList = document.getElementById('comment__list')
 
@@ -24,15 +25,16 @@ const addCommentsInLocalStorage = () => {
 }
 
 //add localstorage comments on the html one by one
-const addCommentDOM = (input) => {
+const addCommentDOM = (comment) => {
   const commentEl = document.createElement('li')
-  if (input.value !== '') {
+  if (comment.text !== '') {
     commentEl.classList.add('comment__content-list')
     commentEl.innerHTML =''
     commentEl.innerHTML = `
     <div class="comment__content-box">
       <p class="comment__user user-link">workoutbutlazy</p>
-      <p class="comment__content">${input}</p>
+      <p class="comment__content">${comment.text}</p>
+      <i class="comment__content-delete fas fa-times" id="delete-btn" onclick="removeComment(${comment.id})"></i>
     </div>
     <img src="../images/heart.png" alt="Comment heart" class="comment__heart"></img>
     `
@@ -42,14 +44,15 @@ const addCommentDOM = (input) => {
 
 //remove comments by id
 const removeComment = (id) => {
+  commentList.innerHTML='' 
   comments = comments.filter((comment) => comment.id !== id)
+  addCommentsInLocalStorage()
   renderComments()
-  localStorage.setItem('transactions', JSON.stringify(transactions))
 }
 
 //render each comments from localstorage when refreshed
 const renderComments = () => { 
-  comments.forEach(comment => addCommentDOM(comment.text))
+  comments.forEach(comment => addCommentDOM(comment))
 }
 
 //activating login button
@@ -76,3 +79,4 @@ commentForm.addEventListener('keyup', activateLoginBtn)
 
 //window load event
 window.addEventListener('load', renderComments)
+
