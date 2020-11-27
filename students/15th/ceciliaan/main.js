9b733c811bf75bc3589fc32f0a52970c49bf743c
devCecy <im.ceciliaan@gmail.com>
2020-11-27 17:21:01 +0900

    Add: 메인페이지 댓글이벤트 작업 완료

--- students/15th/ceciliaan/main.js
+++ students/15th/ceciliaan/main.js
@@ -3,23 +3,14 @@ const commentBtn = document.querySelector('.commentBtn');
 const commentBox = document.querySelector('.commentBox');
 const uploadComment = document.querySelector('.uploadComment');
 
-//엔터로 댓글 삽입
+//엔터로 댓글 삽입 
 function onKeyDown(){
     if(event.keyCode == 13){
     commentHandler()
     }
 }
 
-//버튼 클릭으로 댓글 삽입 
-commentBtn.addEventListener('click', commentHandler);
-
-function commentHandler(){
-    // event.preventDefault();
-    const commentBoxValue = commentBox.value;
-    addComment(commentBoxValue);
-    commentBox.value = "";
-}
-
+//버튼 클릭으로 댓글 삽입 및 삭제 
 function addComment(text){
     const li = document.createElement('li');
     const span = document.createElement('span');
@@ -29,4 +20,22 @@ function addComment(text){
     uploadComment.appendChild(li);
 }
 
+function commentHandler(){
+    // event.preventDefault();
+    const commentBoxValue = commentBox.value;
+    addComment(commentBoxValue);
+    commentBox.value = "";
+}
+
+// function loadComment (){
+//     const loadedComments = localStorage.getItem();
+//     if (loadedComments !== null){  //localStorage에서 가져올 아이템이 있다면, 
+        
+//     }
+
+function init(){
+    commentBtn.addEventListener('click', commentHandler);
+}
+
+init();
 
