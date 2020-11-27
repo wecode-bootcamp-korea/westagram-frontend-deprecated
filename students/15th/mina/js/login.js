1ccc2de894195fe25be008713530eed35b0e8601
mina <yegrinaaa@gmail.com>
2020-11-27 20:40:40 +0900

    js 아이디/비번 value 있으면 버트활성화 되지만 값이 사라졌을 경우 비활성화 되지 않음.

--- students/15th/mina/js/login.js
+++ students/15th/mina/js/login.js
@@ -1,7 +1,7 @@
 const loginId = document.getElementsByClassName('login_id')[0];
 const loginPassword = document.getElementsByClassName('login_password')[0];
 const loginButton = document.getElementsByClassName('login_btn')[0];
-const input = document.getElementsByClassName('input')[0];
+const inputwrap = document.getElementsByClassName('inputwrap')[0];
 
 function buttonChangeColor(){    
     if( loginId.value && loginPassword.value ){
@@ -12,7 +12,8 @@ function buttonChangeColor(){
 }
 buttonChangeColor();
 
-input.addEventListener('keyup',buttonChangeColor);
+inputwrap.addEventListener('keyup',buttonChangeColor);
+
 
 
 
