0933a1806fc99c6e27d77422e4de6e06a9706222
김영광 <kim-young-gwang@gim-yeong-gwang-ui-MacBookPro.local>
2020-11-29 11:19:12 +0900

    메인페이지 추가기능구현중..

--- students/15th/김영광/main.js
+++ students/15th/김영광/main.js
@@ -1,10 +1,48 @@
-const addBtn = document.querySelector('.add');
-const addComment = document.getElementsByClassName('feed_contents')[0];
+const addForm = document.getElementsByClassName('add-form')[0];
 const addInput = document.getElementsByClassName('add_input')[0];
+const addComment = document.getElementsByClassName('feed_contents')[0];
+const commentLs = 'currentComment'
 
-addBtn.addEventListener('click',(e)=>{
+function saveComment(text) {
+    localStorage.setItem(commentLs,text);
+}
+
+function loadComment(){
+    const currentComment = localStorage.getItem(commentLs)
+    if(currentComment !== null) {
+        currentComment.forEach((e)=>{
+            return currentComment.text;
+        })
+    }
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
     const addLi = document.createElement('li');
     addLi.innerHTML = addInput.value;
     addComment.appendChild(addLi);
-    addInput.value = "";
-})
+    addInput.value='';
+    saveComment(addLi);
+    
+}
+
+function init(){
+    addForm.addEventListener('submit',handleSubmit);
+
+}
+
+init();
+
+
+// const addBtn = document.querySelector('.add');
+// const addComment = document.getElementsByClassName('feed_contents')[0];
+// const addInput = document.getElementsByClassName('add_input')[0];
+
+// addBtn.addEventListener('click',(e)=>{
+//     const addLi = document.createElement('li');
+//     addLi.innerHTML = addInput.value;
+//     addComment.appendChild(addLi);
+//     addInput.value = "";
+//     })
+
+    
