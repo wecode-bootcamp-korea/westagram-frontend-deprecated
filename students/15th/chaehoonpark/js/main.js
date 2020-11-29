20e5f3c72c87bc36cc455ae2f879e39b24742174
unknown <xpmxf5@gmail.com>
2020-11-29 19:44:18 +0900

    js 댓글추가 기능 완료

--- students/15th/chaehoonpark/js/main.js
+++ students/15th/chaehoonpark/js/main.js
@@ -1,31 +1,43 @@
-const thisIsComment = document.querySelector('.article-comment');
-const thisIsButton = document.querySelector('.article-comment-button');
+const thisIsComment = document.querySelector(".article-comment");
+const thisIsButton = document.querySelector(".article-comment-button");
+const thisIsUsername = document.querySelector(".username-bold");
 
+thisIsComment.addEventListener("keyup", function (event) {
+  const thisIsCommentValue = thisIsComment.value;
 
-thisIsComment.addEventListener("keyup", function(event){
+  if (!thisIsCommentValue) {
+    thisIsButton.setAttribute("disabled", true);
+    thisIsButton.style.color = "#B3DFFC";
+  } else {
+    thisIsButton.removeAttribute("disabled");
+    thisIsButton.style.color = "#0095f6";
+  }
+});
 
-    const thisIsCommentValue = thisIsComment.value;
-    console.log(thisIsCommentValue);
+thisIsComment.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault;
+    thisIsButton.click();
+  }
+});
 
-    if(!thisIsCommentValue){
-        thisIsButton.setAttribute("disabled",true);
-        thisIsButton.style.color = '#B3DFFC';
-    }else{
-        thisIsButton.removeAttribute("disabled");
-        thisIsButton.style.color = '#0095f6';
-    }
+thisIsButton.addEventListener("click", function (event) {
+  let thisIsCommentValue = thisIsComment.value;
 
-    console.log(event); 
+  let commentToBeAdded = document.createElement("div");
+  let commentContext = document.createTextNode(
+    thisIsUsername.textContent + thisIsCommentValue
+  );
 
-    if(event.keyCode === 13){
-        console.log(event.keyCode);
-        event.preventDefault;
-        thisIsButton.click();
-    }
-})
+  commentToBeAdded.appendChild(commentContext);
 
-thisIsButton.addEventListener('clicked',function(event){
-    
-} )
+  const element = document.querySelector(".article-fifth-row");
 
+  element.appendChild(commentToBeAdded);
+});
 
+thisIsButton.addEventListener("click", function () {
+  thisIsComment.value = "";
+  thisIsButton.setAttribute("disabled", true);
+  thisIsButton.style.color = "#B3DFFC";
+});
