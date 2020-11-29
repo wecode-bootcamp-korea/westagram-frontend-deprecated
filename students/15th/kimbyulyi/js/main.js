0f74854362873e3bdace819babd0276c65ab8542
kimbyulyi <byulyikeem@gmail.com>
2020-11-29 23:46:54 +0900

    Fix : 좋아요, 삭제 기능 수정

--- students/15th/kimbyulyi/js/main.js
+++ students/15th/kimbyulyi/js/main.js
@@ -1,65 +1,70 @@
 "user strict";
 
-const commentList = document.getElementsByClassName("comments");
-const cmtLikeBtn = document.getElementsByClassName("comment__like");
-const cmtDeleteBtn = document.getElementsByClassName("comment__delete");
-let likeCheck = false;
+const commentList = document.getElementsByClassName("comments")[0];
+let check = false;
 
-const clickDeleteBtn = (i) => {
-  const parent = cmtDeleteBtn[i].parentNode;
-  parent.parentNode.removeChild(parent);
+const deleteComment = () => {
+  const cmtDelList = document.querySelectorAll(".comment__delete");
+  cmtDelList.forEach((event) => {
+    event.addEventListener("click", () => {
+      const parent = event.parentElement;
+      parent.remove();
+    });
+  });
 };
 
-const clickLikeBtn = (i) => {
-  if (likeCheck === false) {
-    likeCheck = true;
-    cmtLikeBtn[i].childNodes[1].src = "img/heart_fill.png";
-  } else {
-    likeCheck = false;
-    cmtLikeBtn[i].childNodes[1].src = "img/heart.png";
-  }
+const likeComment = () => {
+  const cmtLikeList = document.querySelectorAll(".comment__like");
+  cmtLikeList.forEach((event) => {
+    event.addEventListener("click", () => {
+      let likeImg = event.querySelectorAll(".like__img")[0];
+      console.log(check);
+      if (check) {
+        likeImg.src = "img/heart.png";
+        check = false;
+      } else {
+        likeImg.src = "img/heart_fill.png";
+        check = true;
+      }
+    });
+  });
 };
 
-const postComment = (i, event) => {
+const postComment = (event) => {
   event.preventDefault();
-  let cmtInput = document.getElementsByTagName("img");
-  if (cmtInput[i].value === "") {
+  let cmtInput = document.getElementsByClassName("comment__input")[0];
+  if (cmtInput.value === "") {
     return;
   } else {
-    let commentString = [
-      "<li>",
-      "<a>Boris</a>",
-      `<span class="comment__text">${cmtInput[i].value}</span>`,
-      `<span class="comment__like"><img src="img/heart.png" alt="like icon"/></span>`,
-      `</li>`,
-    ].join("");
+    let commentString = `
+      <li>
+      <a>Boris</a>
+      <span class="comment__text">${cmtInput.value}</span>
+      <span class="comment__like"><img src="img/heart.png" alt="like icon" class="like__img"/></span>
+      <span class="comment__delete"><img src="img/remove.png" alt="remove icon"/></span>
+      </li>
+    `;
     let domparser = new DOMParser();
     let doc = domparser
       .parseFromString(commentString, "text/html")
       .getElementsByTagName("body")[0].innerHTML;
-    commentList[i].insertAdjacentHTML("beforeend", doc);
-    cmtInput[i].value = "";
+    commentList.insertAdjacentHTML("beforeend", doc);
+    cmtInput.value = "";
   }
+  likeComment();
+  deleteComment();
 };
 
 const init = () => {
-  const cmtBtn = document.getElementsByClassName("comment__btn");
-  const cmtInput = document.getElementsByClassName("comment__input");
+  const cmtBtn = document.getElementsByClassName("comment__btn")[0];
+  const cmtInput = document.getElementsByClassName("comment__input")[0];
 
-  for (let i = 0; i < cmtBtn.length; i++) {
-    cmtBtn[i].addEventListener("click", (event) => postComment(i, event));
-    cmtInput[i].addEventListener("keyup", (event) => {
-      if (event.keyCode === 13) postComment(i, event);
-    });
-  }
-
-  for (let i = 0; i < cmtLikeBtn.length; i++) {
-    cmtLikeBtn[i].addEventListener("click", (event) => clickLikeBtn(i));
-  }
-
-  for (let i = 0; i < cmtDeleteBtn.length; i++) {
-    cmtDeleteBtn[i].addEventListener("click", (event) => clickDeleteBtn(i));
-  }
+  cmtBtn.addEventListener("click", (event) => postComment(event));
+  cmtInput.addEventListener("keyup", (event) => {
+    if (event.keyCode === 13) postComment(event);
+  });
+  likeComment();
+  deleteComment();
 };
 
 init();
