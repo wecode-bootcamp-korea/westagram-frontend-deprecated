5d00ce89328529f7976123dc8cd3320cc8b19d90
unknown <xpmxf5@gmail.com>
2020-11-30 12:50:04 +0900

    git 리뷰 받은 거 다 수정

--- students/15th/chaehoonpark/js/login.js
+++ students/15th/chaehoonpark/js/login.js
@@ -3,15 +3,11 @@ const thisIsPassword = document.getElementById("Password");
 const thisIsId = document.querySelector("#ID");
 const thisIsInputBox = document.querySelector(".input_and_button_box");
 
-
-
 thisIsInputBox.addEventListener("keyup", function () {
-
   const thisIsIdValue = thisIsId.value;
   const thisIsPasswordValue = thisIsPassword.value;
 
-
-  !!(thisIsIdValue && thisIsPasswordValue) ? (thisIsButton.style.backgroundColor = "#0095F6")
-  : (thisIsButton.style.backgroundColor = "#B2DFFC");
+  thisIsIdValue && thisIsPasswordValue
+    ? (thisIsButton.style.backgroundColor = "#0095F6")
+    : (thisIsButton.style.backgroundColor = "#B2DFFC");
 });
-
