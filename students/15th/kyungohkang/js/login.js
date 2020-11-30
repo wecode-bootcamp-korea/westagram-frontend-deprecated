a5bb8ba249af2f2cc4cec7f9590209ed49ca2cc5
강경오 <gang-gyeong-o@gang-gyeong-oui-MacBookPro.local>
2020-11-30 11:41:40 +0900

    수정사항 대부분 완료 로그인버튼 색 바꾸는거 고민중

--- students/15th/kyungohkang/js/login.js
+++ students/15th/kyungohkang/js/login.js
@@ -1,13 +1,17 @@
 let loginbutton = document.querySelector('.loginbtn');
-
 let id = document.getElementsByClassName('txtbox')[0];
 let password = document.getElementsByClassName('passbox')[0];
    
 (id,password).addEventListener('keyup', function(){
-    if(id.value !== null && password.value !== null){
-        loginbutton.style.backgroundColor = 'blue';
-        return;
+
+    if(id.value&&password.value !== null){
+        loginbutton.style.backgroundColor='blue';
     }
+    else{
+    id.value&&password.value === null ? loginbutton.style.backgroundColor='lightblue' : loginbutton.style.backgroundColor='blue';
+    }
+    return;
+
 })
 
 
