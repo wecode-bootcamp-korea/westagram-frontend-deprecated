7fb6e863fcd030c081cb9777bc0fb33c32dc256f
정민선 <minn602@gmail.com>
2020-12-24 09:04:42 +0900

    ADD : 메인 페이지 댓글 추가기능 구현중

--- students/16th/jungminsun/main/js/main.js
+++ students/16th/jungminsun/main/js/main.js
@@ -1,2 +1,39 @@
 'use strict';
 
+const commentBtns = document.querySelectorAll('.commentBtn'),
+      commentInput = document.querySelector('.commentInput'),
+      commentForm = document.querySelector('.commentForm');
+
+function addCommentHandler(evt) {
+  evt.preventDefault();
+  const inputVal = commentInput.value;
+  const commentList = document.querySelector('.commentsList');
+  const li = document.createElement('li');
+  li.classList.add('commentItem');
+  const aTag = document.createElement('a');
+  aTag.setAttribute("href", "#");
+  aTag.classList.add('userId');
+  aTag.innerText = 'usersssss';
+  li.appendChild(aTag);
+  const pTag = document.createElement('p');
+  pTag.innerText = inputVal;
+  li.appendChild(pTag);
+  const button = document.createElement('button');
+  button.setAttribute("type", "button");
+  button.innerHTML = `<img src="./assets/heart.png" alt="like" />`;
+  li.appendChild(button);
+  commentList.appendChild(li);
+  commentInput.value = '';
+}
+
+commentBtns.forEach((btn, idx) => {
+  btn.addEventListener('click', addCommentHandler);
+})
+
+function init() {
+  commentBtns.forEach((btn) => {
+    btn.addEventListener('click', addCommentHandler);
+  })
+}
+
+init();
