06df5f9f4a893169ddb17a03520ff80be070c36c
rlaqudwls157 <rlaqudwls157@gmail.com>
2020-12-25 20:29:37 +0900

    ID PW 유효성 검사 기능 구현 완료

--- students/16th/byungjinkim/login.js
+++ students/16th/byungjinkim/login.js
@@ -1,4 +1,4 @@
-const check_id = document.querySelector('#id_input');
+const check_id = document.querySelector('#id_input'); 
 const check_pw = document.querySelector('#pw_input');
 const btnLogin = document.querySelector('.btn_login');
 
@@ -12,5 +12,29 @@ function activeBtn(){
         btnLogin.classList.add('active');
     }else{
         btnLogin.classList.remove('active');
+    }
+}
+
+// ID, PW 유효성 검사
+function loginCheck(){
+    if(check_id.value == ""){
+        alert("ID를 입력해주세요!");
+        return false;
+    }else if(check_pw.value == ""){
+        alert("PW를 입력해주세요!");
+        return false;
+    }
+
+    if(check_id.value.length < 4){
+        alert("ID는 4글자 이상입니다!")
+        return false;
+    }else if(check_pw.value.length < 4){
+        alert("PW는 4자리 이상입니다!")
+        return false;
+    }
+
+    if(check_id.value.length == 4 && check_pw.value.length ==4){
+        alert("로그인 완료!");
+        return true;
     }
 }
