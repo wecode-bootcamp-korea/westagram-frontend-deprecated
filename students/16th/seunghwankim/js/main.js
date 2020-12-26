b28e7e3290f22c6b16448264f78cee7e929ff90b
김승환 <hwan960528@gmail.com>
2020-12-26 23:48:55 +0900

    Add: Enter key로 댓글 게시하는 기능, 공백 문자만 있으면 게시 불가 기능

--- students/16th/seunghwankim/js/main.js
+++ students/16th/seunghwankim/js/main.js
@@ -1,13 +1,30 @@
 const enrollBtn = document.getElementById('commentEnrollBtn');
+const commentInput = document.getElementById('commentInput');
+const commentBox = document.getElementsByClassName('comments')[0];
+
+commentInput.addEventListener('keydown', e => {
+    console.log(commentInput.value);
+    if ((e.key === 'Enter') && (commentInput.value.trim() !== '')) {
+        postComment();
+    }
+})
+
 enrollBtn.addEventListener('click', e => {
-    const commentBox = document.getElementsByClassName('comments')[0];
-    const commentCont = document.getElementById('commentInput');
+    if (commentInput.value.trim() !== '') {
+        postComment();
+    }
+})
+
+function postComment() {
     const newComment = document.createElement('div');
     const commentFrame = `
         <span>1k3h_ksh</span>
-        <span>${commentCont.value}</span>`;
+        <span>${commentInput.value}</span>`;
     newComment.className = 'comment_line_container';
     newComment.innerHTML = commentFrame;
     commentBox.appendChild(newComment);
-    commentCont.value = '';
-})
+    commentInput.value = '';
+}
+
+// enrollBtn.disabled = true;
+// enrollBtn.disabled = false;
