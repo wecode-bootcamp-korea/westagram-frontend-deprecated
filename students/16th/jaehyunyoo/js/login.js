f3174e6ffc694abcfd7df73a71a72557183586e7
Jaehyun <nike361@naver.com>
2020-12-26 14:31:37 +0900

    Add: Login 추가 기능 Validatuiib Check 구현 완료

--- students/16th/jaehyunyoo/js/login.js
+++ students/16th/jaehyunyoo/js/login.js
@@ -2,6 +2,7 @@ const loginContainer = document.getElementById('login__wrap');
 const userId = document.querySelector('.user__name');
 const userPassword = document.querySelector('.user__password')
 const onBtnabled = document.querySelector('.on__Submit');
+const errorMsg = document.querySelector('.user__validation');
 
 loginContainer.addEventListener('keyup',onLogin);
 
@@ -16,3 +17,20 @@ function onLogin(){
     onBtnabled.disabled = true;
   }
 }
+function onValidation(){
+  const user = userId.value;
+  const passwod = userPassword.value;
+  if(!(user === 'test')){
+    errorMsg.innerText = '잘못된 아이디 입니다.'
+    return;
+  } 
+  if(!(passwod === '1234')){
+    errorMsg.innerText = '잘못된 비밀번호 입니다.'
+    return;
+  }
+}
+onBtnabled.addEventListener('click',(event)=>{
+  event.preventDefault();
+  onValidation();
+  location.href = '../jaehyunyoo/main.html';
+});
