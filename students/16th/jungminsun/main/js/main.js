1a1fbd6fead24772d1c73eb5e02ae59fcbd2896e
정민선 <minn602@gmail.com>
2020-12-26 20:38:46 +0900

    ADD : 아이디 검색기능 구현 중

--- students/16th/jungminsun/main/js/main.js
+++ students/16th/jungminsun/main/js/main.js
@@ -39,6 +39,7 @@ function addCommentPaint(inputIdx, listIdx) {
   button.setAttribute("type", "button");
   button.classList.add("addLikeToCommentBtn");
   button.innerHTML = `<img src="./assets/heart.png" alt="like" />`;
+  button.addEventListener('click', addLikeHandler);
   li.appendChild(button);
   commentLists[listIdx].appendChild(li);
   commentInputs[inputIdx].value = '';
@@ -110,6 +111,108 @@ function openMenuBoxHandler() {
 }
 
 // 검색 기능
+const searchData = [
+  {
+    userId: 'moonjungwon',
+    userName: '문정원',
+    imgUrl: 'https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/s150x150/131999852_691820884851451_676644135174423410_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=CO1Fs0hDsIEAX9tyC1q&tp=1&oh=a1f011ccce35fac066ad4e58ef3684e0&oe=60115A3E'
+  },
+  {
+    userId: 'wedapluggrecords',
+    userName: 'WEDAPLUGG RECORDS',
+    imgUrl: 'https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/s150x150/83272907_834230477014689_2552974207654821888_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=s1OrJSj7t9IAX_l9896&tp=1&oh=6e3d3f4f5b87eaf49b75c3040f392f83&oe=6010446B'
+  },
+  {
+    userId:'wecanrun_officail',
+    userName: '위캔런코리아',
+    imgUrl: 'https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/s150x150/132093215_744431506161903_7154338458712395268_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=xZrRqkTTJwwAX-TQc9o&tp=1&oh=5c02089e9483449c7d47c40e8403a9c5&oe=600FE660'
+  },
+  {
+    userId: 'wecode_bootcamp',
+    userName: '>wecode | 위코드',
+    imgUrl: 'https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/s150x150/64219646_866712363683753_7365878438877462528_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=pHGOsRYRKv0AX9MjtG3&tp=1&oh=61669ec622c76c7c5c844e8b0ad8dd17&oe=601131F0'
+  },
+  {
+    userId: 'wecode_founder',
+    userName: '송은우 (Eun Woo Song)',
+    imgUrl: 'https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/s150x150/118404165_231868348195869_8580469197286780227_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=DRFc7USKRoQAX8Xld3z&tp=1&oh=e0cea04c1b025013da0982490d5e1351&oe=6010B5E2'
+  },
+  {
+    userId: 'we__code_',
+    userName: 'WE CODE',
+    imgUrl: 'https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/s150x150/118198537_171278331152188_6203529355182388111_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=mUl9skTkYiwAX9v1Hsw&tp=1&oh=56f20bd0f54a32fb16074f19b810a804&oe=60117FB9'
+  }
+]
+
+function paintSearchList(id, name, url) {
+  const ul = document.createElement('ul');
+  ul.classList.add('searchBoxList');
+  const li = document.createElement('li');
+  li.classList.add('searchBoxItem');
+  ul.appendChild(li);
+  const a = document.createElement('a');
+  a.setAttribute('href', '#');
+  li.appendChild(a);
+  const img = document.createElement('img');
+  img.setAttribute('src', url);
+  a.appendChild(img);
+  const div = document.createElement('div');
+  div.classList.add('accountInfoContainer');
+  const p = document.createElement('p');
+  p.classList.add('searchId');
+  p.innerText = id;
+  const span = document.createElement('span');
+  span.innerText = name;
+  div.appendChild(p);
+  div.appendChild(span);
+  a.appendChild(div);
+  const searchBox = document.querySelector('.searchBox');
+  searchBox.appendChild(ul);
+}
+
+function noneOfResult() {
+  const ul = document.createElement('ul');
+  ul.classList.add('searchBoxList');
+  const li = document.createElement('li');
+  li.classList.add('searchBoxItem');
+  ul.appendChild(li);
+  const span = document.createElement('span');
+  span.innerText = '검색 결과가 없습니다';
+  li.appendChild(span);
+  const searchBox = document.querySelector('.searchBox');
+  searchBox.appendChild(ul);
+}
+
+function showSearchResult(e) {
+  const searchBox = document.querySelector('.searchBox');
+  searchBox.classList.remove('hide');
+  const value = e.target.value;
+  const result = searchData.filter((el) => {
+    const id = el.userId;
+    const name = el.userName;
+    const url = el.imgUrl;
+    if (el.userId.includes(value)) {
+      paintSearchList(id, name, url);
+    } else {
+      noneOfResult();
+    }
+  })
+  return result;
+}
+
+function hideSearchBox() {
+  const searchBox = document.querySelector('.searchBox');
+  window.addEventListener('click', (e) => {
+    if (e.target.parentElement.id !== 'searchArea') {
+      searchBox.classList.add('hide');
+    }
+  })
+}
+
+function activeSearchbar() {
+  const searchInput = document.querySelector('.searchInput');
+  searchInput.addEventListener('keydown', showSearchResult);
+}
 
 function init() {
   clickCommentBtn();
@@ -117,6 +220,8 @@ function init() {
   activeBtn();
   addLike();
   openMenuBox();
+  activeSearchbar();
+  hideSearchBox();
 }
 
 init();
