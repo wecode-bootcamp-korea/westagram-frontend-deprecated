812648154cf61747e81a4dd61ff5beb6158fa3f0
박영호 <qkrdudgh2701@gmail.com>
2020-12-26 20:24:04 +0900

    login 페이지 버튼 변화 구현

---
+++ students/16th/younghopark/js/login.js
@@ -0,0 +1,13 @@
+const userId = document.getElementsByClassName('userId')[0];
+const userPw = document.getElementsByClassName('userPw')[0];
+const logBtn = document.getElementsByClassName('loginBtn')[0];
+
+function changeColor(){
+    if(userId.value && userPw.value){
+        logBtn.removeAttribute('disabled');
+        logBtn.style.backgroundColor='#0095F6';
+    } 
+}
+
+userId.addEventListener('keyup', changeColor);
+userPw.addEventListener('keyup', changeColor);
