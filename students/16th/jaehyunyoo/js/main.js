81fd932fda01fb0afb1c5884bb7e741939aa7a43
Jaehyun <nike361@naver.com>
2020-12-28 08:48:49 +0900

    Modify:Comment 삭제 버튼 수정 영역외에서도 삭제되는 버그 수정 Modify: 전체 Layout 수정 완료 Add: 좋아요 기능 추가 Svg path Fill 사용하여 클릭시 class  추가 Add: header 프로필 메뉴 박스 추가 완료

--- students/16th/jaehyunyoo/js/main.js
+++ students/16th/jaehyunyoo/js/main.js
@@ -1,6 +1,8 @@
 const comments = document.querySelector('.feed__items');
 const input = document.querySelector('#comment__input');
 const addBtn = document.querySelector('.onSubmit');
+const profileShowMenu = document.querySelector('.navMenu__right');
+const body = document.querySelector("body");
 
 function onAddComment(){
   const text = input.value;
@@ -19,17 +21,16 @@ function createComment(text){
   row.setAttribute('class','comment__Like__Btn');
   row.setAttribute('data-id',id);
   row.innerHTML = `
-    <div class="comment__row">
+    <div class="comment__row" style="flex-grow: 1;">
       <span span class="user__name"><strong>raihyunYoo</strong></span>
       <span class="comment__content">${text}</span>
     </div>
-    <div class="like__delete">
-      <button type="button" class="like__btn">
-        <svg aria-label="활동 피드" class="_8-yf5 " fill="#262626" height="22" viewBox="0 0 48 48" width="22">
-          <path d="M34.6 6.1c5.7 0 10.4 5.2 10.4 11.5 0 6.8-5.9 11-11.5 16S25 41.3 24 41.9c-1.1-.7-4.7-4-9.5-8.3-5.7-5-11.5-9.2-11.5-16C3 11.3 7.7 6.1 13.4 6.1c4.2 0 6.5 2 8.1 4.3 1.9 2.6 2.2 3.9 2.5 3.9.3 0 .6-1.3 2.5-3.9 1.6-2.3 3.9-4.3 8.1-4.3m0-3c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5.6 0 1.1-.2 1.6-.5 1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path>
-        </svg>
+    <div class="like__delete" style="flex-grow: 0;">
+      <button type="button" class="like__btn" style="padding:0;">
+        <svg aria-label="좋아요" viewBox="0 0 48 48" style="display:block;">
+          <path class="heart" d="M34.6 3.1c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5s1.1-.2 1.6-.5c1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path></svg>
       </button>
-      <button type="button" >
+      <button type="button">
         <i class="far fa-trash-alt" data-id="${id}"></i>
       </button>
     </div>`;
@@ -60,4 +61,22 @@ comments.addEventListener('click', event => {
     const deleteRow = document.querySelector(`.comment__Like__Btn[data-id="${id}"]`);
     deleteRow.remove();
   }
+  if(event.target.classList.contains("heart__fill") == true){
+      event.target.classList.remove("heart__fill");
+  } else {
+    event.target.classList.add("heart__fill");
+  }
 });
+body.addEventListener('click', clickBodyEvent);
+
+function clickBodyEvent(event){
+  const target = event.target;
+  if(!(target == event.currentTarget.querySelector('.profile__Menu'))){
+    const hideSubMenu = document.querySelector(".sub__Menu");
+    hideSubMenu.classList.add('d_none');
+    return;
+  }
+  if(target.lastElementChild.classList.contains("d_none")){
+    target.lastElementChild.classList.remove("d_none");
+  } 
+}
