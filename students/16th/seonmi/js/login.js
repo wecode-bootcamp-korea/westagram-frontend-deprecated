78542071c4d72ce3a6e75fc972864b374ecca98f
정 선 미 <kmsun0720@naver.com>
2020-12-27 21:42:25 +0900

    Refactor: 스스로 리팩토링

--- students/16th/seonmi/js/login.js
+++ students/16th/seonmi/js/login.js
@@ -2,6 +2,8 @@ const findId = document.getElementsByClassName('IdInput')[0];
 const findPw = document.getElementsByClassName('PasswordInput')[0];
 const findLoginBtn = document.getElementsByClassName('loginBtn')[0];
 
-document.addEventListener('keyup', function () {
+document.addEventListener('keydown', function () {
     findLoginBtn.style.backgroundColor = findId.value && findPw.value ? '#0095F6':'#B2DFFC' ;
+    findLoginBtn.style.cursor = findId.value && findPw.value ? 'pointer':'default' ;
+    findLoginBtn.disabled = findId.value && findPw.value ? false : true ;
 });
