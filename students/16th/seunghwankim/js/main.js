4975f3e6570af36f16e202b001ce6a36c402190e
김승환 <hwan960528@gmail.com>
2020-12-27 15:14:34 +0900

    Modify: 게시 버튼 레이아웃 변경, 게시 완료 후 게시 버튼 비활성화 되도록 수정

--- students/16th/seunghwankim/js/main.js
+++ students/16th/seunghwankim/js/main.js
@@ -3,12 +3,15 @@ const commentInput = document.getElementById('commentInput');
 const commentBox = document.getElementsByClassName('comments')[0];
 enrollBtn.setAttribute('disabled', 'true');
 
+// Enter key로 댓글 게시
 commentInput.addEventListener('keydown', e => {
     if ((e.key === 'Enter') && (commentInput.value.trim() !== '')) {
+        enrollBtn.setAttribute('disabled', 'true');
         postComment();
     }
 })
 
+// 게시 버튼 활성화/비활성화 
 commentInput.addEventListener('input', e => {
     if (commentInput.value.trim() === '') {
         enrollBtn.setAttribute('disabled', 'true');
@@ -18,12 +21,14 @@ commentInput.addEventListener('input', e => {
     }
 })
 
+// 게시 버튼 클릭으로 댓글 게시
 enrollBtn.addEventListener('click', e => {
     if (commentInput.value.trim() !== '') {
         postComment();
     }
 })
 
+// 댓글 입력창 내용을 댓글창에 추가 시키는 함수
 function postComment() {
     const newComment = document.createElement('div');
     const commentFrame = `
@@ -34,6 +39,3 @@ function postComment() {
     commentBox.appendChild(newComment);
     commentInput.value = '';
 }
-
-// enrollBtn.disabled = true;
-// enrollBtn.disabled = false;
