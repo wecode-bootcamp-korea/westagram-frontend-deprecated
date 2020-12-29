485a43329fbe9e6bd3989dafdd4a5f6e5ffa78f6
임진영 <delheure29@gmail.com>
2020-12-29 18:57:22 +0900

    Add: 리뷰 수정

--- students/16th/jinyounglim/js/main.js
+++ students/16th/jinyounglim/js/main.js
@@ -1,38 +1,33 @@
-const btnTxtMore = document.querySelector('.btn_txt_more');
-const feedTxt = document.querySelector('.feeds_txt p');
+const btnTxtMore = document.querySelector(".btn_txt_more");
+const feedTxt = document.querySelector(".feeds_txt p");
 
-btnTxtMore.addEventListener('click', () => {
-  feedTxt.classList.add('all');
-  btnTxtMore.remove();
+btnTxtMore.addEventListener("click", () => {
+    feedTxt.classList.add("all");
+    btnTxtMore.remove();
 });
 
-const likes = document.querySelectorAll('.btn_heart');
-function likesActive() {
-  for (const like of likes) {
-    like.addEventListener('click', function () {
-      this.classList.toggle('on');
+const likes = document.querySelectorAll(".btn_heart");
+for (const like of likes) {
+    like.addEventListener("click", function () {
+        this.classList.toggle("on");
     });
-  }
 }
 
-const cmtBox = document.querySelector('.cmt_list');
-const cmtInput = document.querySelector('.cmt_write input');
-const cmtBtn = document.querySelector('.btn_submit');
+const cmtBox = document.querySelector(".cmt_list");
+const cmtInput = document.querySelector(".cmt_write input");
+const cmtBtn = document.querySelector(".btn_submit");
 
-cmtBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-  if (!cmtInput.value) {
-    return;
-  }
-  cmtBox.innerHTML = 
-  `<li>
+cmtBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (!cmtInput.value) {
+        return;
+    }
+    cmtBox.innerHTML = `<li>
         <p>
-            <strong class="user_id">${'delheure'}</strong>
+            <strong class="user_id">${"delheure"}</strong>
             ${cmtInput.value}
         </p>
         <button class="btn_heart btn_small btn_com">Like</button>
     </li>`;
-  cmtInput.value = '';
+    cmtInput.value = "";
 });
-
-document.addEventListener('click', likesActive);
