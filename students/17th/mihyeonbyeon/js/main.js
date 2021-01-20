950e5e621ae5b982eeabb2f1fb885090864c8908
mia <cocoball200@gmail.com>
2021-01-20 20:23:15 +0900

    Add: search function

--- students/17th/mihyeonbyeon/js/main.js
+++ students/17th/mihyeonbyeon/js/main.js
@@ -4,10 +4,9 @@ const $commentForm = document.querySelector(".comment-write-form");
 const $commentSubmitButton = document.querySelector(".comment-submit-button");
 const $inputSearchBar = document.querySelector(".input-search-bar");
 const $searchedIdDiv = document.querySelector(".searched-id-list");
-const IdList = ["ho_ho_ho", "like_birds_set_free", "chandelier", "snowman", "cheap_thrills", "courage_to_change", "titanium", "elastic_heart", "move_your_body", "hey_boy", "candy_cane_lane"];
-const dataIndex = 0;
 
-const $commentConatainer = document.querySelector(".comment-conatainer");
+const IdList = ["ho_ho_ho", "like_birds_set_free", "chandelier", "snowman", "cheap_thrills", "courage_to_change", "titanium", "elastic_heart", "move_your_body", "hey_boy", "candy_cane_lane"];
+
 const handleWriting = (event) => {
     if (event.keyCode === 13) {
         if (!event.shiftKey) {
@@ -18,7 +17,6 @@ const handleWriting = (event) => {
 };
 
 const handleSubmit = (event) => {
-
     const $eachCommentDiv = document.createElement("div");
     $eachCommentDiv.className = `each-comment-list`;
     $eachCommentDiv.innerHTML = [
@@ -40,24 +38,28 @@ const removeComment = (event) => {
 }
 
 const handleSearch = (event) => {
+    const declaredSearchTags = document.querySelectorAll(".searched-results");
+    if (declaredSearchTags) {
+        declaredSearchTags.forEach(item => item.remove())
+    };
+    const $searchResults = document.createElement("div");
+    $searchResults.className = "searched-results"
+    $searchResults.textContent = "";
     if (event.keyCode === 13) {
         if (!event.shiftKey) {
             event.preventDefault();
             const target = $inputSearchBar.value;
-            const targetResult = IdList.filter(item => item.indexOf(target) !== -1 && item);
-            const $searchResults = document.createElement("p");
-            $searchResults.textContent = `${targetResult}`
+            const targetResult = IdList.find(item => item.indexOf(target) !== -1);
+            if (targetResult) {
+                $searchResults.textContent = `${targetResult}`
+            } else {
+                $searchResults.textContent = `찾는 결과가 없습니다.`
+            }
             $searchedIdDiv.appendChild($searchResults);
         }
     }
-    $searchResults.textContent = "";
 }
-
-
 
 $commentInput.addEventListener("keydown", handleWriting);
 $commentSubmitButton.addEventListener("click", handleSubmit);
 $inputSearchBar.addEventListener("keydown", handleSearch);
-
-
-
