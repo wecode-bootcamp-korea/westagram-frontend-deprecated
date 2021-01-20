9f4d267472d3fee82cd84da13305ac12a890a9d9
이름 <awaji0829@gmail.com>
2021-01-21 00:51:03 +0900

    add : 로그인 버튼 색깔 기능 추가

--- students/17th/kangminji/js/westalogin.js
+++ students/17th/kangminji/js/westalogin.js
@@ -1,12 +1,35 @@
-let id = document.querySelector(".a");
-let password = document.querySelector(".b");
-let btn = document.querySelector(".c");
+const id =document.getElementById('id');
+const password = document.getElementById("password");
+const btn = document.getElementById('btn');
+
+password.addEventListener('keyup', function changcolor(event){
+
+const idvalue = document.getElementById('id').value;
+const passwordvalue = document.getElementById('password').value;
 
 
-id.addEventListener("keyup",function(){
+
+if(idvalue&&passwordvalue){
+btn.style.backgroundColor =  'blue';
+
+}
 
 
 
+});
 
+id.addEventListener('keyup', function changcolor(event){
 
-} )
+    const idvalue = document.getElementById('id').value;
+    const passwordvalue = document.getElementById('password').value;
+    
+    
+    
+    if(idvalue&&passwordvalue){
+    btn.style.backgroundColor =  'blue';
+    
+    }
+    
+    
+    
+    });
