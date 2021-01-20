c355e7eea2a52f6f07575fd3be220331d0970853
송빈호 <binogood68@gmai.com>
2021-01-20 17:23:51 +0900

    add: first commit test

---
+++ students/17th/binhosong/js/login.js
@@ -0,0 +1,7 @@
+const idInputButton = document.getElementsByClassName('id_input')
+const pwInputButton = document.getElementsByClassName('pw_input')
+const loginInputButton = document.getElementsByClassName('login_bt')
+
+function ()
+
+loginInputButton.addEventListener('keyup', )
