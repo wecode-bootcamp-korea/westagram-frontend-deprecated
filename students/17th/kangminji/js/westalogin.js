9095c8ff61817458a47759555978b3db95e65851
이름 <awaji0829@gmail.com>
2021-01-20 17:23:51 +0900

    Add: 초기 세팅

---
+++ students/17th/kangminji/js/westalogin.js
@@ -0,0 +1,12 @@
+let id = document.querySelector(".a");
+let password = document.querySelector(".b");
+let btn = document.querySelector(".c");
+
+
+id.addEventListener("keyup",function(){
+
+
+
+
+
+} )
