6a3f0bf3b73a7376d72c106df2d1eb2b24f0cb61
93jm <65124480+93jm@users.noreply.github.com>
2021-01-21 20:57:55 +0900

    Modify : reply good

---
+++ students/17th/leejungmin/js/main.js
@@ -0,0 +1,41 @@
+const replyInput = document.querySelector('.content_peed_reply');
+const replySubmit = document.querySelector('.content_peed_submit');
+const replyList = document.querySelector('.content_peed_text');
+
+
+function colorChange() {
+    
+    if(replyInput.value){
+        replySubmit.removeAttribute('disabled');
+        replySubmit.style.color = '#0095f6';
+    }else {
+        replySubmit.setAttribute('disabled', 'true');
+        replySubmit.style.color = '#B2DFFC';
+    }
+}
+
+
+
+function replyEnterAdd(event) {
+    if(event.keyCode === 13 && replyInput.value){
+        // event.preventDefault();
+        const replyP = document.createElement('p');
+        replyP.innerHTML = 'meeeeen93 ' + replyInput.value;
+        replyList.append(replyP);
+        // replyList.append('meeeeen93 '+replyInput.value);
+        replyInput.value = null;
+    }
+}
+
+function replyClickAdd() {
+    if(replyInput.value){
+        replyList.append('meeeeen93 '+replyInput.value);
+        replyInput.value = null;
+    }
+}
+
+
+
+replyInput.addEventListener('keyup', colorChange);
+replyInput.addEventListener('keydown', replyEnterAdd);
+replySubmit.addEventListener('click', replyClickAdd);
