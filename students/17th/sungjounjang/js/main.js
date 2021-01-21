b18ecbb06fd3764de10b0131aba2e25144d9b9f5
ique-coder <tjdwns0309@gmail.com>
2021-01-21 16:26:26 +0900

    Modify : 댓글 등록 엔터키로 등록하게 수정

--- students/17th/sungjounjang/js/main.js
+++ students/17th/sungjounjang/js/main.js
@@ -22,6 +22,45 @@ commentBtn.addEventListener('click', function() {
         commentValue.innerHTML = commentContents.value;
         feedComment.appendChild(commentValue);
 
+        let commentDeleteBtn = document.createElement("button");
+        commentDeleteBtn.className = "commentDeleteBtn";
+        commentDeleteBtn.innerHTML = "삭제";
+        feedComment.appendChild(commentDeleteBtn);
+
         document.getElementsByClassName("feedComment")[0].after(feedComment);
+        commentContents.value = "";
     }
 })
+
+commentContents.addEventListener('keydown', function(e) {
+    if (e.keyCode !== 13) {
+        return
+    }
+
+    if(commentContents.value.length > 1) {
+        let feedComment = document.createElement("div");
+        feedComment.className = "feedComment";
+        
+        let commentId = document.createElement("span");
+        commentId.innerHTML = "castle_jooun";
+        feedComment.appendChild(commentId);
+
+        let commentValue = document.createElement("span");
+        commentValue.innerHTML = commentContents.value;
+        feedComment.appendChild(commentValue);
+
+        let commentDeleteBtn = document.createElement("button");
+        commentDeleteBtn.className = "commentDeleteBtn";
+        commentDeleteBtn.innerHTML = "삭제";
+        feedComment.appendChild(commentDeleteBtn);
+
+        document.getElementsByClassName("feedComment")[0].after(feedComment);
+        commentContents.value = "";
+    }
+})
+
+const commentDeleteBtn = document.getElementsByClassName("commentDeleteBtn");
+
+// commentDeleteBtn.addEventListener('click', function() {
+    
+// })
