6d225419aedeb923e83ba08c2f21671c4e7f67e2
ique-coder <tjdwns0309@gmail.com>
2021-01-21 15:48:17 +0900

    Modify : login.js mission 5 기능 구현 완료

--- students/17th/sungjounjang/js/login.js
+++ students/17th/sungjounjang/js/login.js
@@ -3,9 +3,33 @@ const inputId = document.getElementById("inputId")
 const inputPW = document.getElementById("inputPW")
 
 inputId.addEventListener('keyup', function(e) {
+    let validationId = inputId.value
+    let validationPW = inputPW.value
+
+    if (validationId.indexOf("@") === -1) {
+        loginBtn.style.backgroundColor = "rgb(197,225, 251)";
+        return
+    }
+    if (validationPW.length < 5) {
+        loginBtn.style.backgroundColor = "rgb(197,225, 251)";
+        return
+    }
+
     inputId.value === '' || inputPW.value === '' ? loginBtn.style.backgroundColor = "rgb(197,225, 251)" : loginBtn.style.backgroundColor = "#0095f6"
 })
 
 inputPW.addEventListener('keyup', function(e) {
+    let validationId = inputId.value
+    let validationPW = inputPW.value
+
+    if (validationId.indexOf("@") === -1) {
+        loginBtn.style.backgroundColor = "rgb(197,225, 251)";
+        return
+    }
+    if (validationPW.length < 5) {
+        loginBtn.style.backgroundColor = "rgb(197,225, 251)";
+        return
+    }
+
     inputId.value === '' || inputPW.value === '' ? loginBtn.style.backgroundColor = "rgb(197,225, 251)" : loginBtn.style.backgroundColor = "#0095f6"
 })
