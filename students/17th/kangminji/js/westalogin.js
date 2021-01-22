479b9fc20cc99bc798cf66dc72ecbf4f8f0a614b
이름 <awaji0829@gmail.com>
2021-01-22 13:42:27 +0900

    add :validation 기능 추가

--- students/17th/kangminji/js/westalogin.js
+++ students/17th/kangminji/js/westalogin.js
@@ -2,12 +2,16 @@ const id =document.getElementById('id');
 const password = document.getElementById("password");
 const btn = document.getElementById('btn');
 
+
+
+
+
+
+
 password.addEventListener('keyup', function changcolor(event){
 
 const idvalue = document.getElementById('id').value;
 const passwordvalue = document.getElementById('password').value;
-
-
 
 if(idvalue&&passwordvalue){
 btn.style.backgroundColor =  'blue';
@@ -33,7 +37,29 @@ id.addEventListener('keyup', function changcolor(event){
     }else {
         btn.style.backgroundColor = ' rgb(185, 216,256)';
     }
-    
-    
+
     
     });
+
+
+    btn.addEventListener('click',function validiation(){
+
+        const idvalue = document.getElementById('id').value;
+        const passwordvalue = document.getElementById('password').value;
+    
+    if(!idvalue.includes('@'))
+    {
+           alert ('아이디가 올바르지 않습니다.');
+             }else if(passwordvalue.length<5){
+                 alert('비밀번호는 5자리 이상입니다');
+             }else {
+                location.href = "instagram.html";
+
+                
+             }
+    
+    });
+    
+    
+        
+ 
