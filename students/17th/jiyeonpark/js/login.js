031671a673a652caa9186d9c0969d146dbe98528
jenny87879 <68068885+jenny87879@users.noreply.github.com>
2021-01-24 01:55:34 +0900

    Add: Button Active .js

--- students/17th/jiyeonpark/js/login.js
+++ students/17th/jiyeonpark/js/login.js
@@ -1,10 +1,12 @@
 const loginForm = document.getElementsByClassName('login_form')[0];
-const loginBtn = document.getElementsByClassName('login_btn')[0];
 
 loginForm.addEventListener('change', function(){
-  const idInput = getElementsByClassName('id_input')[0];
-  const pwInput = getElementsByClassName('password_input')[0];
-  console.log(idInput)
-  console.log(pwInput)
+  const idInput = document.getElementsByClassName('id_input')[0].value;
+  const pwInput = document.getElementsByClassName('password_input')[0].value;
+  const loginBtn = document.getElementsByClassName('login_btn')[0];
+  if(idInput.length != 0 && pwInput.length != 0){
+    loginBtn.className = "active_btn login_btn";
+  }else{
+    loginBtn.className = "inactive_btn login_btn";
+  }
 });
-// 
