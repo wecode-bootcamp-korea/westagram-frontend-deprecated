0342de2a2cebc3e899863baadc6090f17c0e4e2b
jenny87879 <jenny87879@gmail.com>
2021-01-23 19:01:15 +0900

    Modify: Login page .js and .css

---
+++ students/17th/jiyeonpark/js/login.js
@@ -0,0 +1,10 @@
+const loginForm = document.getElementsByClassName('login_form')[0];
+const loginBtn = document.getElementsByClassName('login_btn')[0];
+
+loginForm.addEventListener('change', function(){
+  const idInput = getElementsByClassName('id_input')[0];
+  const pwInput = getElementsByClassName('password_input')[0];
+  console.log(idInput)
+  console.log(pwInput)
+});
+// 
