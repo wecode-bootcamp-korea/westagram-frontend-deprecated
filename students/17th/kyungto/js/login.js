a66d0e5f74b97224f73e6600f19eb43b47272c8b
OZOscarZulu <64418552+OZOscarZulu@users.noreply.github.com>
2021-01-24 23:29:26 +0900

    add:로그인페이지 완성(수정)

--- students/17th/kyungto/js/login.js
+++ students/17th/kyungto/js/login.js
@@ -1,31 +1,5 @@
 // btn , userId , userPassword
 "use strict";
-// const btn = document.getElementsByClassName("logInButton")[0]
-
-// btn.addEventListener("keyup", function() {
-//  const pw = document.getElementById("userPassword").value;
-//  const id = document.getElementById("userId").value;
-
-//   if (pw && id) {
-//   btn.re
-//   btn.style.backgroundColor="#0095F6";
-//   return
-//   }
-// }
-
-// const btn = document.getElementsByClassName("logInButton")[0];
-// const input = document.getElsmentsByClassName("input");
-// const id = document.getElementById("userId");
-// const pw = document.getElementById("userPassword");
-
-// function changebtn() {
-//   if (id.value && pw.value) {
-//     btn.removeAttribute("disabled");
-//     btn.style.backgroundColor = "#0095F6";
-//   }
-// }
-
-// pw.addEventListener("keyup", function());
 
 const pw = document.getElementById("userPassword");
 const btn = document.getElementsByClassName("logInButton")[0];
