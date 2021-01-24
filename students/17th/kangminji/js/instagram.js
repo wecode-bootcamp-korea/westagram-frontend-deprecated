6bba57851d6ffff428476db8c47176a11d220e57
이름 <awaji0829@gmail.com>
2021-01-24 10:18:37 +0900

    add:프로필 클릭시 모달창 구현

--- students/17th/kangminji/js/instagram.js
+++ students/17th/kangminji/js/instagram.js
@@ -1,68 +1,105 @@
-const form = document.getElementById('comment');
-const formvalue = document.getElementById('comment').value;
+const form = document.getElementById("comment");
+const formvalue = document.getElementById("comment").value;
 const deletebtn = document.createElement("BUTTON");
 const likebtn = document.createElement("img");
+const modalcontainer = document.querySelector(".two");
 
+form.addEventListener("keyup", addcomment);
 
+function addcomment(e) {
+  if (e.keyCode === 13) {
+    const commentcontainer = document.getElementById("commentbox");
+    const commentbtnbox = document.createElement("div");
+    const commenttext = document.createElement("span");
+    const deletebtn = document.createElement("BUTTON");
+    const likebtn = document.createElement("img");
+    const btncontainer = document.createElement("div");
 
-form.addEventListener("keyup",addcomment); // 댓글을 썼을때 작동되는 함수
+    commentcontainer.className = "box";
+    commenttext.className = "text";
+    deletebtn.className = "deletebtn";
+    commentbtnbox.className = "commentbtn";
+    likebtn.className = "likebtn";
+    btncontainer.className = "btncontainer";
 
-function addcomment(e){
-      if (e.keyCode === 13){
-       
-  const commentcontainer = document.getElementById("commentbox");
-  const commentbtnbox = document.createElement("div");
-  const commenttext = document.createElement("span");
-  const deletebtn = document.createElement("BUTTON");
-  const likebtn = document.createElement("img");
-  const btncontainer = document.createElement("div");
+    likebtn.src = "img/heart.png";
+    deletebtn.innerText = "삭제";
+    commenttext.innerHTML = form.value;
+    commentcontainer.appendChild(commentbtnbox);
+    commentbtnbox.appendChild(commenttext);
+    commentbtnbox.appendChild(btncontainer);
+    btncontainer.appendChild(likebtn);
+    btncontainer.appendChild(deletebtn);
 
-  commentcontainer.className = 'box'; // class속성부텨
-  commenttext.className='text';
-  deletebtn.className='deletebtn';
-  commentbtnbox.className='commentbtn';
-  likebtn.className='likebtn';
-  btncontainer.className = 'btncontainer';
+    form.value = "";
+    likebtn.addEventListener("click", function changered() {
+      likebtn.className = "likebtn";
+      if (likebtn.getAttribute("src") == "img/heart-2.png") {
+        likebtn.src = "img/heart.png";
+      } else if (likebtn.getAttribute("src") == "img/heart.png") {
+        likebtn.src = "img/heart-2.png";
+      }
+    });
 
-
-  likebtn.src="img/heart.png";
-  deletebtn.innerText="삭제";
-commenttext.innerHTML=form.value;
-commentcontainer.appendChild(commentbtnbox);
-commentbtnbox.appendChild(commenttext);
-commentbtnbox.appendChild(btncontainer);//commentbtnbox에 text랑 like delete btn이 들어있는 div 넣기 
-btncontainer.appendChild(likebtn);
-btncontainer.appendChild(deletebtn);// btncontainer에 delete와 like btn 넣기 
-
-form.value='';
-likebtn.addEventListener("click",function changered(){ //댓글 좋아요 색깔 바꾸는 함수 
-  likebtn.className='likebtn';
-  
-
-
-if(likebtn.getAttribute('src')=="img/heart-2.png")
-{
-  likebtn.src="img/heart.png";
-}else if(
-  likebtn.getAttribute('src')=="img/heart.png"){
-  likebtn.src="img/heart-2.png";
+    deletebtn.addEventListener("click", (event) => {
+      const btn = event.target;
+      console.log(btn);
+      const btnbox = btn.parentNode;
+      const deleteAll = btnbox.parentNode;
+      commentcontainer.removeChild(deleteAll);
+    });
+  }
 }
 
-});
+const bubblespace = document.querySelector(".rightone");
+const profilebubble = document.createElement("span");
+const listone = document.createElement("li");
+const listtwo = document.createElement("li");
+const listthree = document.createElement("li");
+const listfour = document.createElement("li");
+const myprofile = document.querySelector(".me");
+const body = document.querySelector(".two");
 
-deletebtn.addEventListener('click',(event)=>{ //delete 버튼함수
+function clickmyprofile() {
+  const outside = document.createElement("div");
 
-  const btn = event.target; 
-        console.log(btn);
-const btnbox =  btn.parentNode; 
-        const deleteAll = btnbox.parentNode; // deletebtn과 likebtn을 묶어주는 div박스를 생성했기때문에 그 조부모요소(텍스트, btn박스가 포함된)를 지워야 된다.
-        commentcontainer.removeChild(deleteAll);
-});
- 
-}};
+  outside.className = "outside";
+  profilebubble.className = "arrow_box";
+  listone.className = "profileclicklist";
+  listtwo.className = "profileclicklist";
+  listthree.className = "profileclicklist";
+  listfour.className = "profileclicklist logout";
 
+  listone.innerHTML =
+    "&nbsp;<img src='img/user.png' class='profileclickphoto'>&nbsp;profile";
+  listtwo.innerHTML =
+    "&nbsp;<img src='img/bookmark.png' class='profileclickphoto'>&nbsp;bookmark";
+  listthree.innerHTML =
+    "&nbsp;<img src='img/management.png' class='profileclickphoto'>&nbsp;setting";
+  listfour.innerHTML =
+    "&nbsp;<img src='img/logout.png' class='profileclickphoto'>&nbsp;logout";
 
+  bubblespace.appendChild(profilebubble);
+  body.appendChild(outside);
+  profilebubble.appendChild(listone);
+  profilebubble.appendChild(listtwo);
+  profilebubble.appendChild(listthree);
+  profilebubble.appendChild(listfour);
 
+  profilebubble.style.display = "block";
+  listone.style.display = "block";
+  listtwo.style.display = "block";
+  listthree.style.display = "block";
+  listfour.style.display = "block";
 
-
-     
+  function offclick() {
+    modalcontainer.removeChild(outside);
+    profilebubble.style.display = "none";
+    listone.style.display = "none";
+    listtwo.style.display = "none";
+    listthree.style.display = "none";
+    listfour.style.display = "none";
+  }
+  outside.addEventListener("click", offclick);
+}
+myprofile.addEventListener("click", clickmyprofile);
--- students/17th/kangminji/js/westalogin.js
+++ students/17th/kangminji/js/westalogin.js
@@ -1,65 +1,35 @@
-const id =document.getElementById('id');
+const id = document.getElementById("id");
 const password = document.getElementById("password");
-const btn = document.getElementById('btn');
+const btn = document.getElementById("btn");
 
+password.addEventListener("keyup", function changcolor(event) {
+  const idvalue = document.getElementById("id").value;
+  const passwordvalue = document.getElementById("password").value;
 
-
-
-
-
-
-password.addEventListener('keyup', function changcolor(event){
-
-const idvalue = document.getElementById('id').value;
-const passwordvalue = document.getElementById('password').value;
-
-if(idvalue&&passwordvalue){
-btn.style.backgroundColor =  'blue';
-
-}else {
-    btn.style.backgroundColor = ' rgb(185, 216,256)';
-}
-
-
-
+  if (idvalue && passwordvalue) {
+    btn.style.backgroundColor = "blue";
+  } else {
+    btn.style.backgroundColor = " rgb(185, 216,256)";
+  }
 });
+id.addEventListener("keyup", function changcolor(event) {
+  const idvalue = document.getElementById("id").value;
+  const passwordvalue = document.getElementById("password").value;
 
-id.addEventListener('keyup', function changcolor(event){
-
-    const idvalue = document.getElementById('id').value;
-    const passwordvalue = document.getElementById('password').value;
-    
-    
-    
-    if(idvalue&&passwordvalue){
-    btn.style.backgroundColor =  'blue';
-    
-    }else {
-        btn.style.backgroundColor = ' rgb(185, 216,256)';
-    }
-
-    
-    });
-
-
-    btn.addEventListener('click',function validiation(){
-
-        const idvalue = document.getElementById('id').value;
-        const passwordvalue = document.getElementById('password').value;
-    
-    if(!idvalue.includes('@'))
-    {
-           alert ('아이디가 올바르지 않습니다.');
-             }else if(passwordvalue.length<5){
-                 alert('비밀번호는 5자리 이상입니다');
-             }else {
-                location.href = "instagram.html";
-
-                
-             }
-    
-    });
-    
-    
-        
- 
+  if (idvalue && passwordvalue) {
+    btn.style.backgroundColor = "blue";
+  } else {
+    btn.style.backgroundColor = " rgb(185, 216,256)";
+  }
+});
+btn.addEventListener("click", function validiation() {
+  const idvalue = document.getElementById("id").value;
+  const passwordvalue = document.getElementById("password").value;
+  if (!idvalue.includes("@")) {
+    alert("아이디가 올바르지 않습니다.");
+  } else if (passwordvalue.length < 5) {
+    alert("비밀번호는 5자리 이상입니다");
+  } else {
+    location.href = "instagram.html";
+  }
+});
