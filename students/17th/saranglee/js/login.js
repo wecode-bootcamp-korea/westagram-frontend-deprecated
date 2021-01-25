12dea79a87e06799e71de70c1ef17a86dbe35499
rosarang <72490935+rosarang@users.noreply.github.com>
2021-01-25 14:52:47 +0900

    Refactor: login 페이지 리뷰 반영 - 들여쓰기 4칸 -> 2칸으로 수정 - 버튼 disabled 속성 수정 - 속성 띄어쓰기 수정 - img alt 속성 순서 변경 - js 변수 수정 및 if문 조건 수정

--- students/17th/saranglee/js/login.js
+++ students/17th/saranglee/js/login.js
@@ -1,15 +1,15 @@
 const loginContainer =  document.querySelector(".login-container");
 
 loginContainer.addEventListener("keydown", function() {
-    const loginBtn = document.querySelector(".login-btn");
-    const idInput = document.querySelector(".id-input");
-    const pwInput = document.querySelector(".pw-input");
-    
-    if(idInput.value !== "" && pwInput.value !== "") {
-        loginBtn.disabled = false;
-        loginBtn.style.opacity = "1";
-    } else if(idInput.value === "" || pwInput.value === "") {
-        loginBtn.disabled = true;
-        loginBtn.style.opacity = "0.3";
-    }
+  const loginBtn = document.querySelector(".login-btn");
+  const id = document.querySelector(".id-input").value;
+  const pw = document.querySelector(".pw-input").value;
+  
+  if(id && pw) {
+    loginBtn.disabled = false;
+    loginBtn.style.opacity = "1";
+  } else if(!id || !pw ) {
+    loginBtn.disabled = true;
+    loginBtn.style.opacity = "0.3";
+  }
 });
