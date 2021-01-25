5267618f630488d49f2c906249f57df3e9fdaeec
jenny87879 <68068885+jenny87879@users.noreply.github.com>
2021-01-26 04:21:05 +0900

    Add: Main page comment add feature

--- students/17th/jiyeonpark/js/main.js
+++ students/17th/jiyeonpark/js/main.js
@@ -1,9 +1,28 @@
-const comment_form = document.getElementsByClassName('form')[0];
-const comment_button = document.getElementsByClassName('comment_input_button')[0];
+const commentForm = document.getElementsByClassName('form')[0];
 
-comment_form.addEventListener('submit',() =>{
-  const commentInput = document.getElementsByClassName('comment_input_box')[0].value;
+commentForm.addEventListener('input',()=>{
+  const commentInput = document.getElementsByClassName('comment_input_box')[0];
+  const commentButton = document.getElementsByClassName('login_btn')[0];
+  if(commentInput.value.length != 0){
+    commentButton.className = "active_button login_btn";
+  } else {
+    commentButton.className = "inactive_button login_btn";
+  }
+})
+
+commentForm.addEventListener('submit',(e) =>{
+  const commentInput = document.getElementsByClassName('comment_input_box')[0];
   const commentList = document.getElementsByClassName('comment_list')[0];
-  console.log(commentInput)
-  commentList.innerHTML = `<p>jiyeon0807 ${commentInput}</p>`;
+  if(commentInput.value.length != 0){
+    e.preventDefault();
+    const comment = document.createElement("div");
+    const span = document.createElement("span");
+    const a = document.createElement("a");
+    a.innerText = "jiyeon0807";
+    span.innerText = commentInput.value;
+    comment.appendChild(a);
+    comment. appendChild(span);
+    commentList.appendChild(comment);
+    commentInput.value = '';
+  }
 })
