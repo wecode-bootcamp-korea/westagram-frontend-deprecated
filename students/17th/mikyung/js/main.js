9ce20f74918a21a9adc29075f0ef72912f97950a
peng0code <peng0code>
2021-01-26 23:07:48 +0900

    first 수정

--- students/17th/mikyung/js/main.js
+++ students/17th/mikyung/js/main.js
@@ -1,43 +1,37 @@
+const commentList = document.getElementsByClassName('comments')[0];
 const commentInput = document.getElementsByClassName('textarea1')[0];
 const commentBtn = document.getElementsByClassName('submit-comment')[0];
-const commentList = document.getElementsByClassName('comments')[0];
 
-commentInput.addEventListener('keypress', function(e){
-const newInput = commentInput.value;
+function updateComment() {
     if (commentInput.value) {
-        if (e.which === 13) {
-            let newComment = document.createElement('li');
-            newComment.innerHTML = `
-            <span><span class="comment_userID">ggiiiaa</span><span="comment2">&nbsp&nbsp${commentInput.value}</span>
-            <i class="far fa-heart"></i></div> 
-            `;
-            newComment.classList.add('comment_ul')
-            commentList.appendChild(newComment);
-            commentInput.value = "";
-        }
+      let newComment = document.createElement('li');
+      newComment.innerHTML = `
+                <span>
+                  <span class="comment_userID">ggiiiaa</span>
+                  <span="comment2">&nbsp&nbsp${commentInput.value}</span>
+                  <i class="far fa-heart"></i>
+                </span> 
+                `;
+      newComment.className = 'comment_ul';
+      commentList.appendChild(newComment);
+      commentInput.value = '';
     }
-})
+  }
 
-commentBtn.addEventListener('click', function(){
-    const newInput = commentInput.value;
-    if (commentInput.value) {
-        let newComment = document.createElement('li')
-        newComment.innerHTML = `
-            <span><span class="comment_userID">ggiiiaa</span><span="comment2">&nbsp&nbsp${commentInput.value}</span>
-            <i class="far fa-heart"></i></div> 
-            `;
-            newComment.classList.add('comment_ul')
-        commentList.appendChild(newComment);
-        commentInput.value = "";
+  commentInput.addEventListener('keyup', function (event) {
+    if (event.keyCode === 13) {
+      updateComment();
     }
-})
+  });
 
-commentInput.addEventListener('keyup', function(event) {
+  commentBtn.addEventListener('click', updateComment);
+
+  commentInput.addEventListener('keyup', function () {
     const comment = commentInput.value;
+  
     if (comment.length > 0) {
-        return commentBtn.removeAttribute('disabled');
+      return commentBtn.removeAttribute('disabled');
+    } else {
+      return commentBtn.setAttribute('disabled', 'disabled');
     }
-    else {
-        return commentBtn.setAttribute('disabled','disabled')
-    }   
-})
+  });
