19d4f2b541df0de2dd6d68f6f0398f77179b7313
Jieun-yyang <poohv7@gmail.com>
2021-02-25 01:11:01 +0900

    위스타그램 로그인 화면 업데이트

--- students/18th/18기 양지은/login.js
+++ students/18th/18기 양지은/login.js
@@ -1,10 +1,20 @@
-const userId = document.getElementById('userid');
-const userPassword = document.getElementById('password');
-document.addEventListener('keyup', function(e) {
-    if (userId.vlaue !== "") {
-        if (userPassword.value !== "") {                        
+
+document.addEventListener('keyup', function() {
+    const userId = document.getElementById('userid');
+    const userPassword = document.getElementById('password');
+
+    if (userId.value) {
+        if (userPassword.value) {                        
             let color = document.getElementsByClassName('btn')[0];
             color.style.backgroundColor = "#1565c0";
         }
+    }
+    if (!userId.value) {
+            let color = document.getElementsByClassName('btn')[0];
+            color.style.backgroundColor = "lightskyblue";
+    }
+    if (!userPassword.value) {
+        let color = document.getElementsByClassName('btn')[0];
+        color.style.backgroundColor = "lightskyblue";
     }
 });
