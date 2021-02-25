49139bd3654f1509dba0e055c920ff4ad5497bb7
songbetter <5ongnaeu17@gmail.com>
2021-02-25 21:44:44 +0900

    Add: 로그인페이지 버튼 활성화 및 메인페이지 댓글기능 구현 완료

--- students/18th/naeunsong/js/login.js
+++ students/18th/naeunsong/js/login.js
@@ -1,1 +1,14 @@
-// 아직..............
+const login_btn = document.getElementsByTagName('button')[0];
+const thisIsPw = document.getElementById('user_password')
+
+thisIsPw.addEventListener("keyup", function(){
+    const user_id = document.getElementById('user_id').value;
+    const user_password = document.getElementById('user_password').value;
+    
+    if(user_id && user_password){
+        login_btn.className = "active_btn"
+    }
+    else{
+        login_btn.className = "" 
+    }
+})
--- students/18th/naeunsong/js/main.js
+++ students/18th/naeunsong/js/main.js
@@ -1,1 +1,11 @@
-// 아직..............
+const commentBtn = document.querySelector('.commentBtn');
+
+commentBtn.addEventListener("click", function(){
+    const comment = document.querySelector(".textArea").value;
+    console.log(comment);
+    const li = document.createElement('li');
+    const ul = document.querySelector(".commentArea");
+    li.innerHTML = `songbetter ${comment}`;
+    ul.appendChild(li);
+    // comment = '';
+});
