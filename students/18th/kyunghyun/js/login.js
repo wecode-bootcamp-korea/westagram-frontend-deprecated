4b3e67bf612e9ef652e02a9c03c70b308a87ff54
Hyun <hyun@Hyunui-MacBookAir.local>
2021-02-25 21:08:50 +0900

    Add: Login page complete

---
+++ students/18th/kyunghyun/js/login.js
@@ -0,0 +1,37 @@
+const Btn = document.querySelector('.btn')
+const id = document.getElementById('id'); //id 입력창
+const password = document.getElementById('password'); //password 입력창
+
+
+function allClear() {
+    console.log(id.value !== "" && password.value !== "" && password.value.length >= 6);
+    if (id.value !== "" && password.value !== "" && password.value.length >= 6) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// 이벤트 콜백 함수 정의
+function btnOn(allClear) {
+    const check = allClear();
+    function yes() {
+        Btn.style.opacity = "1";
+    }
+
+    function no() {
+        Btn.style.opacity = "0.3";
+    }
+
+    return (check ? yes() : no())
+}
+
+
+
+//이벤트 등록
+id.addEventListener("keyup", ()=>{
+    btnOn(allClear);
+});
+password.addEventListener("keyup", ()=>{
+    btnOn(allClear);
+});
