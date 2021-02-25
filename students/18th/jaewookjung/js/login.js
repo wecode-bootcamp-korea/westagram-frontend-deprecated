a26e0155795fa77b53339d22b3f7375dea147bf6
JAEWOOK JUNG <jaewookjung@JAEWOOKui-MacBookPro.local>
2021-02-25 23:18:53 +0900

    add: 댓글작성 기능 구현

--- students/18th/jaewookjung/js/login.js
+++ students/18th/jaewookjung/js/login.js
@@ -4,36 +4,34 @@ const password = document.querySelector(".input_pw");
 const showPasswordBtn = document.querySelector(".show_pw");
 
 // 이벤트 콜백함수 정의
-function keyupEvent() {
-  if (userId.value !== "" && password.value !== "") {
-    loginBtn.style.opacity = "1";
-  } else {
-    loginBtn.style.opacity = "0.3";
-  }
+function runLoginBtn() {
+    userId.value !== "" && password.value !== ""
+        ? (loginBtn.style.opacity = "1")
+        : (logtinBtn.style.opacity = "0.3");
 }
 
 function showBtn() {
-  if (password.value !== "") {
-    showPasswordBtn.style.display = "flex";
-  } else {
-    showPasswordBtn.style.display = "none";
-  }
+    if (password.value !== "") {
+        showPasswordBtn.style.display = "flex";
+    } else {
+        showPasswordBtn.style.display = "none";
+    }
 }
 
 // 이벤트 등록
-userId.addEventListener("keyup", keyupEvent);
+userId.addEventListener("keyup", runLoginBtn);
 
 password.addEventListener("keyup", function () {
-  keyupEvent();
-  showBtn();
+    runLoginBtn();
+    showBtn();
 });
 
 showPasswordBtn.addEventListener("click", function () {
-  if (showPasswordBtn.innerHTML === "비밀번호 표시") {
-    password.setAttribute("type", "text");
-    showPasswordBtn.innerHTML = "숨기기";
-  } else {
-    password.setAttribute("type", "password");
-    showPasswordBtn.innerHTML = "비밀번호 표시";
-  }
+    if (showPasswordBtn.innerHTML === "비밀번호 표시") {
+        password.setAttribute("type", "text");
+        showPasswordBtn.innerHTML = "숨기기";
+    } else {
+        password.setAttribute("type", "password");
+        showPasswordBtn.innerHTML = "비밀번호 표시";
+    }
 });
--- students/18th/jaewookjung/js/main.js
+++ students/18th/jaewookjung/js/main.js
@@ -1,1 +1,97 @@
+const navSearch = document.querySelector(".nav_search");
+const searchContainer = document.querySelector(".search_container");
+const searchInput = document.querySelector(".search");
+const searchSpan = document.querySelector(".search_span");
+const searchCancleBtn = document.querySelector(".cancle_search_btn");
+const sectionList = document.querySelectorAll("section");
+const imgContentsList = document.querySelectorAll(".img_contents");
+const imgDateList = document.querySelectorAll("time");
+const imgArticlesList = document.querySelectorAll(".img_articles");
+const showAllArticleBtn = document.querySelectorAll(".show_all_article");
+const commentsInputList = document.querySelectorAll(".comments_text");
+const addCommentBtnList = document.querySelectorAll(".add_comment_btn");
+const commentsBlockList = document.querySelectorAll(".comments_column");
+const likesNum = document.querySelectorAll(".likes_num");
 
+// //좋아요 개수 변수
+// let likesCount = 0;
+
+//naviation bar에 검색창 클릭 이벤트
+searchContainer.addEventListener("click", function (e) {
+    console.log(e.target.childNodes, e.target.children);
+    searchInput.style.display = "flex";
+    searchInput.focus();
+    searchCancleBtn.style.display = "flex";
+    searchSpan.style.display = "none";
+});
+
+//검색창 x버튼 클릭 이벤트
+searchCancleBtn.addEventListener("click", function () {
+    searchInput.value = "";
+    searchInput.style.display = "none";
+    searchCancleBtn.style.display = "none";
+    searchSpan.style.display = "flex";
+});
+
+//feed 내의 이벤트
+for (let i = 0; i < sectionList.length; i++) {
+    //더보기 버튼 이벤트
+    showAllArticleBtn[i].addEventListener("click", function () {
+        showAllArticleBtn[i].style.display = "none";
+        imgArticlesList[i].style["white-space"] = "normal";
+    });
+
+    //댓글창 입력시 게시버튼 활성화 이벤트
+    commentsInputList[i].addEventListener("keyup", function () {
+        if (commentsInputList[i].value === "") {
+            addCommentBtnList[i].style.opacity = 0.3;
+        } else {
+            addCommentBtnList[i].style.opacity = 1;
+        }
+    });
+
+    //댓글 달기 이벤트
+    addCommentBtnList[i].addEventListener("click", function () {
+        if (addCommentBtnList[i].style.opacity === "1") {
+            if (imgDateList[i].previousElementSibling.tagName !== "UL") {
+                let parentTag = imgDateList[i].parentElement;
+                let ul = document.createElement("ul");
+                ul.className = "comments_container";
+                parentTag.insertBefore(ul, imgDateList[i]);
+            }
+            let comment = commentsInputList[i].value;
+            let li = document.createElement("li");
+            let commentContents = document.createElement("div");
+            let commentUser = document.createElement("span");
+            let commentLike = document.createElement("i");
+            commentContents.className = "comment_contents";
+            commentUser.className = "comment_user";
+            commentLike.className = "far fa-heart comment_like";
+            commentUser.innerHTML = "Jhon Doe";
+            commentContents.appendChild(commentUser);
+            commentUser.after(comment);
+            li.appendChild(commentContents);
+            commentContents.after(commentLike);
+            imgDateList[i].previousElementSibling.appendChild(li);
+            commentsInputList[i].value = "";
+
+            //좋아요 기능 구현중
+            // const commentLikeBtn = document.querySelectorAll(".comment_like");
+
+            // for (let j = 0; j < commentLikeBtn.length; j++) {
+            //     commentLikeBtn[j].addEventListener("click", function () {
+            //         commentLikeBtn[j].style.color === "red"
+            //             ? ((commentLikeBtn[j].style.color = "black"),
+            //               (commentLikeBtn[j].className =
+            //                   "far fa-heart comment_like"),
+            //               likesCount--)
+            //             : ((commentLikeBtn[j].style.color = "red"),
+            //               (commentLikeBtn[j].className =
+            //                   "fas fa-heart comment_like"),
+            //               likesCount++);
+            //         likesNum[i].innerHTML = likesCount;
+            //     });
+            // }
+        }
+    });
+}
