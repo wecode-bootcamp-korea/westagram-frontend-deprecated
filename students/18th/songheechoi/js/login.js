ac33713328b485a0fd47546ea3009308288b7f48
iamsonghee <65877281+iamsonghee@users.noreply.github.com>
2021-02-25 20:57:20 +0900

    UPDATE : login, main 기능추가

--- students/18th/songheechoi/js/login.js
+++ students/18th/songheechoi/js/login.js
@@ -4,19 +4,43 @@ const btnLogin = document.querySelector(".btnLogin");
 
 
 function checkInputs(id, pwd){
-    if(id && pwd){
+    if(id && pwd.length>5){
         btnLogin.classList.add("enable");
+        btnLogin.removeAttribute("disabled");
+        return true;
         // console.log(id,pwd);
     }else{
         btnLogin.classList.remove("enable");
+        btnLogin.setAttribute("disabled","");
+        return false;
     }
+}
+function checkValidation(id, pwd){
+    // console.log(id.indexOf("@"), pwd.length);
+    if(id.indexOf("@")>-1 && pwd.length>5){
+        location.href= "./main.html"
+    }
+    else{
+        alert("아이디와 비밀번호 확인 후 재시도");
+        return;
+    }
+
 }
 loginId.addEventListener("keyup", function(e){
     // console.log(`loginID: ${loginId.value}`);
     checkInputs(loginId.value, passWord.value);
 })
 
-passWord.addEventListener("keyup", function(e){
-    checkInputs(loginId.value, passWord.value);
-    // console.log(`passWord: ${passWord.value}`);
+document.addEventListener("keyup",function(e){
+    if(checkInputs(loginId.value, passWord.value)){
+        if(e.keyCode === 13){
+            btnLogin.click();
+        }
+    }
+})
+
+
+btnLogin.addEventListener("click", function(){
+    checkValidation(loginId.value, passWord.value);
+
 })
--- students/18th/songheechoi/js/main.js
+++ students/18th/songheechoi/js/main.js
@@ -1,20 +1,24 @@
 
 const commentInput = document.querySelector(".feed__myComment input");
+
 const postButton = document.querySelector(".comment__btn-post")
+const postButton1 = document.getElementsByClassName("comment__btn-post")
+
 const commentsList = document.querySelector(".feed__commentList");
+const logo = document.querySelector(".logo");
 
-const likeButton = 
-
-commentInput.addEventListener("input",function(){
-    // console.log(commentInput.value);
+// const likeButton = 
+function test(){
+    // console.log(postButton,postButton1);
     if(commentInput.value){
         postButton.style.opacity ="1";
     }else{
         postButton.style.opacity ="0.3";
     }
-})
+}
+commentInput.addEventListener("input",test)
 commentInput.addEventListener("keyup", function(e){
-    console.log(e);
+    // console.log(e);
     if(e.keyCode ===13&&commentInput.value){
         postButton.click();
     }
@@ -29,14 +33,23 @@ const postComment = function(inputValue){
     return comment;
 }
 
-
+logo.addEventListener("click",function(){
+    location.href = "./login.html";
+})
 postButton.addEventListener("click",function(){
-    // alert(commentInput.value);
-    let inputValue = commentInput.value;
-    let row = document.createElement("div");
-    row.innerHTML = postComment(inputValue);
-    row.classList.add("comment-row");
-    commentsList.appendChild(row);
+    if(commentInput.value){
+        // alert(commentInput.value);
+        let inputValue = commentInput.value;
+        let row = document.createElement("div");
+        row.innerHTML = postComment(inputValue);
+        row.classList.add("comment-row");
+        commentsList.appendChild(row);
 
-    commentInput.value = "";
+        commentInput.value = "";
+        test();
+        //SONGQ. 여기서 그냥 input 이벤트를 한번 발생하고싶은데 할수있는방법없나?? postButton.click()처럼..
+        // postButton.style.opacity ="0.3";
+        // postButton.input();
+
+    }
 }) 
