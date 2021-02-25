e9243da5285309ccfd111342b323a62ce75a321e
SungHoon <tjdgns503@naver.com>
2021-02-25 13:07:09 +0900

    로그인페이지 크기 수정 및 버튼 활성화 수정

--- students/18th/sunghoonbae/js/login.js
+++ students/18th/sunghoonbae/js/login.js
@@ -2,25 +2,20 @@ const loginId = document.querySelector('#loginId');
 const loginPw = document.querySelector('#loginPw');
 const loginBtn = document.querySelector('.login__btn');
 
-let loginIdValue;
-let loginPwValue;
+let loginIdValue = 0;
+let loginPwValue = 0;
 
 loginId.addEventListener('keyup', function(e) {
-    loginIdValue = e.target.value; 
-    loginBtnColor();
-}); 
+    loginIdValue = this.value; 
+    changeLoginBtnColor();
+});  
 
 loginPw.addEventListener('keyup', function(e) {
-    loginPwValue = e.target.value;
-    loginBtnColor();
+    loginPwValue = this.value;
+    changeLoginBtnColor();
 });
 
-function loginBtnColor() {
-   // console.log(loginIdValue.length, loginPwValue.length)
+function changeLoginBtnColor() {
+   console.log(loginIdValue.length, loginPwValue.length)
    return (loginIdValue.length >= 1 && loginPwValue.length >= 1) ? loginBtn.classList.add('login__color') : loginBtn.classList.remove('login__color');
-} 
-
-/* function loginBtnColor() {
-     console.log(loginIdValue)
-   // return (loginIdValue.length >= 1 && loginPwValue.length >=1) ? loginBtn.classList.add('login__color') : loginBtn.classList.remove('login__color');
-} */
+}
