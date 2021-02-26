09a0dbe3b81336695daff4ea03ac095430902bda
durumi <fisinbi@gmail.com>
2021-02-26 16:21:18 +0900

    로그인페이지 js 추가

--- students/18th/mihyunGim/js/login.js
+++ students/18th/mihyunGim/js/login.js
@@ -1,18 +1,23 @@
-const logo =document.querySelector('.logo')
-const inputName =document.querySelector('.inputName')
-const inputPassword =document.querySelector('.inputPassword')
+// const logo =document.querySelector('.logo')
+const text =document.querySelector('.inputName')
+const password =document.querySelector('.inputPassword')
 const logInBtn =document.querySelector('.logInBtn')
 
+
 function changeInputColor(){
-if(inputName.value && inputPassword.value){
-    logInBtn.style.background="blue"
+    console.log("dPQJ")
+if(text.value && password.value){
+    logInBtn.style.background="rgb(0,149,246)"
+
+}
+if(!text.value || !password.value){
+    logInBtn.style.background="rgb(178,223,252)"
+
 }
 }
 
 
 
-function newPage()  {
-    window.location.href = 'main.html'
-  }
 
-  logInBtn.addEventListener('click', newPage)
+password.addEventListener('keyup', changeInputColor)
+text.addEventListener('keyup', changeInputColor)
