15944bf3baea84624c8f9d6f5fa2fcc819ff49be
songbetter <5ongnaeu17@gmail.com>
2021-02-26 16:08:31 +0900

    Add: 댓글 유저ID span 추가

--- students/18th/naeunsong/js/main.js
+++ students/18th/naeunsong/js/main.js
@@ -1,13 +1,13 @@
 const textArea = document.querySelector(".textArea");
 const commentBtn = document.querySelector('.commentBtn');
 const search = document.querySelector('.search');
+const userId = "songbetter";
 
 commentBtn.addEventListener("click", function(){
     const comment = document.querySelector(".textArea").value;
-    console.log(comment);
     const li = document.createElement('li');
     const ul = document.querySelector(".commentArea");
-    li.innerHTML = `songbetter ${comment}`;
+    li.innerHTML = `<a>${userId}</a> ${comment}`;
     ul.appendChild(li);
     // comment = '';
 });
