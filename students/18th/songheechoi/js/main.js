710cf95b5d748f73a07b81ae270bb9540c0eb7e4
iamsonghee <65877281+iamsonghee@users.noreply.github.com>
2021-02-26 19:54:36 +0900

    ADD:좋아요 기능 추가

--- students/18th/songheechoi/js/main.js
+++ students/18th/songheechoi/js/main.js
@@ -2,27 +2,22 @@
 const commentInput = document.querySelector(".feed__myComment input");
 
 const postButton = document.querySelector(".comment__btn-post")
-const postButton1 = document.getElementsByClassName("comment__btn-post")
+// const postButton1 = document.getElementsByClassName("comment__btn-post")
 
 const commentsList = document.querySelector(".feed__commentList");
 const logo = document.querySelector(".logo");
+// const commentLike = document.querySelector(".comment-row .fa-heart");
+const bookmark = document.querySelector(".fa-bookmark");
 
-// const likeButton = 
-function test(){
-    // console.log(postButton,postButton1);
-    if(commentInput.value){
-        postButton.style.opacity ="1";
-    }else{
-        postButton.style.opacity ="0.3";
-    }
-}
-commentInput.addEventListener("input",test)
-commentInput.addEventListener("keyup", function(e){
-    // console.log(e);
-    if(e.keyCode ===13&&commentInput.value){
-        postButton.click();
-    }
+const CLLICKED_CLASS = "fas";
+
+// console.log(commentLike)
+
+bookmark.addEventListener("click",function(){
+    bookmark.classList.toggle(CLLICKED_CLASS);
+
 })
+
 const postComment = function(inputValue){
     let comment = `<div class="comment">
                     <span class="comment__userId">songhee</span>
@@ -33,6 +28,24 @@ const postComment = function(inputValue){
     return comment;
 }
 
+// const likeButton = 
+function setButtonStatus(){
+    // console.log(postButton,postButton1);
+    
+    if(commentInput.value){
+        postButton.style.opacity ="1";
+    }else{
+        postButton.style.opacity ="0.3";
+    }
+}
+commentInput.addEventListener("input",setButtonStatus)
+
+commentInput.addEventListener("keyup", function(e){
+    // console.log(e);
+    if(e.keyCode ===13&&commentInput.value){
+        postButton.click();
+    }
+})
 logo.addEventListener("click",function(){
     location.href = "./login.html";
 })
@@ -44,12 +57,13 @@ postButton.addEventListener("click",function(){
         row.innerHTML = postComment(inputValue);
         row.classList.add("comment-row");
         commentsList.appendChild(row);
-
+        const commentLike = row.querySelector(".fa-heart");
+        commentLike.addEventListener("click",function(){
+            commentLike.classList.toggle(CLLICKED_CLASS);
+        })
+        
         commentInput.value = "";
-        test();
-        //SONGQ. 여기서 그냥 input 이벤트를 한번 발생하고싶은데 할수있는방법없나?? postButton.click()처럼..
-        // postButton.style.opacity ="0.3";
-        // postButton.input();
-
+        setButtonStatus();
+        
     }
 }) 
