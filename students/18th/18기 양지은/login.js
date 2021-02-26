455c4b5b747935a0f490234ce2d4d166358f537e
Jieun-yyang <poohv7@gmail.com>
2021-02-26 14:19:06 +0900

    로그인 페이지 업데이트

--- students/18th/18기 양지은/login.js
+++ students/18th/18기 양지은/login.js
@@ -3,18 +3,12 @@ document.addEventListener('keyup', function() {
     const userId = document.getElementById('userid');
     const userPassword = document.getElementById('password');
 
-    if (userId.value) {
-        if (userPassword.value) {                        
+    if (userId.value && userPassword.value ) {                        
             let color = document.getElementsByClassName('btn')[0];
             color.style.backgroundColor = "#1565c0";
-        }
     }
-    if (!userId.value) {
+    if (!userId.value || !userPassword.value ) {
             let color = document.getElementsByClassName('btn')[0];
             color.style.backgroundColor = "lightskyblue";
-    }
-    if (!userPassword.value) {
-        let color = document.getElementsByClassName('btn')[0];
-        color.style.backgroundColor = "lightskyblue";
     }
 });
