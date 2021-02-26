426ca3fe5820f335822d53365f0b8c3199a83f43
Jieun-yyang <poohv7@gmail.com>
2021-02-26 14:34:54 +0900

    로그인 페이지 코드 리뷰 후 수정

--- students/18th/18기 양지은/login.js
+++ students/18th/18기 양지은/login.js
@@ -1,14 +1,13 @@
+document.addEventListener("keyup", function () {
+  const userId = document.getElementById("userid");
+  const userPassword = document.getElementById("password");
 
-document.addEventListener('keyup', function() {
-    const userId = document.getElementById('userid');
-    const userPassword = document.getElementById('password');
-
-    if (userId.value && userPassword.value ) {                        
-            let color = document.getElementsByClassName('btn')[0];
-            color.style.backgroundColor = "#1565c0";
-    }
-    if (!userId.value || !userPassword.value ) {
-            let color = document.getElementsByClassName('btn')[0];
-            color.style.backgroundColor = "lightskyblue";
-    }
+  if (userId.value && userPassword.value) {
+    let color = document.getElementsByClassName("btn")[0];
+    color.style.backgroundColor = "#1565c0";
+  }
+  if (!userId.value || !userPassword.value) {
+    let color = document.getElementsByClassName("btn")[0];
+    color.style.backgroundColor = "lightskyblue";
+  }
 });
