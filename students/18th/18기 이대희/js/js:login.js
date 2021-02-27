9943ba5117087ce85118f5728ba27f7b9e7e2393
이대희 <ldaehi0205@gmail.com>
2021-02-28 02:58:56 +0900

    refacr: 불필요 코드 제거

--- students/18th/18기 이대희/js/js:login.js
+++ students/18th/18기 이대희/js/js:login.js
@@ -1,27 +1,8 @@
-const idinput = document.querySelectorAll('.id');
+const idinput = document.querySelector("input");
 const pwinput = document.querySelector('.pw');
+const inputs = document.querySelector('.inputs');
 const btn = document.querySelector('.btn');
-const rightbox1 = document.querySelector('.rightBox1');
 
-const changebtn = () =>{
-    let idinputval = idinput.value;
-    let pwinputval = pwinput.value;
-//     if(!idinputval || !pwinputval){
-//         btn.style.backgroundColor='rgb(192,223,253)';
-//     }
-//     else{
-//         btn.style.backgroundColor='blue';
-//     }
-// }
-const activeContion = !idInputVal || !pwinputval;
-btn.style.background = activeContion ? 'rgb(192,1123,153)' :'blue';
-}
-let inputClick = idinput, pwinput;
-idinput.addEventListener('keyup', function(e){
-    changebtn();
-    }
-)
-// pwinput.addEventListener('keyup', function(e){
-//     changebtn();
-// }
-// )
+inputs.addEventListener('keyup', (e) => {
+    btn.style.background = !idinput.value || !pwinput.value ? 'rgb(191,224,253)' :'blue';
+})
--- students/18th/18기 이대희/js/js:main.js
+++ students/18th/18기 이대희/js/js:main.js
@@ -1,65 +1,54 @@
 const ul = document.querySelector('.comments');
-const inputbox = document.querySelector('.inputbox');
-const wename = document.querySelector('.name').innerHTML;
+const inputBox = document.querySelector('.inputBox');
+const weName = document.querySelector('.name').innerHTML.bold();
 const article = document.querySelector('article');
-const wenames = wename.bold(); 
-let heartx2 = document.querySelector('.heartx2');
-let delicon = document.querySelector('.heartx2');
-const iconbox = document.querySelector('.iconbox'); 
+let heartIcon = document.querySelector('.heartIcon');
+let delIcon = document.querySelector('.heartx2');
+const iconBox = document.querySelector('.iconBox'); 
 
-inputbox.addEventListener('keydown', (e) => {
+inputBox.addEventListener('keyup', (e) => {
 
     if (e.key === 'Enter') {
         const btn = document.createElement('button');
         btn.className='textremove';
-        let p = document.createElement('i');
-        p.className='fas fa-ellipsis-h';
-        btn.appendChild(p);
+        const i = document.createElement('i');
+        i.className='fas fa-ellipsis-h';
+        btn.append(i);
         
-        const div_heart = document.createElement('div');
-        div_heart.className='div_heart';
+        const makeDiv = document.createElement('div');
+        makeDiv.className='divHeart';
         const heart = document.createElement('i');
         heart.className='far fa-heart';
-        div_heart.appendChild(heart);
+        makeDiv.appendChild(heart);
        
-        let makeul = document.createElement('li');
-        makeul.className='comment';
-        makeul.innerHTML=`${wenames} ${inputbox.value}`;
-        makeul.appendChild(btn);
-        makeul.appendChild(div_heart);
-        ul.appendChild(makeul);
-        console.log(makeul);
+        let makeli = document.createElement('li');
+        makeli.innerHTML=`${weName} ${inputBox.value}`;
+        makeli.append(btn,makeDiv);
+        ul.append(makeli);
 
-        inputbox.value = '';
+        inputBox.value = '';
         //댓글 삭제
         btn.addEventListener('click', () =>{   
-        makeul.remove();        
+            makeli.remove();        
         })
         //댓글 좋아요
-        div_heart.addEventListener('click', () =>{  
-            
-            if(div_heart.style.color==='red'){
-                div_heart.style.color='rgb(219,219,219)';
-            }else{
-                div_heart.style.color='red';
-            }
-            console.log(div_heart.style.color);        
+        makeDiv.addEventListener('click', () =>{  
+            makeDiv.style.color = makeDiv.style.color==='red' ? makeDiv.style.color='rgb(219,219,219)': makeDiv.style.color='red';   
         })
     }
-});
+})
  
 
-heartx2.addEventListener('click', (e) => {   
+heartIcon.addEventListener('click', (e) => {   
     
-        heartx2.remove();
+        heartIcon.remove();
         let a = document.createElement('a');
-        a.className="heartx2";
-        a.id = 'ch2';
-        iconbox.prepend(a)
-        let p1 = document.createElement('i');
-        p1.className="fas fa-heart fa-2x";
-        p1.style.color='red';
-        a.appendChild(p1);
+        a.className="heartIcon";
+        iconBox.prepend(a)
+        let i = document.createElement('i');
+        i.className="fas fa-heart fa-2x";
+        i.style.color='red';
+        a.appendChild(i);
 });
 
 
