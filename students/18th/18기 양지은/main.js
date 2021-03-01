9fea386a99cb1f8307ce76971349e70caa0c877e
Jieun-yyang <poohv7@gmail.com>
2021-03-01 20:38:06 +0900

    메인 페이지 초기 세팅 완료

--- students/18th/18기 양지은/main.js
+++ students/18th/18기 양지은/main.js
@@ -6,3 +6,28 @@ function change_color(){
     clickLike.style.color="red";
   } 
 }
+
+let clickComment = document.getElementsByClassName('enter')[0];
+let commentArea = document.querySelector('.input_comment');
+
+function addComment(){
+  const inputValue = commentArea.value
+  const wrapNickname = document.createElement('div')
+  wrapNickname.innerHTML="yang_ji_eun"
+  wrapNickname.classList.add('nickname')
+
+  const wrapDescription = document.createElement('div')
+  wrapDescription.innerHTML= inputValue
+  wrapDescription.classList.add('description')
+  
+  const container = document.createElement('div')
+  container.classList.add('wrap_description')
+
+  const textInput = document.querySelector('.section_description');
+
+  container.appendChild(wrapNickname)
+  container.appendChild(wrapDescription)
+  textInput.appendChild(container)
+
+}
+clickComment.addEventListener('click', addComment )
