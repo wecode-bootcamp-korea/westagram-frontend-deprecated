118ee2ab0a58572676e9262364fc09931e690f63
Hyun <pikatropika@gmail.com>
2021-03-04 21:35:49 +0900

    Refactor: 리뷰 수정사항 반영 완료

--- students/18th/kyunghyun/js/login.js
+++ students/18th/kyunghyun/js/login.js
@@ -2,27 +2,15 @@ const Btn = document.querySelector('.btn')
 const id = document.getElementById('id');
 const password = document.getElementById('password'); 
 
-function allClear() {
-    if (id.value !== "" && password.value !== "" && password.value.length >= 5) { 
-        return true;
-    } else {
-        return false;
-    }
+function validate() {
+    Btn.style.opacity
+    = id.value !== "" && password.value !== "" && password.value.length >= 5 ? "1" : "0.3";
 }
 
-function btnOn(allClear) { 
-    const check = allClear(); 
-    const yes = () => Btn.style.opacity = "1"; 
-    const no = () => Btn.style.opacity = "0.3";
-    return (check ? yes() : no()) 
-}
-
-id.addEventListener("keyup", ()=>{ 
-    btnOn(allClear); 
+id.addEventListener("input", ()=>{ 
+    validate(); 
 });
-password.addEventListener("keyup", ()=>{
-    btnOn(allClear);
+password.addEventListener("input", ()=>{
+    validate();
 });
-
-
 
