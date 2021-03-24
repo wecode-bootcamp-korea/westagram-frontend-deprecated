8ff1a865ae48d6d6b56832907aec5c8936297aa4
moripark32 <moripark32@gmail.com>
2021-03-24 23:44:42 +0900

    login_html_css만

---
+++ students/19th/19기_박세리/인스타만들기/js/login.js
@@ -0,0 +1,21 @@
+// var el = document.getElementById('changeProgramatic');
+// el.value='New Value'
+// el.dispatchEvent(new Event('change'));
+
+// $('input').on('keyup', function() {
+//     if (this.value.length > 1) {
+//          // do search for this.value here
+//     }
+// });
+$(".input").keyup(function(){
+//     var val = this.value;
+//     if (val.length > 1) {
+        
+//         return 
+//     }
+//     if (data) $(this).next(".combo").html(data).fadeIn(); else $(this).next(".combo").hide().html("");
+// });
+
+// $(".input").blur(function(){
+//     $(this).next(".combo").hide();
+// });
