e016b1677a1d3735054203cff0fb6a7959983f87
hyojin <gywlsh274@gmail.com>
2021-03-25 19:51:19 +0900

    login page 사이즈 수정, 추가 css

--- students/19th/hyojinkim/js/login.js
+++ students/19th/hyojinkim/js/login.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const loginBtn = document.getElementsByClassName("login_btn")[0];
 const idInput = document.getElementsByClassName("login_id")[0];
 const pwInput = document.getElementsByClassName("login_pw")[0];
@@ -7,26 +9,17 @@ loginBtn.disabled = true;
 
 function btnActive() {
   loginBtn.disabled = false;
-  loginBtn.style.background = '#0096F6';
+  loginBtn.style.background = "#0096F6";
 }
 function btnDisable() {
   loginBtn.disabled = true;
 }
 
-loginInfo.addEventListener('change', function () {  // 어떤 이벤트인지 찾는 방법은 ? 
-  if ((idInput.value === "" ) || (pwInput.value === "")) {
+loginInfo.addEventListener("keyup", function () {
+  if (idInput.value === "" || pwInput.value === "") {
     return btnDisable();
   }
-  if ((idInput.value !== "" ) && (pwInput.value !== "")) { // null vs "" -> null로하면 안되는 이유는 ? 
+  if (idInput.value !== "" && pwInput.value !== "") {
     return btnActive();
   }
 });
-
-// loginInfo.addEventListener('change', function () {  // 어떤 이벤트인지 찾는 방법은 ? 
-//   if (loginInfo.value === "" ) {
-//     return btnDisable();
-//   }
-//   if (loginInfo.value !== "" ) { // null vs "" -> null로하면 안되는 이유는 ? 
-//     return btnActive();
-//   }
-// });
