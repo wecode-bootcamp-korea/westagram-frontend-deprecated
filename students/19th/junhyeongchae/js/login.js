d52f339f458be8b00a07f0185568744d8daed46e
JunHyeongChae <78126381+JunHyeongChae@users.noreply.github.com>
2021-03-26 01:32:04 +0900

    Add:Login Page JS까지 구현

---
+++ students/19th/junhyeongchae/js/login.js
@@ -0,0 +1,9 @@
+const getId = document.getElementsByClassName("login_id")[0];
+const getPw = document.getElementsByClassName("login_pw")[0];
+const getSubmit = document.getElementsByClassName("login_form_submit")[0];
+const getLoginform = document.querySelector(".login_form");
+
+getLoginform.addEventListener("keyup", function(){
+  const changeColor = getId.value.includes('@') && getPw.value.length > 5;
+  getSubmit.disabled = changeColor ? false : true;
+})
