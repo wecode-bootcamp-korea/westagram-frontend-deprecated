1c805cd0c10d37cbdb308bd3c743d5d7318a917a
hello-dodohee <73160509+hello-dodohee@users.noreply.github.com>
2021-03-25 16:04:07 +0900

    add 스토리영역수정완료, 친구추천영역 진행중

--- students/19th/doheekim/main.js
+++ students/19th/doheekim/main.js
@@ -1,13 +1,2 @@
-const search = document.body.querySelector(".search-input");
-const searchIcon = document.body.querySelector(".search-wrap > i");
-
-search.addEventListener("keyup", function () {
-    let resultSearch = search.value;
-    console.log('search keyup function')
-
-    if(resultSearch.length >= 1){
-        searchIcon.hide();
-    }else{
-        searchIcon.show();
-    }
-});
+const nextBtn = document.querySelector(.next);
+const storyList = document.querySelector(.story-list);
