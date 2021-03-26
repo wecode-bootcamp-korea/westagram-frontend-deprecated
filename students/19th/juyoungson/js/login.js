7fe6a4267a754ca59169323a1a488ee90ea60a93
Juyoung Son <wndud0647@gmail.com>
2021-03-26 21:02:11 +0900

    추가수정

--- students/19th/juyoungson/js/login.js
+++ students/19th/juyoungson/js/login.js
@@ -4,7 +4,7 @@ function loginCheck() {
     loginButton.addEventListener("click", () => {
         const id = document.getElementById("inputId").value;
         const pw = document.getElementById("inputPw").value;
-
+        
         if(!id || !pw) {
             alert("ID/PW를 입력해주세요");
             return;
@@ -17,13 +17,10 @@ function loginCheck() {
 
         if(id !== pw) {
             alert("환영합니다.");
-            newPage();
+            return;
         }
     });
 }
 
-loginCheck();    div.innerHTML="";
-    a.innerHTML = "안녕";
-    span.innerHTML= commentValue;
-    
-    commentBox.appendChild(div).appendChild(a).appendChild(span);
+loginCheck();
+
