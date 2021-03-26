569dd3c1b5a99196169b038125f1c8052f072d5a
민석 / MIN / ミンソク <55354735+minsoku@users.noreply.github.com>
2021-03-26 17:17:04 +0900

    Add feed content and comment , image

---
+++ students/19th/js/login.js
@@ -0,0 +1,15 @@
+const Email = document.getElementById("InputEmail");
+const Password = document.getElementById("InputPassword");
+const ButtonBlue = document.getElementById("BlueBotton")
+
+const LoginBlue = () =>{
+    if(Email.value !== undefined && Password.value !== undefined){
+
+    }
+    ButtonBlue.style.backgroundColor = "red";
+}
+
+
+
+
+// LoginBlue();
