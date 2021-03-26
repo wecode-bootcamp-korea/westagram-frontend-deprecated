26a201430136f6d6ee170f14103a31c1d3a64d87
hyojin <gywlsh274@gmail.com>
2021-03-27 01:08:52 +0900

    main page HTML, CSS 구현 / login page  피드백 적용

--- students/19th/hyojinkim/js/login.js
+++ students/19th/hyojinkim/js/login.js
@@ -5,8 +5,6 @@ const idInput = document.getElementsByClassName("login_id")[0];
 const pwInput = document.getElementsByClassName("login_pw")[0];
 const loginInfo = document.getElementsByClassName("login_info")[0];
 
-loginBtn.disabled = true;
-
 function btnActive() {
   loginBtn.disabled = false;
   loginBtn.style.background = "#0096F6";
@@ -16,10 +14,10 @@ function btnDisable() {
 }
 
 loginInfo.addEventListener("keyup", function () {
-  if (idInput.value === "" || pwInput.value === "") {
-    return btnDisable();
+  if (!idInput.value || !pwInput.value) {
+    btnDisable();
   }
-  if (idInput.value !== "" && pwInput.value !== "") {
-    return btnActive();
+  if (idInput.value && pwInput.value) {
+    btnActive();
   }
 });
