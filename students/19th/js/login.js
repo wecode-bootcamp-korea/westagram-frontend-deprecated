a46192c64aef216d5af3d082a8d8c1f29d00f2b9
민석 / MIN / ミンソク <55354735+minsoku@users.noreply.github.com>
2021-03-27 16:12:04 +0900

    ID, PW validation

--- students/19th/js/login.js
+++ students/19th/js/login.js
@@ -6,14 +6,28 @@ const LoginBlue = () =>{
     if(Email.value !== "" && Password.value !== ""){
         ButtonBlue.style.backgroundColor = "rgb(0,149,247)";
     }
-    if(Email.value == "" && Password.value == ""){
+    if(Email.value == "" || Password.value == ""){
         ButtonBlue.style.backgroundColor = "rgba(0, 149, 247, 0.5)";
     }
 }
-
+const DisableButton = () =>{
+    if(Email.value.indexOf("@") == -1 || Password.ButtonBlue.length < 5){
+        ButtonBlue.disabled = true;
+        alert("asd");
+    }
+}
+const EnableButton = () =>{
+    if(Email.value.indexOf("@") !== -1 && Password.value.length > 5){
+        ButtonBlue.disabled = false;
+    }
+}
 Email.addEventListener("keyup", function(){
     LoginBlue();
+    EnableButton();
+    DisableButton();
 })
 Password.addEventListener("keyup", function(){
     LoginBlue();
+    EnableButton();
+    DisableButton();
 })
