0fa219c3643b322b2f2bfed3ed63dedba5c9a2b0
민석 / MIN / ミンソク <55354735+minsoku@users.noreply.github.com>
2021-03-28 23:29:30 +0900

    Modified waching the code review and add remove function

--- students/19th/js/login.js
+++ students/19th/js/login.js
@@ -3,17 +3,16 @@ const Password = document.getElementById("InputPassword");
 const ButtonBlue = document.getElementById("BlueButton");
 
 const LoginBlue = () =>{
-    if(Email.value !== "" && Password.value !== ""){
+    if(Email.value && Password.value ){
         ButtonBlue.style.backgroundColor = "rgb(0,149,247)";
     }
-    if(Email.value == "" || Password.value == ""){
+    if(!Email.value || !Password.value){
         ButtonBlue.style.backgroundColor = "rgba(0, 149, 247, 0.5)";
     }
 }
 const DisableButton = () =>{
     if(Email.value.indexOf("@") == -1 || Password.ButtonBlue.length < 5){
         ButtonBlue.disabled = true;
-        alert("asd");
     }
 }
 const EnableButton = () =>{
@@ -21,12 +20,12 @@ const EnableButton = () =>{
         ButtonBlue.disabled = false;
     }
 }
-Email.addEventListener("keyup", function(){
+Email.addEventListener("input", function(){
     LoginBlue();
     EnableButton();
     DisableButton();
 })
-Password.addEventListener("keyup", function(){
+Password.addEventListener("input", function(){
     LoginBlue();
     EnableButton();
     DisableButton();
--- students/19th/js/main.js
+++ students/19th/js/main.js
@@ -1,17 +1,36 @@
 const InputText = document.getElementsByClassName('FeedContentInputBoxCommentInput')[0];
-
-
+let IdCount = 0;
+let Regex = /\d+/g;
 const AddComment = () =>{
     const List = document.querySelector('.FeedContentCommentList');
+    const RemoveCommnetDiv = document.createElement('div');
     const NewList = document.createElement('li');
     const NewSpan = document.createElement('span');
+    const RemoveCommentBtn = document.createElement('div');
+    
+    RemoveCommentBtn.classList.add("Remove");
+    RemoveCommnetDiv.classList.add('RemoveCommnet');
+    RemoveCommnetDiv.appendChild(NewList);
     NewList.appendChild(NewSpan);
+
+    RemoveCommnetDiv.appendChild(RemoveCommentBtn);
+
     NewSpan.textContent = InputText.value;
-    List.append(NewList);
+
+    List.append(RemoveCommnetDiv);
+    RemoveCommentBtn.append("X");
+    RemoveCommnetDiv.id = "CommentId" + IdCount;
+    IdCount++;
     InputText.value = "";
+
+    RemoveCommentBtn.onclick = function(){
+
+    let CommentId = document.getElementById(RemoveCommnetDiv.id);
+    CommentId.remove();
     
 }
-
+    
+}
 const Enter = (e) => {
     if(e.key == "Enter"){
         AddComment();
