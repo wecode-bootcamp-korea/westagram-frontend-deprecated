e0be426c8ad3651a17363039e768d21da912d869
Kim-Heeyeol <76519867+Kim-Heeyeol@users.noreply.github.com>
2021-03-29 16:47:49 +0900

    Modified: CSS Convention, JavaSctipt 중복 코드

--- students/19th/KimHeeyeol/login.js
+++ students/19th/KimHeeyeol/login.js
@@ -1,8 +1,15 @@
-'use strict';
-
 const idForChangeColor = document.body.querySelector(".changeColorId");
 const pwForChangeColor = document.body.querySelector(".changeColorPw");
 
+const loginButtonActivation = () => {
+    const loginButtonOpacity = document.body.querySelector('.logInButton');
+
+    idForChangeColor.value && pwForChangeColor.value
+    ? ( loginButtonOpacity.style.opacity = '1')
+    : ( loginButtonOpacity.style.opacity = '0.3');
+}
+
+/*
 idForChangeColor.addEventListener("keyup", function () {
     const loginButtonOpacity = document.body.querySelector('.logInButton');
 
@@ -17,4 +24,7 @@ pwForChangeColor.addEventListener("keyup", function () {
     idForChangeColor.value && pwForChangeColor.value
     ? ( loginButtonOpacity.style.opacity = '1' )
     : ( loginButtonOpacity.style.opacity = '0.3');
-});
+});*/
+
+idForChangeColor.addEventListener("keyup", loginButtonActivation);
+pwForChangeColor.addEventListener("keyup", loginButtonActivation);
--- students/19th/KimHeeyeol/main.js
+++ students/19th/KimHeeyeol/main.js
@@ -3,15 +3,14 @@
 const newComment = document.querySelector('.commentInput');
 //const clickHeart = document.querySelector('.commentHeart');
 
-
 function createNewComment (event) {
 
     if (event.keyCode === 13) {
 
-        let commentContainer = document.querySelector('.replierMentionContainer');
-        let makeDiv = document.createElement('div');
-        let makeUserName = document.createElement('span');
-        let makeComment = document.createElement('span');
+        const commentContainer = document.querySelector('.replierMentionContainer');
+        const makeDiv = document.createElement('div');
+        const makeUserName = document.createElement('span');
+        const makeComment = document.createElement('span');
         //let makeHeart = document.createElement('div');
 
         makeDiv.className = "replierMentionContainer"
