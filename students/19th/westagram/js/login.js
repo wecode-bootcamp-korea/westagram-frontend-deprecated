199305e3245920f19e0cb9ce2f92ff7b7ee849a3
jaewon <jrsophie97@gmail.com>
2021-03-30 16:59:36 +0900

    로그인 페이지 구현

---
+++ students/19th/westagram/js/login.js
@@ -0,0 +1,19 @@
+const userId = document.querySelector(".login_id_input");
+const userPw = document.querySelector(".login_pw_input");
+const loginBtn = document.querySelector("button");
+const container = document.querySelector('section');
+
+// userId.addEventListener("keyup", handleInput);
+// userPw.addEventListener("keyup", handleInput);
+container.addEventListener("keyup", handleInput);
+
+
+function handleInput() {
+        const changeColor = userId.value.includes('@') && userPw.value.length > 1;
+    console.log('loginBtn keyup funtion');
+
+    // changeColor ? (loginBtn.style.backgroundColor = "#0094f6")
+    //     : (loginBtn.style.backgroundColor = "#0094f64b");
+    loginBtn.style.backgroundColor = changeColor ? "#0094f6" : "#0094f64b";
+
+}
