6684ef1331ed300a27b609c2acd6e48f653db34a
노선경 <nohsunkyung@noseongyeong-ui-MacBookAir.local>
2021-04-22 01:12:35 +0900

    함수 수정 및 사이드 레이아웃 수정

--- students/20th/sunkyungnoh/main/main.js
+++ students/20th/sunkyungnoh/main/main.js
@@ -2,7 +2,6 @@
 //댓글 버튼 활성화
 const commentInput = document.querySelector('.commentInput');
 const commentBtn = document.querySelector('.commentBtn');
-console.log(commentBtn.innerHTML);
 
 function active() {
     if (commentInput.value) {
@@ -16,4 +15,36 @@ function active() {
 
 commentInput.addEventListener('input', active);
 
-//
+//댓글 추가
+function addComment(crrInput) {
+    const commentModel = document.querySelector('.comment');
+    const newCommentBox = commentModel.cloneNode(true);
+    
+    newCommentBox.querySelector(".CM").innerHTML = crrInput;
+
+    const ul = document.querySelector('ul');
+    ul.append(newCommentBox)
+    
+    newCommentBox.classList.toggle('invisible')
+}
+
+//댓글 추가 이벤트 실행
+const commentForm = document.querySelector('form');
+
+commentBtn.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const crrInput = commentForm.elements.commentInput.value;
+    addComment(crrInput);
+    //입력값 초기화
+    commentForm.elements.commentInput.value = '';
+})
+
+
+
+
+
+
+
+
+
