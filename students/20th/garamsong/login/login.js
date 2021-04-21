a3464feb20b85f40e4e9c0a003e30460b1f40834
GaramSong <61130851+GarmaSong@users.noreply.github.com>
2021-04-21 17:40:39 +0900

    add: 초기세팅

---
+++ students/20th/garamsong/login/login.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const loginInputValue = document.querySelector('input').value;
+
+console.log(loginInputValue);
+
+// loginInputValue.addEventListener('keyup', activeLoginBtn);
+
+// function activeLoginBtn(){
+//     const loginInputId = document.querySelector('.loginId').value;
+//     const loginInputPassword = document.querySelector('.loginPassword').value;
+//     const loginBtn = document.querySelector('.loginBtn');
+//     if(!loginInputId && !loginInputPassword){
+//         return;
+//     } return loginBtn.style.backgroundColor = "rgba(0,149,246,1)"
+// }
