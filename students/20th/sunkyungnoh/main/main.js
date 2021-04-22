3c84a26af27858db33693a6ebd9c6c03828db4ed
노선경 <nohsunkyung@noseongyeong-ui-MacBookAir.local>
2021-04-22 19:25:01 +0900

    class를 부여하여 버튼 색상 변경하는 함수로 수정

--- students/20th/sunkyungnoh/main/main.js
+++ students/20th/sunkyungnoh/main/main.js
@@ -1,43 +1,36 @@
 'use strict'
 
-//댓글 버튼 활성화
 const commentInput = document.querySelector('.commentInput');
 const commentBtn = document.querySelector('.commentBtn');
 
 function active() {
     if (commentInput.value) {
-        commentBtn.toggleAttribute('disabled');
-        commentBtn.style.color = '#0095F6';
+        commentBtn.removeAttribute('disabled');
+        commentBtn.classList.add('active');
     } else {
-        commentBtn.toggleAttribute('disabled');
-        commentBtn.style.color = '#B3DFFC';
+        commentBtn.setAttribute('disabled', 'disabled');
+        commentBtn.classList.remove('active');
     }
 };
 
 commentInput.addEventListener('input', active);
 
-//댓글 추가
 function addComment(crrInput) {
     const commentModel = document.querySelector('.comment');
     const newCommentBox = commentModel.cloneNode(true);
-    
-    newCommentBox.querySelector(".CM").innerHTML = crrInput;
-
     const ul = document.querySelector('ul');
+
+    newCommentBox.querySelector(".CM").innerHTML = crrInput;
     ul.append(newCommentBox)
-    
     newCommentBox.classList.toggle('invisible')
 }
 
-//댓글 추가 이벤트 실행
 const commentForm = document.querySelector('form');
 
 commentForm.addEventListener('submit', (e) => {
     e.preventDefault();
-
     const crrInput = commentForm.elements.commentInput.value;
     addComment(crrInput);
-    //입력값 초기화
     commentForm.elements.commentInput.value = '';
 })
 
