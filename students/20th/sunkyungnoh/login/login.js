eedafadfca4f70070aadc719ea19e511a08e36ed
노선경 <nohsunkyung@noseongyeong-ui-MacBookAir.local>
2021-04-22 20:29:10 +0900

    인풋 안의 텍스트를 움직이는 함수 수정 & 로그인 페이지와 메인 페이지 연결

--- students/20th/sunkyungnoh/login/login.js
+++ students/20th/sunkyungnoh/login/login.js
@@ -1,4 +1,4 @@
-'use strinct'
+'use strict'
 
 const loginForm = document.querySelector('.loginForm');
 const inputLogin = document.querySelectorAll('input');
@@ -7,43 +7,31 @@ const inputID = document.querySelectorAll('input')[0];
 const inputPW = document.querySelectorAll('input')[1];
 const loginBtn = document.querySelector('.login.btn');
 
-const showBtn = document.querySelector('.hidden')
+const descriptionID = document.querySelectorAll('.form-description')[0];
+const descriptionPW = document.querySelectorAll('.form-description')[1];
 
-//로그인 설명 자리 이동
-const idDes = document.querySelectorAll('.form-description')[0];
-const pwDes = document.querySelectorAll('.form-description')[1];
-
-inputID.addEventListener('input', () => {   
-    if (inputID.value) {
-        idDes.classList.add('focused');
-        inputID.classList.add('entered');
+function moveToUpside(inputType, descriptionType) {
+    if (inputType.value) {
+        descriptionType.classList.add('focused');
+        inputType.classList.add('entered');
     } else {
-        idDes.classList.remove('focused');
-        inputID.classList.remove('entered');
+        descriptionType.classList.remove('focused');
+        inputType.classList.remove('entered');
     }
-})
+};
 
-inputPW.addEventListener('input', () => {   
-    if (inputPW.value) {
-        pwDes.classList.add('focused');
-        inputPW.classList.add('entered');
-    } else {
-        pwDes.classList.remove('focused');
-        inputPW.classList.remove('entered');
-    }
-})
+inputID.addEventListener('input', () => {moveToUpside(inputID, descriptionID)});
+inputPW.addEventListener('input', () => { moveToUpside(inputPW, descriptionPW)});
 
-//버튼 활성화
 function active() {
     if (inputID.value && inputPW.value) {
-        loginBtn.toggleAttribute('disabled');
+        loginBtn.removeAttribute('disabled');
         loginBtn.style.backgroundColor = 'rgb(0, 149, 246)';
     } else {
-        loginBtn.toggleAttribute('disabled');
+        loginBtn.setAttribute('disabled','disabled');
         loginBtn.style.backgroundColor = 'rgb(179,223,252)';
     }
 };
-//-> inline style로 하기보다는, class로 부여하기!!!
 
 inputID.addEventListener('input', active);
 inputPW.addEventListener('input', active);
