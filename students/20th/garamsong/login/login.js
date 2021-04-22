6f5b84841b119c39dc1405207f6e85f0db42f83c
GaramSong <61130851+GarmaSong@users.noreply.github.com>
2021-04-22 16:54:47 +0900

    add login page after refactoring

--- students/20th/garamsong/login/login.js
+++ students/20th/garamsong/login/login.js
@@ -4,22 +4,18 @@ const loginId = document.querySelector('.loginId');
 const loginPassword = document.querySelector('.loginPassword');
 const loginBtn = document.querySelector('.loginBtn');
 
-loginId.addEventListener('input', activeBtn);
-loginPassword.addEventListener('input', activeBtn);
-
-
 function activeBtn(){
-    let loginIdValue = loginId.value;
-    let loginPasswordValue = loginPassword.value;
-    if((loginIdValue.length) && (loginPasswordValue.length)){
-        
-        loginBtn.style.backgroundColor ="rgba(0,149,246,1)";
-        
-    } else{
-        return; 
-    }
+let loginIdValue = loginId.value;
+  let loginPasswordValue = loginPassword.value;
+  if((loginIdValue.length) && (loginPasswordValue.length)){
+    loginBtn.classList.add('afterBtnActive');
+  } else{
+      return; 
+  }
 };
 
+loginId.addEventListener('input', activeBtn);
+loginPassword.addEventListener('input', activeBtn);
 
 
 
