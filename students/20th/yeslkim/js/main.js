32a25127704e481a7149784ba1b0a3ad3efd7690
Kim Yeseul <96linda@hanmail.net>
2021-04-22 19:22:06 +0900

    Modify: 메인 페이지 리팩토링

--- students/20th/yeslkim/js/main.js
+++ students/20th/yeslkim/js/main.js
@@ -2,48 +2,29 @@ const commentForm = document.commentForm,
     submitBtn = commentForm.submitComment,
     inputComment = commentForm.inputComment
 
-// 댓글 추가
 function addComment(e) {
 e.preventDefault();
 
-const commentValue = inputComment.value,
+const comment = inputComment.value,
     commentBox = document.querySelector('.js-comments'),
     p = document.createElement('p'),
     span = document.createElement('span'),
-    comment = document.createTextNode(` ${commentValue}`),
-    myId = document.querySelector('#user-profile .userName').textContent;
+    commentNode = document.createTextNode(` ${comment}`),
+    myId = document.querySelector('#user-profile .js-user-name').textContent;
 
-span.className = 'userName';
+span.className = 'user-name';
 span.innerText = myId;
 p.appendChild(span);
-p.appendChild(comment);
+p.appendChild(commentNode);
 commentBox.appendChild(p);
 inputComment.value = '';
 }
 
-// 게시 버튼 비활성화 / 활성화
-function checkBtnDisabled() {
-    const commentValue = inputComment.value;
-    submitBtn.disabled = commentValue.length > 0 ? false : true;
+function active() {
+    const comment = inputComment.value;
+    submitBtn.disabled = comment.length > 0 ? false : true;
     return;
 }
 
 submitBtn.addEventListener('click', addComment);
-inputComment.addEventListener('keyup', checkBtnDisabled);
-
-
-/*
-메세지 인자를 받아온다
-요소를 만든다 (p, span)
-span.innerText = my_id;
-p.innerText = inputComment.value;
-
-+ 아이디 가져오기
-
-
-+ 정규표현식?
-innerComment.value 중에
-/@^\w+\s$/로 표현되는 표현은 a 태그에 담기
-
-글자 수에 따라 게시 버튼 비활성화, 활성화
-*/
+inputComment.addEventListener('keyup', active);
