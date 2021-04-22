456b8ff0cdd6d850e845bb73ffbd62fa6b9b0b0e
geborenik <geborenik@geborenikui-MacBookAir.local>
2021-04-22 14:26:28 +0900

    westagram 4/22

--- students/20th/kwonojae/js/login.js
+++ students/20th/kwonojae/js/login.js
@@ -2,32 +2,13 @@ const loginButton = document.querySelector(".loginBtn");
 const inputID = document.querySelector(".inputID");
 const inputPW = document.querySelector(".inputPW");
 
-inputID.addEventListener("keydown", function() {
-    console.log(inputID)
-    if (!inputID.value && !inputPW.value) {
-        console.log(inputID)
-        loginButton.style.backgroundColor = "red";
+function inputValueCheck() {
+    if (!(inputID.value == "") && !(inputPW.value == "")) {
+        loginButton.style.backgroundColor = "#0095F6";
+    } else {
+        loginButton.style.backgroundColor = "#c4e1fb";
     }
-});
+};
 
-inputPW.addEventListener("keydown", function() {
-    console.log(inputPW.value)
-    if (!inputID.value && !inputPW.value) {
-        loginButton.style.backgroundColor = "red";
-    }
-});
- 
-// inputPW.addEventListener("keyup", function()
-// {       
-//     const idValue = inputID.value;
-//     const pwValue = inputPW.value;
-//     // const inputID = document.querySelector(".inputID");
-//     // const inputPW = document.querySelector(".inputPW");
-//     // const loginButton = document.querySelector(".loginBtn");
-
-//     if (!idValue && !pwValue) {
-//         console.log(pwValue);
-//         loginButton.style.backgroundColor = 'red';
-
-//     } 
-// });
+inputID.addEventListener("keyup", inputValueCheck);
+inputPW.addEventListener("keyup", inputValueCheck);
