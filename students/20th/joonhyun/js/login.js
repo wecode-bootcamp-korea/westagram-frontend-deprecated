9b72a91a7dc290d7aa626bace37313bb09ba0f0f
안덕 <anduk@andeog-ui-MacBook-Pro.local>
2021-04-22 17:16:33 +0900

    modify login page and start main page layout

--- students/20th/joonhyun/js/login.js
+++ students/20th/joonhyun/js/login.js
@@ -2,14 +2,23 @@ const id = document.getElementById ("userId");
 const pw = document.getElementById ("userPw");
 
 
+function activeButton(loginButton){
+    console.log("active");
+    loginButton.disabled = false;
+    loginButton.style.backgroundColor = "#1997F3";
+}
+
+function nonActiveButton(loginButton){
+    loginButton.disabled = true;
+    loginButton.style.backgroundColor = "#B2DFFC";
+}
+
 function actLogin(){
     const loginButton = document.getElementById("userLoginBtn");
-    id.value.length >= 1  && pw.value.length >=6 ? (
-    loginButton.style.backgroundColor = "#1997F3"
-    ) : (
-    loginButton.style.backgroundColor = "#B2DFFC"
-    );
+    id.value.length >= 1  && pw.value.length >=6 ? activeButton(loginButton)
+      : nonActiveButton(loginButton)
 }
+
 const eventCheck = document.querySelector("form");
 eventCheck.addEventListener("keyup", actLogin)
 
