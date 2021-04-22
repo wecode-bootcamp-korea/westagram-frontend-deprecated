bd79b531c650144cfe1470684dd5299806199f2a
dydalsdl1414 <dydalsdl1414@gmail.com>
2021-04-23 01:18:11 +0900

    login page done, modifying main page

---
+++ students/20th/yongminJeon/westagram/js/main.js
@@ -0,0 +1,39 @@
+'use strict';
+// 댓글 작성 칸 입력값
+const comment = document.querySelector('.section-article__commentInput');
+
+// 댓글 남기기 함수
+function leaveComment() {
+  let commentValue = comment.value;
+  const ulTag = document.querySelector('.section-article__comment');
+  const listTag = document.createElement('li');
+  ulTag.appendChild(listTag);
+  listTag.innerHTML = `<div class='section-article__commentContents'><p><a class='section-article__fanslink' href='#'>yongmin</a> ${commentValue}</p><button class='section-article__commentLinkButton'><i class='far fa-heart'></i></button></div>`;
+}
+
+// 댓글 작성시 게시 버튼 활성화
+const activeCommentLeaveButton = function () {
+  comment.addEventListener('keyup', function () {
+    const commentValue = comment.value;
+    const commentUploadButton = document.querySelector(
+      '.section-article__commentUpload'
+    );
+
+    if (commentValue.length > 0) {
+      commentUploadButton.classList.add('active');
+      commentUploadButton.disabled = false;
+      commentUploadButton.addEventListener('click', leaveComment);
+    }
+
+    if (commentValue.length === 0) {
+      commentUploadButton.classList.remove('active');
+      commentUploadButton.disabled = true;
+    }
+  });
+};
+
+function init() {
+  activeCommentLeaveButton();
+}
+
+init();
