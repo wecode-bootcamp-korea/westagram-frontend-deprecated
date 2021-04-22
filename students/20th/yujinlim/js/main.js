c785b9e9c62fe6136e41c2023a3ed8de095549fa
Emewjin <76927618+Emewjin@users.noreply.github.com>
2021-04-22 21:49:48 +0900

    Add : 댓글 좋아요, 삭제 기능 구현

--- students/20th/yujinlim/js/main.js
+++ students/20th/yujinlim/js/main.js
@@ -7,9 +7,7 @@ const commentInputs = commentInputDivs.map((el) => {
 const submitBtns = commentInputDivs.map((el) => {
   return el.querySelector(".feed__submit-btn");
 });
-
 const myID = document.querySelector(".main-right__my-id").innerText;
-
 const OPACITY = "feed__submit-btn--opacity";
 
 function removeOpacity(commentInputValue, submitBtn) {
@@ -30,30 +28,89 @@ function getCommentValues(e) {
   removeOpacity(commentInputValue, submitBtn);
 }
 
-function addComment(e) {
+function updateLikeCount() {
+  this.querySelector(".far").classList.replace("far", "fas");
+  // const likesCountSpan = this.parentNode.parentNode.parentNode.parentNode
+  //   .querySelector(".feed__likes")
+  //   .querySelector(".js-likes-count");
+  // const currentLikeCount = parseInt(likesCountSpan.innerText);
+  // likesCountSpan.innerText = currentLikeCount + 1;
+}
+
+function deleteComment() {
+  const selectedComment = this.parentNode.parentNode;
+  selectedComment.remove();
+}
+
+function addComment(
+  commentColumn,
+  commentAuthor,
+  commentContent,
+  commentBtns,
+  commentList,
+  commentInput,
+  submitBtn
+) {
+  commentColumn.appendChild(commentAuthor);
+  commentColumn.appendChild(commentContent);
+  commentColumn.appendChild(commentBtns);
+  commentList.appendChild(commentColumn);
+
+  commentInput.value = "";
+  submitBtn.classList.add(OPACITY);
+
+  const commentDeleteBtns = Array.from(
+    document.getElementsByClassName("delete-btn")
+  );
+  const commentLikeBtns = Array.from(
+    document.getElementsByClassName("like-btn")
+  );
+
+  commentDeleteBtns.forEach((el) => {
+    el.addEventListener("click", deleteComment);
+  });
+  commentLikeBtns.forEach((el) => {
+    el.addEventListener("click", updateLikeCount);
+  });
+}
+
+function makeComment(e) {
+  const commentColumn = document.createElement("div");
+  const commentContent = document.createElement("span");
+  const commentAuthor = document.createElement("span");
+  const commentBtns = document.createElement("div");
+  commentBtns.classList.add("js-comment-btns");
+
   const commentInput = e.target.parentNode.querySelector(".feed__input");
   const commentInputValue = commentInput.value;
   const commentList = e.target.parentNode.parentNode
     .querySelector(".feed__texts")
     .querySelector(".js-feed-comments");
-  const commentColumn = document.createElement("div");
-  const commentContent = document.createElement("span");
-  const commentAuthor = document.createElement("span");
-  const commentHeart = document.createElement("span");
+
+  const submitBtn = e.target;
 
   if (commentInputValue) {
     commentContent.innerText = commentInputValue;
     commentAuthor.innerText = myID;
-    commentHeart.innerHTML = `<i class="far fa-heart"></i>`;
-    commentColumn.appendChild(commentAuthor);
-    commentColumn.appendChild(commentContent);
-    commentColumn.appendChild(commentHeart);
-    commentList.appendChild(commentColumn);
-    commentInput.value = "";
-    e.target.classList.add(OPACITY);
+    commentBtns.innerHTML = `<button class="js-comment-btn like-btn"><i class="far fa-heart"></i></button><button class="js-comment-btn delete-btn"><i class="fas fa-times"></i></button>`;
+
+    addComment(
+      commentColumn,
+      commentAuthor,
+      commentContent,
+      commentBtns,
+      commentList,
+      commentInput,
+      submitBtn
+    );
   } else {
     return false;
   }
+}
+
+function handleSubmitBtn(e) {
+  e.preventDefault();
+  makeComment(e);
 }
 
 commentInputs.forEach((el) => {
@@ -61,5 +118,5 @@ commentInputs.forEach((el) => {
 });
 
 submitBtns.forEach((el) => {
-  el.addEventListener("click", addComment);
+  el.addEventListener("click", handleSubmitBtn);
 });
