816181f00af743fe557716579a6a84c28ebf2bb4
Kim Yeseul <96linda@hanmail.net>
2021-04-23 09:52:20 +0900

    Add: 로그인 페이지 아이디 유효성 검사 기능 추가

--- students/20th/yeslkim/js/login.js
+++ students/20th/yeslkim/js/login.js
@@ -1,12 +1,12 @@
 const inputBox = document.querySelector('.js-input-box');
-    
 
 function active() {
     const btn = document.querySelector('.js-login-btn'),
         userId = document.querySelector('.js-input-id').value,
-        userPw = document.querySelector('.js-input-pw').value;
-    
-    btn.disabled = userId.length < 1 ? true : (userPw.length < 6 ? true : false);
+        userPw = document.querySelector('.js-input-pw').value,
+        checkId = /^\w[\w\-\.]*@\w+\.\w{2,}/;
+
+    btn.disabled = checkId.test(userId) ? (userPw.length > 5 ? false : true) : true;
 }
 
 inputBox.addEventListener('keyup', active);
