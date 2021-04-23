bb4799b8c3db0032da68d9bf8ef02e9058ab84fc
Hwisung <heesong1130@gmail.com>
2021-04-23 10:41:20 +0900

    refac_1th

--- students/20th/hwisungkim/js/main.js
+++ students/20th/hwisungkim/js/main.js
@@ -3,39 +3,40 @@ const post = document.querySelector(".post");;
 const reply_list = document.querySelector(".reply_list_all");
 const ilike = document.querySelector(".iLike");
 const del = document.querySelector(".del");
+// const search_id =
 
-reply.addEventListener('keydown',function(e){
-    if(e.keyCode===13){
-        reply_add();
-    }
-})
+reply.addEventListener('keydown', function (e) {
+        if (e.keyCode === 13) {
+            reply_add();
+        }
+    })
 
-post.addEventListener('click',function(e){
+post.addEventListener('click', function (e) {
     reply_add();
 });
 
-ilike.addEventListener('click',function(e){
+ilike.addEventListener('click', function (e) {
     like_or_dislike();
 });
 
-del.addEventListener('click',function(){
+del.addEventListener('click', function () {
     reply_del();
 })
 
-function reply_del(){
+function reply_del() {
     let reply = document.querySelector(".added_reply");
     reply.remove();
 }
 
-function like_or_dislike(){
-       let like="img/heart.png";
-       let dislike="img/like.png";
-        document.querySelector(".status_like").src=like;
+function like_or_dislike() {
+    let like = "img/heart.png";
+    //  let dislike="img/like.png";
+    document.querySelector(".status_like").src = like;
 }
 
-function reply_add(){
-   let reply = document.createElement('li');
-   reply.innerHTML = `${document.querySelector(".reply").value}`;
-   reply.className = "added_reply";
-   reply_list.appendChild(reply);
+function reply_add() {
+    let reply = document.createElement('li');
+    reply.innerHTML = `${document.querySelector(".reply").value}`;
+    reply.className = "added_reply";
+    reply_list.appendChild(reply);
 }
