8030320b2fe476092df09bc40b8cca9268dbb29e
Hoonic-kim <78582468+Hoonic-kim@users.noreply.github.com>
2021-04-23 14:38:08 +0900

    add: 수정완료

--- students/20th/hoonickim/westagram/js/login.js
+++ students/20th/hoonickim/westagram/js/login.js
@@ -1,1 +1,20 @@
-"Hello"
+
+const id = document.querySelector("#id");
+const pw = document.querySelector("#pw");
+const button = document.querySelector("button");
+
+function login() {
+    if(id.value.length > 0 && pw.value.length > 0) {
+        button.disabled = false;
+        button.style.cursor = "pointer";
+        button.style.backgroundColor = "#0095f6";
+    }
+    else {
+        button.style.cursor = "defult"
+        button.style.backgroundColor = "#bfdffd";
+    }
+};
+
+id.addEventListener('input', login);
+pw.addEventListener('input', login);
+
---
+++ students/20th/hoonickim/westagram/js/main.js
@@ -0,0 +1,18 @@
+const text = document.querySelector(".text");
+const button = document.querySelector("button");
+
+function activate() {
+    if(text.value) {
+      button.disabled = false;
+      button.style.color = "#0095f6";
+      button.style.cursor = "pointer";
+    }
+    else{
+      button.style.color = "#B2DFFC";
+      button.style.cursor = "defult"
+        }
+};
+
+text.addEventListener('input', activate);
+
+const 
