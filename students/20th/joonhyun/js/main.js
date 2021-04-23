c09fa3af9ff625c1caf5851ee36368fd2caa47d1
안덕 <anduk@andeog-ui-MacBook-Pro.local>
2021-04-23 10:43:54 +0900

    mainpage js updated

--- students/20th/joonhyun/js/main.js
+++ students/20th/joonhyun/js/main.js
@@ -1,13 +1,36 @@
 
-const replyList = document.querySelector(".userId");
-const submitBtn = document.querySelector(".uploadBtn");
-const typeArea = document.querySelector(".typeArea");
+const clickBtn = document.getElementsByTagName('button')[0]; 
+const commentInput = document.getElementsByTagName('input')[1];
 
-function addNewInput () {
-  const message = typeArea.value;
-  const newDiv = document.createElement("div");
-  newDiv.className = "userId";
-  console.log(newDiv)
-  let elem = `<div><span>yurim4545</span>${message}<span class="delBtn"><i class="fas fa-times"></i></span></div><button><i class="far fa-heart"></i></button>`;
-  commentList.append(newDiv);
-  newDiv.innerHTML = elem;
+commentInput.addEventListener("keyup", uploadComment); 
+
+function uploadComment(e) { 
+    if(e.keyCode === 13) { 
+
+        let comment = document.getElementById("commentPlus");
+        let makeDiv = document.createElement('div'); 
+        let makeUser = document.createElement('span'); 
+        let makeMsg = document.createElement('span'); 
+        let makeBtn = document.createElement('button'); 
+        
+        makeDiv.className = "commentDiv" 
+        makeUser.className = "commentUser"; 
+        makeMsg.className = "commentMsg"; 
+        makeBtn.className = "deleteBtn"; 
+        
+        makeMsg.innerHTML = commentInput.value;
+        makeUser.innerHTML = "wecode_20"; 
+        makeBtn.innerHTML = "삭제" 
+        
+        comment.appendChild(makeDiv); 
+        makeDiv.appendChild(makeUser); 
+        makeDiv.appendChild(makeMsg);
+        makeDiv.appendChild(makeBtn); 
+          
+    makeBtn.addEventListener("click", (event) => {
+        const btn = event.target; 
+        const deleteAll = btn.parentNode; 
+        comment.removeChild(deleteAll); 
+    })
+}}
+
