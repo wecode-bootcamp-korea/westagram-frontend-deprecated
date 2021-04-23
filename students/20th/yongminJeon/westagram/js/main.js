19b602c2a95718965fccaeb275ccf137c94fe601
dydalsdl1414 <dydalsdl1414@gmail.com>
2021-04-24 01:53:19 +0900

    add erase input area after upload comment

--- students/20th/yongminJeon/westagram/js/main.js
+++ students/20th/yongminJeon/westagram/js/main.js
@@ -1,6 +1,17 @@
 'use strict';
 const comment = document.querySelector('.sectionArticleCommentInput');
 
+const eraseCommentAfterUpload = function () {
+  const commentUploadButton = document.querySelector(
+    '.sectionArticleCommentUpload'
+  );
+  comment.value = '';
+  commentUploadButton.classList.remove('active');
+  commentUploadButton.disabled = true;
+  commentUploadButton.removeEventListener('click', leaveComment);
+  comment.removeEventListener('keydown', pressEnter);
+};
+
 const leaveComment = function () {
   const commentValue = comment.value;
   const ulTag = document.querySelector('.sectionArticleComment');
@@ -17,6 +28,7 @@ const leaveComment = function () {
       <i class='far fa-heart'></i>
     </button>
   </div>`;
+  eraseCommentAfterUpload();
 };
 
 const pressEnter = function (e) {
