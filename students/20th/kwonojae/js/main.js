4160e74dd43c4a5ed42259f2a950d7fc14c0008a
geborenik <geborenik@geborenikui-MacBookAir.local>
2021-04-23 20:54:37 +0900

    add: mission 6 complete.

--- students/20th/kwonojae/js/main.js
+++ students/20th/kwonojae/js/main.js
@@ -1,21 +1,34 @@
-let commentInput = document.querySelector(".writeHereComment input");
-const commentArea = document.querySelector(".feedComment");
-const postButton = document.querySelector(".postComment");
-const likeButton = document.querySelectorAll(".fa-heart");
+const commentForm = document.querySelector(".writeHereComment");
 
-function clickAddComment() {
-  let comment = document.createElement("p");
-  comment.innerHTML = commentInput.value;
+function AddComment(e) {
+  const comment = document.createElement("p");
+  const commentInput = document.querySelector(".writeHereComment input");
+  const commentArea = document.querySelector(".feedComment");
+
+  comment.innerHTML = `<span><a href="">댓글쓴사람ID</a> ${commentInput.value}</span>
+    <span class="likeDelete">
+      <i class="far fa-heart"></i>
+      <i class="fas fa-times"></i>
+    </span>`;
+
+  const heart = comment.querySelector(".fa-heart");
+  heart.addEventListener("click", () => {
+    if (heart.classList.contains("far")) {
+      heart.classList.replace("far", "fas");
+    } else {
+      heart.classList.replace("fas", "far");
+    }
+  });
+
+  const commentDelete = comment.querySelector(".fa-times");
+  commentDelete.addEventListener("click", () => {
+    comment.innerHTML = "";
+  });
+
+  comment.classList.add("commentLine");
   commentArea.appendChild(comment);
   commentInput.value = "";
+  e.preventDefault();
 }
 
-function enterAddComment(event) {
-  const enter = event.keyCode;
-  if (enter === 13) {
-    clickAddComment();
-  }
-}
-
-postButton.addEventListener("click", clickAddComment);
-commentInput.addEventListener("keydown", enterAddComment);
+commentForm.addEventListener("submit", AddComment);
