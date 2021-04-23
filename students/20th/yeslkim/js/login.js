b63e7e740100cd0cbee037c5f6ce973801880e23
Kim Yeseul <96linda@hanmail.net>
2021-04-23 09:25:07 +0900

    main.js, login.js 리팩토링

--- students/20th/yeslkim/js/login.js
+++ students/20th/yeslkim/js/login.js
@@ -5,12 +5,8 @@ function active() {
     const btn = document.querySelector('.js-login-btn'),
         userId = document.querySelector('.js-input-id').value,
         userPw = document.querySelector('.js-input-pw').value;
-
+    
     btn.disabled = userId.length < 1 ? true : (userPw.length < 6 ? true : false);
 }
 
-function init() {
-    inputBox.addEventListener('keyup', active)
-}
-
-init();
+inputBox.addEventListener('keyup', active);
--- students/20th/yeslkim/js/main.js
+++ students/20th/yeslkim/js/main.js
@@ -1,30 +1,29 @@
 const commentForm = document.commentForm,
-    submitBtn = commentForm.submitComment,
-    inputComment = commentForm.inputComment
+    inputComment = commentForm.inputComment,
+    submitBtn = commentForm.submitComment;
+    
 
 function addComment(e) {
-e.preventDefault();
+    e.preventDefault();
 
-const comment = inputComment.value,
-    commentBox = document.querySelector('.js-comments'),
-    p = document.createElement('p'),
-    span = document.createElement('span'),
-    commentNode = document.createTextNode(` ${comment}`),
-    myId = document.querySelector('#user-profile .js-user-name').textContent;
+    const p = document.createElement('p'),
+        span = document.createElement('span'),
+        myId = document.querySelector('#user-profile .js-user-name').textContent,
+        comment = document.createTextNode(` ${inputComment.value}`),
+        commentBox = document.querySelector('.js-comments');
 
-span.className = 'user-name';
-span.innerText = myId;
-p.appendChild(span);
-p.appendChild(commentNode);
-commentBox.appendChild(p);
-inputComment.value = '';
+    span.className = 'user-name';
+    span.innerText = myId;
+    p.appendChild(span);
+    p.appendChild(comment);
+    commentBox.appendChild(p);
+    inputComment.value = '';
 }
 
 function active() {
-    const comment = inputComment.value;
-    submitBtn.disabled = comment.length > 0 ? false : true;
+    submitBtn.disabled = inputComment.value.length > 0 ? false : true;
     return;
 }
 
-submitBtn.addEventListener('click', addComment);
 inputComment.addEventListener('keyup', active);
+submitBtn.addEventListener('click', addComment);
