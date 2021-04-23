53eb54e844d5fd7e58fa4fc4027ab05733b55341
Junmo Park <junmopark@Junmo-Parkui-MacBookPro.local>
2021-04-23 18:17:06 +0900

    self refactoring

--- students/20th/junmopark/main.js
+++ students/20th/junmopark/main.js
@@ -1,7 +1,6 @@
 // 전역 변수 선언
-const commentInput = document.getElementsByClassName("commentInput")[0]; /*input*/
-const commentButton = document.getElementsByClassName("commentAddBtn")[0];/*button*/
-const commentsList = document.getElementsByClassName("commentsList")[0];/*ul*/
+const commentInput = document.getElementsByClassName("commentInput")[0];
+const commentsList = document.getElementsByClassName("commentsList")[0];
 const form = document.querySelector('.commentBox');
 
 // 코멘트 푸쉬 함수
