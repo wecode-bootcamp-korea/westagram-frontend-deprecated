29f08111200d41300e0cf46e81701c7045ac35b6
노선경 <nohsunkyung@noseongyeong-ui-MacBookAir.local>
2021-04-24 09:36:50 +0900

    반복문으로 수정하고 move함수 인자 줄임

--- students/20th/sunkyungnoh/login/login.js
+++ students/20th/sunkyungnoh/login/login.js
@@ -2,53 +2,37 @@
 
 const loginForm = document.querySelector('.loginForm');
 const inputLogin = document.querySelectorAll('input');
-
 const inputID = document.querySelectorAll('input')[0];
 const inputPW = document.querySelectorAll('input')[1];
 const loginBtn = document.querySelector('.login.btn');
 
-const descriptionID = document.querySelectorAll('.form-description')[0];
-const descriptionPW = document.querySelectorAll('.form-description')[1];
+function moveToUpside(input) {
+    const descriptionType = input.previousElementSibling
 
-function moveToUpside(inputType, descriptionType) {
-    if (inputType.value) {
+    if (input.value) {
         descriptionType.classList.add('focused');
-        inputType.classList.add('entered');
+        input.classList.add('entered');
     } else {
         descriptionType.classList.remove('focused');
-        inputType.classList.remove('entered');
+        input.classList.remove('entered');
     }
 };
-
-// 1번
-// loginForm.addEventListener('input', (e) => { moveToUpside(e.target, e.target.previousElementSibling) })
-
-
-// loginForm.addEventListener('input', (e) => {moveToUpside(e.target, e.target.previousElementSibling)})
-
-
-//2번
-// inputID.addEventListener('input', () => {moveToUpside(inputID, descriptionID)});
-// inputPW.addEventListener('input', () => {moveToUpside(inputPW, descriptionPW)});
 
 function active() {
     if (inputID.value && inputPW.value) {
         loginBtn.removeAttribute('disabled');
         loginBtn.classList.add('active');  
     } else {
-        loginBtn.setAttribute('disabled', 'disabled');
+        loginBtn.setAttribute('disabled', 'true');
         loginBtn.classList.remove('active')
     }
 };
 
-//3번
-const init = () => {
+const login = () => {
     inputLogin.forEach(input => {
         input.addEventListener('input', active)
-
-        input.addEventListener('input', (e) => {moveToUpside(e.target, e.target.previousElementSibling)})
+        input.addEventListener('input', () => moveToUpside(input))
     })
 };
 
-init();
-
+login();
