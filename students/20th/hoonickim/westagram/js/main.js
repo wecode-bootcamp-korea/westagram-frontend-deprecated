51f950bcedb0e3a6cb13910b4d658b9c39351ad6
Hoonic-kim <78582468+Hoonic-kim@users.noreply.github.com>
2021-04-26 17:58:35 +0900

    add: 댓글기능 추가

--- students/20th/hoonickim/westagram/js/main.js
+++ students/20th/hoonickim/westagram/js/main.js
@@ -1,16 +1,41 @@
-const text = document.querySelector(".text");
-const button = document.querySelector("button");
+const commentInput = document.querySelector(".commentInput");
+const commentButton = document.querySelector(".commentButton");
 
-function activate() {
-    if(text.value) {
-      button.disabled = false;
-      button.style.color = "#0095f6";
-      button.style.cursor = "pointer";
+function colorActivate() {
+    if(commentInput.value) {
+      commentButton.removeAttribute("disabled");
+      commentButton.classList.add("buttonActive");
     }
-    else{
-      button.style.color = "#B2DFFC";
-      button.style.cursor = "defult"
-        }
+    else {
+      commentButton.setAttribute("disabled", "disabled");
+      commentButton.classList.remove("buttonActive")
+    }
 };
 
-text.addEventListener('input', activate);
+commentInput.addEventListener('input', colorActivate);
+
+
+// add comment
+
+const commentLists = document.querySelector('.commentLists');
+
+const addComment = () => {
+  // const commentList = document.createElement('li');
+  const commentText = document.createElement('p');
+
+  // commentList.className = 'commentList';
+  commentText.className = 'commentTxet';
+
+  // commentLists.appendChild(commentList);
+  commentLists.appendChild(commentText);
+
+  commentText.innerText = commentInput.value;
+  commentInput.value = '';
+
+};
+
+  commentInput.addEventListener('keyup', (e) => {
+    if(e.keyCode === 13) {
+      addComment();
+    }
+  });
