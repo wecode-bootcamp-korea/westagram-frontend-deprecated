85d4af99c27da1e690a0c96d8868f9aad9dec597
노선경 <nohsunkyung@noseongyeong-ui-MacBookAir.local>
2021-04-26 13:40:37 +0900

    🎨 format of the code

--- students/20th/sunkyungnoh/login/login.js
+++ students/20th/sunkyungnoh/login/login.js
@@ -1,34 +1,33 @@
 'use strict'
 
-const loginForm = document.querySelector('.loginForm');
-const inputID = document.querySelectorAll('input')[0];
-const inputPW = document.querySelectorAll('input')[1];
-const loginBtn = document.querySelector('.login.btn');
+const loginForm = document.querySelector('.loginForm')
+const inputID = document.querySelectorAll('input')[0]
+const inputPW = document.querySelectorAll('input')[1]
+const loginBtn = document.querySelector('.login.btn')
 
 function moveLabelUp(input) {
-    const descriptionType = input.previousElementSibling
+  const descriptionType = input.previousElementSibling
 
-    if (input.value) {
-        descriptionType.classList.add('focused');
-        input.classList.add('entered');
-    } else {
-        descriptionType.classList.remove('focused');
-        input.classList.remove('entered');
-    }
-};
+  if (input.value) {
+    descriptionType.classList.add('focused')
+    input.classList.add('entered')
+  } else {
+    descriptionType.classList.remove('focused')
+    input.classList.remove('entered')
+  }
+}
 
 function enableLogIn() {
-    if (inputID.value.includes('@') && inputPW.value.length >= 6) {
-        loginBtn.removeAttribute('disabled');
-        loginBtn.classList.add('active');  
-    } else {
-        loginBtn.setAttribute('disabled', 'true');
-        loginBtn.classList.remove('active')
-    }
-};
+  if (inputID.value.includes('@') && inputPW.value.length >= 6) {
+    loginBtn.removeAttribute('disabled')
+    loginBtn.classList.add('active')
+  } else {
+    loginBtn.setAttribute('disabled', 'true')
+    loginBtn.classList.remove('active')
+  }
+}
 
 loginForm.addEventListener('input', (e) => {
-  enableLogIn();
-  moveLabelUp(e.target);
-});
-
+  enableLogIn()
+  moveLabelUp(e.target)
+})
--- students/20th/sunkyungnoh/main/main.js
+++ students/20th/sunkyungnoh/main/main.js
@@ -1,39 +1,39 @@
 'use strict'
 
-const commentInput = document.querySelector('.commentInput');
-const commentBtn = document.querySelector('.commentBtn');
+const commentInput = document.querySelector('.commentInput')
+const commentBtn = document.querySelector('.commentBtn')
 
 function changeColorOfBtn() {
-    if (commentInput.value) {
-        commentBtn.removeAttribute('disabled');
-        commentBtn.classList.add('active');
-    } else {
-        commentBtn.setAttribute('disabled', 'disabled');
-        commentBtn.classList.remove('active');
-    }
-};
-
-commentInput.addEventListener('input', changeColorOfBtn);
-
-function addComment(crrInput) {
-    const commentModel = document.querySelector('.comment');
-    const newCommentBox = commentModel.cloneNode(true);
-    const ul = document.querySelector('ul');
-
-    newCommentBox.querySelector(".CM").innerHTML = crrInput;
-    ul.append(newCommentBox)
-    newCommentBox.classList.toggle('invisible')
+  if (commentInput.value) {
+    commentBtn.removeAttribute('disabled')
+    commentBtn.classList.add('active')
+  } else {
+    commentBtn.setAttribute('disabled', 'disabled')
+    commentBtn.classList.remove('active')
+  }
 }
 
-const commentForm = document.querySelector('form');
+commentInput.addEventListener('input', changeColorOfBtn)
+
+function addComment(crrInput) {
+  const commentModel = document.querySelector('.comment')
+  const newCommentBox = commentModel.cloneNode(true)
+  const ul = document.querySelector('ul')
+
+  newCommentBox.querySelector('.CM').innerHTML = crrInput
+  ul.append(newCommentBox)
+  newCommentBox.classList.toggle('invisible')
+}
+
+const commentForm = document.querySelector('form')
 
 commentForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const crrInput = commentForm.elements.commentInput.value;
-    if (!crrInput) {
-        alert("댓글을 입력해주세요!")
-    } else {
-      addComment(crrInput);
-    }
-    commentForm.elements.commentInput.value = '';
+  e.preventDefault()
+  const crrInput = commentForm.elements.commentInput.value
+  if (!crrInput) {
+    alert('댓글을 입력해주세요!')
+  } else {
+    addComment(crrInput)
+  }
+  commentForm.elements.commentInput.value = ''
 })
