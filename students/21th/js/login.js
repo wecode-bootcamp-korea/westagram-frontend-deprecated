61522a8125a04c59b95232b27172430384baf72d
무제 <mint1480123@gmail.com>
2021-05-22 19:34:30 +0900

    login page refactoring

---
+++ students/21th/js/login.js
@@ -0,0 +1,24 @@
+const inputItems = document.querySelector(".input_container");
+const inputId = inputItems.querySelector(".id");
+const inputPw = inputItems.querySelector(".password");
+const logInButton = inputItems.querySelector(".log_in_button");
+
+const loginEventTrue = () => {
+    logInButton.disabled = false;
+    logInButton.style.cursor = "pointer";
+    logInButton.style.backgroundColor = "#0095f6";
+    inputItems.addEventListener("key", (e) => {
+        if (e.key === "Enter") {
+        }
+    });
+};
+const loginEventFalse = () => {
+    logInButton.disabled = true;
+    logInButton.style.cursor = "default";
+    logInButton.style.backgroundColor = "#B9DFFC";
+};
+
+inputItems.addEventListener("keyup", (logInValid) => {
+    const inputValid = inputId.value.length >= 2 && inputPw.value.length >= 5 && inputId.value.includes("@");
+    inputValid ? loginEventTrue() : loginEventFalse();
+});
