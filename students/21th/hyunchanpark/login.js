d2fd8dbf8714b20db4becb98424c22ce6cfc4525
Eyes0n <eye3570@naver.com>
2021-05-22 21:20:06 +0900

    Add : 로그인 페이지 로그인 검증 기능 추가 - 정규 표현식을 사용하여 ID와 PW의 유효성을 검증 기능 구현

--- students/21th/hyunchanpark/login.js
+++ students/21th/hyunchanpark/login.js
@@ -3,13 +3,27 @@ const inputEls = document.querySelectorAll("input");
 const inputId = document.querySelector("input[type=text]");
 const inputPassword = document.querySelector("input[type=password]");
 
+const emailRegExp =
+  /[a-zA-Z0-9.-_+!]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]{2,}(?:.[a-zA-Z0-9]{2,3})?/;
+
+const passwordRegExp = /[a-zA-Z0-9]{5,100}/;
+
+const isValidateEmailAndPassword = (email, password) => {
+  let isValidEmail = false;
+  let isValidPassword = false;
+
+  if (email.match(emailRegExp)) isValidEmail = true;
+  if (password.match(passwordRegExp)) isValidPassword = true;
+
+  return isValidEmail && isValidPassword;
+};
+
 const buttonCondition = () => {
-  if (inputId.value.length !== 0 && inputPassword.value.length !== 0) {
+  if (isValidateEmailAndPassword(inputId.value, inputPassword.value)) {
     return true;
   } else return false;
 };
 
-// 재활용 할 수 있을거 같은데 loginButton -> this?
 const opacityAndDisableButton = (value, boolean) => {
   loginButton.style.opacity = value;
   loginButton.disabled = boolean;
