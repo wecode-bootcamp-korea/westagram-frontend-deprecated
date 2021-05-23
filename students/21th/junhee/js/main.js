ec157a9d7284ea339f594e03a64940d42ae6ce86
June <juneandkang@gmail.com>
2021-05-23 18:08:34 +0900

    Modify:리팩토링 및 코드 질문

--- students/21th/junhee/js/main.js
+++ students/21th/junhee/js/main.js
@@ -1,16 +1,49 @@
-const listContainer = document.querySelector('.list-container');
+const listContainer = document.querySelector('.listContainer');
 const commentContent = document.querySelector('.commentText');
-const uploadButton = document.querySelector('.uploadBtn');
+const uploadButton = document.querySelector('.uploadButton');
+const myNameId = document.getElementsByClassName('myNameText');
 
 
 const addComment = () =>{
     const contentValue = commentContent.value;
     const newComment = document.createElement('li');
+    const strongId = document.createElement('a');
+    const listBox = document.createElement('div');
+
+    strongId.innerHTML = myNameId[0].innerText;
+    strongId.setAttribute("href","#");
+    strongId.classList.add("myCommentId");
     newComment.innerHTML = contentValue;
-    newComment.classList.add('comment-content')
-    listContainer.appendChild(newComment);
+    newComment.classList.add('commentContent');
+    listBox.classList.add('myCommentWrapper');
+    listBox.appendChild(strongId);
+    listBox.appendChild(newComment);
+    listContainer.appendChild(listBox);
     commentContent.value = "";
 }
  
 uploadButton.addEventListener('click',addComment);
 
+const addCommentEnter = (event) => {
+    const contentValue = commentContent.value;
+    const newComment = document.createElement('li');
+    const strongId = document.createElement('a');
+    const listBox = document.createElement('div');
+
+    if(event.key==='Enter'){
+    strongId.innerHTML = myNameId[0].innerText;
+    strongId.setAttribute("href","#");
+    strongId.classList.add("myCommentId");
+    newComment.innerHTML = contentValue;
+    newComment.classList.add('commentContent');
+    listBox.classList.add('myCommentWrapper');
+    listBox.appendChild(strongId);
+    listBox.appendChild(newComment);
+    listContainer.appendChild(listBox);
+    commentContent.value = "";
+
+    }
+
+}
+
+commentContent.addEventListener('keydown',addCommentEnter);
