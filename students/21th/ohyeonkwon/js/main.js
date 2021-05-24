4062a546eff3cc929bbf172f369ed9c006a2f0a8
mac <mac@Bingbing.local>
2021-05-24 19:40:13 +0900

    ADD:댓글 추가 기능완료

--- students/21th/ohyeonkwon/js/main.js
+++ students/21th/ohyeonkwon/js/main.js
@@ -1,12 +1,11 @@
 /*main Script*/
-let commentInput=document.querySelector(".comment-input").value ='';
-let commentInput = document.querySelector(".comment-input");
-let commentBtn = document.querySelector(".upload-btn");
-let ul = document.querySelector(".comment-ul")
+const commentInput = document.querySelector(".comment-input");
+const commentBtn = document.querySelector(".upload-btn");
+const ul = document.querySelector(".comment-ul")
 
 function uploadBtnChange(){
     
-    let InputValue = commentInput.value;
+    const InputValue = commentInput.value;
     
 
     if(InputValue.length>0){
@@ -23,28 +22,30 @@ function uploadBtnChange(){
 
 function addcomment(){
     
-    let li = document.createElement("li");
-    console.log(li);
-    
-    let commentContainer = document.createElement("div");
-    console.log(commentContainer);
-    let textBox = document.createElement("span");
-    console.log(textBox);
-    let name = document.createElement("span");
-    console.log(name);
+    const li = document.createElement("li");  
+    const commentContainer = document.createElement("div");
+    const textBox = document.createElement("span");  
+    const name = document.createElement("span");
+    const commentText = document.createElement("span");
    
     commentContainer.classList.add('comment-container');
     li.classList.add('comment-li');
     textBox.classList.add('comment-text');
     name.classList.add('comment-name');
+    commentText.classList.add('comment');
+    
+    name.innerText = 'headbingbing__';
+    commentText.innerText = commentInput.value;
+    
+    li.appendChild(commentContainer)
+    commentContainer.appendChild(textBox);
+    textBox.appendChild(name);
+    textBox.appendChild(commentText);
+      
+ul.appendChild(li);
 
-    textBox=commentInput.value;
-    newTextBox.innerText = textBox
+console.log(li);
 
-                                   ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋZzzzzzzzz
-    //ul.appendChild(commentContainer);
-    ul.appendChild(li);
-  //ul.appendChild(textBox);
 }
 
 commentBtn.addEventListener('click',addcomment)
