28e052890a080bf930ca7d0f5492b6d84c6ee0d8
unseo Jang <unseojang@seoun-iui-maegbug.local>
2021-06-16 18:04:35 +0900

    westagram html,css compelet

---
+++ students/22th/unseoJang/westargram/tobe/js/login.js
@@ -0,0 +1,8 @@
+const userId = document.getElementById('userId');
+const userPw = document.getElementById('userPassWord');
+const btnLogin = document.getElementsByClassName('btn-login');
+
+userId.addEventListener("keyup", function (event) {
+    console.log('aa');
+});
+
