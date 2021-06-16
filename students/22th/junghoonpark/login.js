439e97c940c9a7b3fe7849341ae90f8038b377c8
박정훈 <parkjeonghoon@park-ui-MacBook-Pro.local>
2021-06-16 17:27:44 +0900

    [Add] 인스타그램 로그인 페이지 - 미션1 레이아웃 - 레이아웃 완성(Flex 사용) - input 창 크기조절 보완 필

---
+++ students/22th/junghoonpark/login.js
@@ -0,0 +1,30 @@
+// const thisIsButton = document.getElementsByClassName('login-btn')[0];
+
+// thisIsButton.addEventListener('click', function() {
+//   const password = document.getElementById('typingID').value;
+//   const rePassword = document.getElementById('password').value;
+  
+//   if (!password) {
+//     alert('비밀번호를 입력해주세요!');
+//     return;
+//   }
+  
+//   if (!rePassword) {
+//     alert('비밀번호 확인을 입력해주세요!');
+//     return;
+//   }
+  
+//   if (password !== rePassword) {
+//     alert('비밀번호가 맞지 않습니다!');
+//     return;
+//   }
+  
+//   alert('로그인 성공!!');
+// });
+
+// const thisIsPw = document.getElementById('password');
+// const thisIsCode = document.getElementById('code');
+
+// thisIsPw.addEventListener('keyup', function(event) {
+//   thisIsCode.innerHTML = event.keyCode;
+// });
