678bb76029edcea477e4f0967bb9f3e36461bc22
Somi Hwang <somihwang@Somis-MacBook-Pro.local>
2021-06-17 16:20:17 +0900

    Add: add comments

--- students/22th/somihwang/js/main.js
+++ students/22th/somihwang/js/main.js
@@ -1,3 +1,48 @@
-// 검색 클릭 이벤트
+//selectors
+const commentBtn = document.querySelector(".commentBtn");
+const commentList = document.querySelector(".commentList");
+const commentInput = document.querySelector(".commentInput");
+const userID = document.querySelector(".profileID").textContent;
 
-// 댓글 추가 기능
+//eventListeners
+commentBtn.addEventListener("click", addComment);
+commentInput.addEventListener("keyup", colorChange);
+
+//functions
+//댓글추가기능
+function addComment(e) {
+    //댓글 새로고침 방지
+    e.preventDefault();
+
+    //add comment box div
+    const commentBox = document.createElement("div");
+    commentBox.classList.add("commentBox");
+
+    //add id
+    const newIdSpan = document.createElement("span");
+    newIdSpan.classList.add("igID");
+    newIdSpan.innerHTML = userID;
+    commentBox.appendChild(newIdSpan);
+
+    //add comment content
+    const newCommentContent = document.createElement("span");
+    newCommentContent.classList.add("commentContent");
+    newCommentContent.innerText = commentInput.value;
+    commentBox.appendChild(newCommentContent);
+
+    if(commentInput.value){
+        //appendchild
+        commentList.appendChild(commentBox);
+        //clear input
+        commentInput.value = "";
+    }
+};
+
+//Post 버튼 색상변경기능
+function colorChange(){
+    if(commentInput.value){
+        commentBtn.classList.add("activate")
+    }else if(!commentInput.value){
+        commentBtn.classList.remove("activate")
+    }
+};
