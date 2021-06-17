f81d446554066c4b1d55abf7ec38055847f9874b
성정준 <sungjungjun@seongjeongjun-ui-MacBookPro.local>
2021-06-17 16:34:54 +0900

    [ADD] 추가기능 구현 - Mission5 ID,PW vaildation - Mission6 댓글 좋아요/삭제 기능 - Missuin9 반응형 구현 [MODIFY] 수정사항 - HTML,CSS,JS 링크 재연결

--- students/22th/jungjun/js/login.js
+++ students/22th/jungjun/js/login.js
@@ -1,22 +1,47 @@
-window.onload = function () {
-  const inputId = document.getElementById('inputId');
-  const inputPassword = document.getElementById('inputPassword');
-  const loginBtn = document.getElementById('loginBtn');
+const inputId = document.getElementById('inputId');
+const inputPassword = document.getElementById('inputPassword');
+const loginBtn = document.getElementById('loginBtn');
 
-  inputId.addEventListener('keyup', () => {
-    valueCheck();
-  });
-  inputPassword.addEventListener('keyup', () => {
-    valueCheck();
-  });
-  function valueCheck() {
-    let inputIdValue = inputId.value;
-    let inputPasswordValue = inputPassword.value;
+inputId.addEventListener('keyup', () => {
+  valueCheck();
+});
+inputPassword.addEventListener('keyup', () => {
+  valueCheck();
+});
 
-    if (inputIdValue || inputPasswordValue) {
-      loginBtn.classList.add('active');
-    } else {
-      loginBtn.classList.remove('active');
-    }
+loginBtn.addEventListener('click', () => {
+  let inputIdValue = inputId.value;
+  let inputPasswordValue = inputPassword.value;
+
+  console.log(vaildateEmail(inputIdValue));
+  if (vaildateEmail(inputIdValue) === false && inputPasswordValue.length < 9) {
+    alert('Please check your Email and Password!');
+  } else if (vaildateEmail(inputIdValue) === false) {
+    alert('Please check your Email!');
+  } else if (inputPasswordValue.length < 9) {
+    alert('Please check your Password!');
+  } else {
+    alert('Vaild your Email and Password!');
   }
-};
+});
+
+function valueCheck() {
+  let inputIdValue = inputId.value;
+  let inputPasswordValue = inputPassword.value;
+
+  if (inputIdValue || inputPasswordValue) {
+    loginBtn.classList.add('active');
+  } else {
+    loginBtn.classList.remove('active');
+  }
+
+  // vaildateEmail('jungzkxm@name.com');
+}
+
+function vaildateEmail(email) {
+  console.log(email);
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  let check = re.test(String(email).toLowerCase());
+  return check;
+}
--- students/22th/jungjun/js/main.js
+++ students/22th/jungjun/js/main.js
@@ -1,29 +1,60 @@
-window.onload = function () {
-  const form = document.getElementById('form');
-  const input = document.getElementById('input');
-  // const likesBtn = document.getElementsByClassName('likesBtn');
+const form = document.getElementById('form');
+const input = document.getElementById('input');
 
-  form.addEventListener('submit', e => {
-    e.preventDefault();
-    addList();
-  });
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  addList();
+});
 
-  function addList() {
-    const commentList = input.value;
+// form이 subbit안되어도 기본으로 적용될 사항
+likedComment();
+deleteList();
 
+function addList() {
+  const commentList = input.value;
+  if (commentList === '') {
+    return alert('댓글을 입력해주세요');
+  } else {
     let createEle = document.createElement('div');
-    let a = (createEle.innerHTML = `
-    <div class="commentSection">
-    <p class="personalAccount">아이디~<span class="commnets">${commentList}</span></p>
-    <i class="likesBtn far fa-heart"></i>
-    </div>
-    `);
-    document.getElementById('mbWrittenComments').innerHTML += a;
-    input.value = '';
+    let innerEle = (createEle.innerHTML = `
+      <div class="commentSection">
+      <p class="personalAccount">아이디 <span class="commnets">${commentList}</span></p>
+      <div class="commentIconWrap">
+        <i class="likesBtn far fa-heart"></i> 
+        <i class="far fa-trash-alt deleteBtn"></i>
+      </div>
+      </div>
+      `);
+    document.getElementById('mbWrittenComments').innerHTML += innerEle;
   }
+  //   새로운 댓글이 생성될때 like와 delete할 수 있는 기능을 준다.
+  likedComment();
+  deleteList();
+  input.value = '';
+}
+function likedComment() {
+  const likesBtn = document.querySelectorAll('.likesBtn');
 
-  // likesBtn.addEventListener('click', () => {
-  //   console.log('ksdas');
-  //   likesBtn.classList.toggle('likedBtn');
-  // });
-};
+  for (let i = 0; i < likesBtn.length; i++) {
+    likesBtn[i].addEventListener('click', e => {
+      let result = likesBtn[i].classList.contains('far');
+      if (result) {
+        likesBtn[i].classList.remove('far');
+        likesBtn[i].classList.add('fas');
+      } else {
+        likesBtn[i].classList.remove('fas');
+        likesBtn[i].classList.add('far');
+      }
+    });
+  }
+}
+
+function deleteList() {
+  const deleteBtn = document.querySelectorAll('.deleteBtn');
+
+  for (let i = 0; i < deleteBtn.length; i++) {
+    deleteBtn[i].addEventListener('click', () => {
+      deleteBtn[i].parentElement.parentElement.remove();
+    });
+  }
+}
