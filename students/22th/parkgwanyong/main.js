93af76cfc110ade66ab7d4013008f7abdc889586
gwanyong <78327961+gwanyong@users.noreply.github.com>
2021-06-17 13:24:40 +0900

    Add : 댓글 추가 및 삭제 기능 완
    레이아웃 완성
    댓글 enter or  버튼 클릭시 추가 기능
    댓글 삭제 아이콘 클릭 시 삭제기능

---
+++ students/22th/parkgwanyong/main.js
@@ -0,0 +1,73 @@
+const replyBtn = document.querySelector(".replyBtn");
+const insertInput = document.querySelector(".replyInput");
+const deleteBtn = document.querySelectorAll(".fa-trash-alt");
+
+replyBtn.addEventListener("click", () => {
+    if (insertInput.value == "") {
+        alert("댓글을 입력해주세요");
+        return;
+    }
+    insertReplyTag();
+    insertInput.value = "";
+});
+
+insertInput.addEventListener("keyup", (e) => {
+    if (insertInput.value == "") {
+        alert("댓글을 입력해주세요");
+        return;
+    } else {
+        if (e.key === "Enter") {
+            insertReplyTag();
+            insertInput.value = "";
+        }
+    }
+});
+
+const removeReply = () => {
+    for (let ele of deleteBtn) {
+        ele.addEventListener("click", (event) => {
+            event.target.parentElement.parentElement.remove();
+        });
+    }
+};
+removeReply();
+
+const insertReplyTag = () => {
+    const replyBox = document.querySelector(".replyBox");
+    const replyInput = document.querySelector(".replyInput");
+
+    let inputText = replyInput.value;
+
+    const divTag = document.createElement("div");
+    const userInfoTag = document.createElement("div");
+    const spanTag = document.createElement("span");
+    const pTag = document.createElement("p");
+    const optionIcons = document.createElement("div");
+    const heart = document.createElement("i");
+    const trash = document.createElement("i");
+
+    replyBox.appendChild(divTag);
+    divTag.classList.add("replyColumn");
+    userInfoTag.classList.add("userInfo");
+    spanTag.classList.add("userId");
+    pTag.classList.add("replyContent");
+    optionIcons.classList.add("optionIcons");
+    heart.classList.add("far", "fa-heart");
+    trash.classList.add("far", "fa-trash-alt");
+
+    pTag.innerHTML = inputText;
+
+    divTag.appendChild(userInfoTag);
+    divTag.appendChild(optionIcons);
+    optionIcons.append(heart);
+    optionIcons.append(trash);
+    userInfoTag.prepend(spanTag);
+    userInfoTag.append(pTag);
+    spanTag.innerHTML = "댓글다는사람";
+
+    trash.addEventListener("click", (e) => {
+        e.target.parentElement.parentElement.remove();
+    });
+};
+
+removeReply();
