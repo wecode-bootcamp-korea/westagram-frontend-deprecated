3433b68c51d1d4042cdb6f618dcf884a0a7efa77
yunkyung <poew91@gmail.com>
2021-06-18 17:30:36 +0900

    [Add] 메인 페이지 댓글 기능 구현 완료

--- students/22th/yunkyunglee/js/main.js
+++ students/22th/yunkyunglee/js/main.js
@@ -4,6 +4,14 @@ const list = document.getElementById('list');
 
 replyButton.addEventListener('click', function(e) {
     const temp = document.createElement('li');
-    temp.innerHTML = input.value;
+    temp.innerHTML = `<span class="name"><strong> zzz_yk</strong></span><span>${input.value}</span>`;
     list.appendChild(temp);
+
+    input.value="";
 });
+
+function enterKey() {
+    if(e.keyCode == 13) {
+
+    }
+}
