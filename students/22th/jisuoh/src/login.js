503896bb65c92effde21e85758873e0335537c21
silviaoh <silvia.jisu.oh@gmail.com>
2021-06-18 20:52:01 +0900

    Refactor: login.js 리팩토링

--- students/22th/jisuoh/src/login.js
+++ students/22th/jisuoh/src/login.js
@@ -1,40 +1,32 @@
 "use strict";
-
-// 아이디 받아오기
-// 패스워드 받아오기
-// 버튼 받아오기
-// 아이디, 패스워드 각각 한 글자 이상이면
-// true -> 버튼 활성화
-// false -> 버튼 비활성화
-const inputId = document.querySelector(".text-id");
-const inputPw = document.querySelector(".pw");
+const form = document.querySelector(".login-form");
 const btn = document.querySelector(".btn-login");
-
-// 1. id @ 없으면 alert 뜨도록
-// 2. pw 5글자 이상이어야 함 5글자 미만이면 알람창
 
 function checkValidation() {
   const valueId = inputId.value;
   const valuePw = inputPw.value;
 
-  if (!valueId.includes("@")) alert("@를 포함시켜 주세요!");
-  else if (valuePw.length < 5) alert("비밀번호는 5글자 이상이여야 합니다!");
-}
-
-function checkMoreThanOneLetter() {
-  // input : onChange or onKeyPress 이벤트 걸어서 값이 있는지를 실시간으로 검사
-  // 버튼 -> setAttribute('disable', 'disabled');
-  // removeAttribute('disable');
-  const lengthId = inputId.value.length;
-  const lengthPw = inputPw.value.length;
-
-  if (!lengthId || !lengthPw) {
-    btn.setAttribute("disabled", "disabled");
-  } else {
-    btn.removeAttribute("disabled", "disabled");
+  if (!valueId.includes("@")) {
+    alert("@를 포함시켜 주세요!");
+  } else if (valuePw.length < 5) {
+    alert("비밀번호는 5글자 이상이여야 합니다!");
   }
 }
 
-inputId.addEventListener("keyup", checkMoreThanOneLetter);
-inputPw.addEventListener("keyup", checkMoreThanOneLetter);
-btn.addEventListener("click", checkValidation);
+function checkMoreThanOneLetter(e) {
+  const inputId = document.getElementsByClassName("text-id")[0].value;
+  const inputPW = document.getElementsByClassName("pw")[0].value;
+
+  if (!inputId || !inputPW) {
+    btn.setAttribute("disabled", "");
+  } else {
+    btn.removeAttribute("disabled", "");
+  }
+}
+
+const init = () => {
+  form.addEventListener("keyup", checkMoreThanOneLetter);
+  btn.addEventListener("click", checkValidation);
+};
+
+init();
