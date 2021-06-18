c23fa156325c52110585c2ae1d73501ce4c9fc06
Soojong Kim <soojongkim@Soojongui-MacBookPro.local>
2021-06-18 15:03:19 +0900

    메인페이지 댓글 기능 수정 완료

--- students/22th/soojongkim/js/main.js
+++ students/22th/soojongkim/js/main.js
@@ -1,17 +1,23 @@
 "use strict";
 
 let inputComment = document.getElementById("input_comment");
-let commentSubmit = document.getElementById("comment_submit");
-let commentTime = document.getElementsByClassName("comment_time")[0]
+let submitBtn = document.getElementById("comment_submit");
+let myComment = document.getElementsByClassName("new_comment")[0];
 
-inputComment.addEventListener("keyup", function (event) {
+inputComment.addEventListener("keyup", function() {
+  if (event.key === "Enter") {
+    var newComment = document.createElement("p");
+    newComment.innerHTML = "<b>Soox_jk</b> " + "" + this.value;
+    myComment.appendChild(newComment);
+    inputComment.value = "";
+  }
+});
 
-    if (event.key === "Enter") {
-        let newComment = document.createElement('li')
-
-        newComment.innerHTML = inputComment.value;
-        commentTime.appendChild(newComment)
-
-    }
-
+submitBtn.addEventListener("click", function() {
+  if (inputComment.value) {
+    var newComment = document.createElement("p");
+    newComment.innerHTML = "<b>Soox_jk</b> " + "" + newComment.value;
+    myComment.appendChild(newComment);
+    inputComment.value = "";
+  }
 });
