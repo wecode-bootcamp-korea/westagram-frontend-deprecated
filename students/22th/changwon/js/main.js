232c0e1edf90975a8e8c5410c7e66c7d34ec76e8
최창원 <choich@ChangWon-Choi-MacBookPro.local>
2021-06-18 17:23:31 +0900

    [ADD] 댓글 입력 엔터 기능 추가

--- students/22th/changwon/js/main.js
+++ students/22th/changwon/js/main.js
@@ -1,12 +1,16 @@
+const addClick = document.getElementById('new-comment');
+addClick.addEventListener('keyup',function(e){
+  if(e.code == 'Enter'){
+    submitComment();
+  }
+})
+
+
 function submitComment() {
   const newcommentEL = document.getElementById("new-comment");
   const newcomment = newcommentEL.value.trim();
 
   if(newcomment) {
-    // const writeId = document.createElement('p');
-    // writeId.classList.add('commentId')
-    // writeId.innerText = "test ID";
-    // console.log(writeId);
     const contentEL = document.createElement('div');
     contentEL.classList.add('comment-content');
     contentEL.innerHTML = "Test ID" +" " + newcomment;
