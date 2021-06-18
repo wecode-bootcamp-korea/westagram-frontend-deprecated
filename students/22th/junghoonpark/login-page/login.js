de9bd361d7c9e177fce81fe37dba122e13cae90a
박정훈 <parkjeonghoon@park-ui-MacBook-Pro.local>
2021-06-18 20:26:38 +0900

    로그인 기능구현 완료, 메인 기능구현 진행중

--- students/22th/junghoonpark/login-page/login.js
+++ students/22th/junghoonpark/login-page/login.js
@@ -1,30 +1,19 @@
-// const thisIsButton = document.getElementsByClassName('login-btn')[0];
+"use strict";
+const westagramId = document.getElementById('typingId');
+const westagramPw = document.getElementById('password');
+const logInBtn = document.getElementsByClassName('login-btn')[0];
 
-// thisIsButton.addEventListener('click', function() {
-//   const password = document.getElementById('typingID').value;
-//   const rePassword = document.getElementById('password').value;
-  
-//   if (!password) {
-//     alert('비밀번호를 입력해주세요!');
-//     return;
-//   }
-  
-//   if (!rePassword) {
-//     alert('비밀번호 확인을 입력해주세요!');
-//     return;
-//   }
-  
-//   if (password !== rePassword) {
-//     alert('비밀번호가 맞지 않습니다!');
-//     return;
-//   }
-  
-//   alert('로그인 성공!!');
-// });
+westagramId.addEventListener('keyup', function(){
+  if(westagramId.value !== "" && westagramPw.value.length > 5 ){
+    logInBtn.style.opacity = '1';
+    logInBtn.disabled = false;
+  }
+})
 
-// const thisIsPw = document.getElementById('password');
-// const thisIsCode = document.getElementById('code');
+westagramPw.addEventListener('keyup', function(){
+  if(westagramPw.value !== "" && westagramId.value.length > 5)
+  logInBtn.style.opacity = '1';
+  logInBtn.disabled = false;
+})
 
-// thisIsPw.addEventListener('keyup', function(event) {
-//   thisIsCode.innerHTML = event.keyCode;
-// });
+
