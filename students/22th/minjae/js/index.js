e13dc274c90033ae7d4d9588abe8f9630aa3e26a
rious275 <rious275@naver.com>
2021-06-21 10:50:48 +0900

    댓글 기능 구현완료(westagram 적용x) a.html, a.css에 저장함. westagram css 작업 후 적용 필요함.

--- students/22th/minjae/js/index.js
+++ students/22th/minjae/js/index.js
@@ -1,10 +1,18 @@
 const inputId = document.querySelector(".id");
 const inputPassword = document.querySelector(".password");
 const button = document.querySelector(".button");
+// 각각이 아닌 폼 태그로 한번에 가져오면 이벤트가 자동으로 생성된다.
+
 
 function loginBtn() {
     let idValue = inputId.value;
     let passwordValue = inputPassword.value;
+
+    // loginBtn.disabled = !btn ? true : false;
+    //   loginBtn.style.opacity = btn ? 1 : 0.5;
+    // 3항 연산자 예시
+    // true, false 일때는 3항 연산자를 사용하기도 한다.
+
 
     if (idValue.length > 0 && passwordValue.length > 0) {
         button.disabled = false;
@@ -29,3 +37,7 @@ inputPassword.addEventListener('keyup', loginBtn);
 // inputPassword.addEventListener('keyup', function(e) {
 //     console.log('4');
 // })
+
+
+// location.replace("url")
+// url로 이동
