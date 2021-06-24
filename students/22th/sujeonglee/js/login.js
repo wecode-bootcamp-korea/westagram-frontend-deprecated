7cbbd58249a7b51ce9b22dc3d28893f6efa07700
soojeonglee <eee.soojeong@gmail.com>
2021-06-24 20:51:49 +0900

    Add: ID,PW validation(Mission5)

--- students/22th/sujeonglee/js/login.js
+++ students/22th/sujeonglee/js/login.js
@@ -1,13 +1,24 @@
 'use strict';
 
 // 로그인 버튼 활성화
+// addEventListener( 'keyup', () => {
+//   let loginId = document.getElementById('loginId').value; 
+//   let loginPw = document.getElementById('loginPw').value;
+//   let loginBtn = document.getElementsByClassName('loginBtn')[0];
+
+//   let loginBtnColor = loginBtn.style; 
+  
+//   return (loginId !=="") && (loginPw !=="") ? loginBtnColor.backgroundColor = "#0096f6" : loginBtnColor.backgroundColor = "#C0DFFD"; 
+// });
+
+// 유효성 검사
 addEventListener( 'keyup', () => {
   let loginId = document.getElementById('loginId').value; 
+  let findAt = loginId.indexOf('@')
   const loginPw = document.getElementById('loginPw').value;
   const loginBtn = document.getElementsByClassName('loginBtn')[0];
 
   const loginBtnColor = loginBtn.style; 
   
-  return (loginId !=="") && (loginPw !=="") ? loginBtnColor.backgroundColor = "#0096f6" : loginBtnColor.backgroundColor = "#C0DFFD"; 
+  return (findAt !== -1) && (loginPw.length >= 5) ? loginBtnColor.backgroundColor = "#0096f6" : loginBtnColor.backgroundColor = "#C0DFFD"; 
 });
-
